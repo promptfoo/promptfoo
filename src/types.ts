6affccb9030739a01f35e6dcb6aa3d21e983caf5
Ian Webster <ianw_github@ianww.com>
2023-03-31 08:47:37 -0700

    Initial commit

---
+++ src/types.ts
@@ -0,0 +1,31 @@
+export interface EvaluationOptions {
+  prompts: string[];
+  output: string;
+  provider: string;
+  vars?: string;
+}
+
+export interface ApiProvider {
+  callApi: (prompt: string) => Promise<ProviderResult>;
+}
+
+interface TokenUsage {
+  total: number;
+  prompt: number;
+  completion: number;
+}
+
+export interface ProviderResult {
+  output: string;
+  tokenUsage?: TokenUsage;
+}
+
+export interface CsvRow {
+  [key: string]: string;
+}
+
+export interface EvaluateResult {
+  successes: number;
+  failures: number;
+  tokenUsage: TokenUsage;
+}
