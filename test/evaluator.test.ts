4ec6695da7cb180e8d94a7d50238aceb307e6a2d
Ian Webster <ianw_github@ianww.com>
2023-05-07 09:15:26 -0700

    add tests

--- test/evaluator.test.ts
+++ test/evaluator.test.ts
@@ -10,6 +10,22 @@ const mockApiProvider: ApiProvider = {
   }),
 };
 
+const mockGradingApiProviderPasses: ApiProvider = {
+  id: jest.fn().mockReturnValue('test-grading-provider'),
+  callApi: jest.fn().mockResolvedValue({
+    output: JSON.stringify({pass: true, reason: 'Test grading output'}),
+    tokenUsage: { total: 10, prompt: 5, completion: 5 },
+  }),
+};
+
+const mockGradingApiProviderFails: ApiProvider = {
+  id: jest.fn().mockReturnValue('test-grading-provider'),
+  callApi: jest.fn().mockResolvedValue({
+    output: JSON.stringify({pass: false, reason: 'Grading failed reason'}),
+    tokenUsage: { total: 10, prompt: 5, completion: 5 },
+  }),
+};
+
 describe('evaluator', () => {
   afterEach(() => {
     jest.clearAllMocks();
@@ -140,6 +156,44 @@ describe('evaluator', () => {
       prompts: ['Test prompt'],
       vars: [{ __expected: 'eval:output === "Different output";' }],
       providers: [mockApiProvider],
+    };
+
+    const summary = await evaluate(options);
+
+    expect(mockApiProvider.callApi).toHaveBeenCalledTimes(1);
+    expect(summary.stats.successes).toBe(0);
+    expect(summary.stats.failures).toBe(1);
+    expect(summary.results[0].success).toBe(false);
+    expect(summary.results[0].response?.output).toBe('Test output');
+  });
+
+  test('evaluate with grading expected value', async () => {
+    const options = {
+      prompts: ['Test prompt'],
+      vars: [{ __expected: 'grade:output is a test output' }],
+      providers: [mockApiProvider],
+      grading: {
+        provider: mockGradingApiProviderPasses,
+      },
+    };
+
+    const summary = await evaluate(options);
+
+    expect(mockApiProvider.callApi).toHaveBeenCalledTimes(1);
+    expect(summary.stats.successes).toBe(1);
+    expect(summary.stats.failures).toBe(0);
+    expect(summary.results[0].success).toBe(true);
+    expect(summary.results[0].response?.output).toBe('Test output');
+  });
+
+  test('evaluate with grading expected value does not pass', async () => {
+    const options = {
+      prompts: ['Test prompt'],
+      vars: [{ __expected: 'grade:output is a test output' }],
+      providers: [mockApiProvider],
+      grading: {
+        provider: mockGradingApiProviderFails,
+      },
     };
 
     const summary = await evaluate(options);
