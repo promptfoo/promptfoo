0a2bb491b79b7431b9e0c17f3f0be4b3590837de
Ian Webster <ianw_github@ianww.com>
2023-05-22 11:50:26 -0700

    web viewer dark mode support

--- src/web/client/src/App.tsx
+++ src/web/client/src/App.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { io as SocketIOClient } from 'socket.io-client';
 
 import ResultsView from './ResultsView.js';
@@ -12,9 +14,31 @@ function App() {
   const { table, setTable } = useStore();
   const [loaded, setLoaded] = React.useState<boolean>(false);
 
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const [darkMode, setDarkMode] = React.useState(prefersDarkMode);
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: darkMode ? 'dark' : 'light',
+        },
+      }),
+    [darkMode],
+  );
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    if (!darkMode) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+      document.documentElement.removeAttribute('data-theme');
+    }
+  };
+
   React.useEffect(() => {
-    const socket = SocketIOClient(`http://${window.location.host}`);
-    //const socket = SocketIOClient(`http://localhost:15500`);
+    //const socket = SocketIOClient(`http://${window.location.host}`);
+    const socket = SocketIOClient(`http://localhost:15500`);
 
     socket.on('init', (data) => {
       console.log('Initialized socket connection');
@@ -33,10 +57,10 @@ function App() {
   }, [loaded, setTable]);
 
   return (
-    <>
-      <NavBar />
+    <ThemeProvider theme={theme}>
+      <NavBar darkMode={darkMode} onToggleDarkMode={toggleDarkMode} />
       {loaded && table ? <ResultsView /> : <div>Loading...</div>}
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/web/client/src/NavBar.tsx
+++ src/web/client/src/NavBar.tsx
@@ -1,11 +1,22 @@
 import Logo from './Logo';
 
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
+
 import './NavBar.css';
 
-export default function NavBar() {
+interface NavbarProps {
+  darkMode: boolean;
+  onToggleDarkMode: () => void;
+}
+
+export default function NavBar({ darkMode, onToggleDarkMode }: NavbarProps) {
   return (
     <nav>
       <Logo />
+      <div className="dark-mode-toggle" onClick={onToggleDarkMode}>
+        {darkMode ? <DarkModeIcon /> : <LightModeIcon />}
+      </div>
     </nav>
   );
 }
