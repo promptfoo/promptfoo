8eebc46eefc28a59af69b01eaaf6fb57b94bfcbd
Ian Webster <ianw_github@ianww.com>
2023-08-08 21:56:22 -0700

    aider: Created new component `ProviderConfigDialog.tsx` for editing the `config` object.
    # Aider chat conversation:
    
    USER: In ProviderSelector, when a Chip is clicked, show a Dialog that displays the ProviderConfig's `config` object for editing. This dialog should be a new component src/web/webui/pages/setup/ProviderConfigDialog.tsx
    
    ASSISTANT: To implement this feature, we need to create a new component `ProviderConfigDialog.tsx` and modify the existing `ProviderSelector.tsx` component.
    
    Let's start with creating the new component `ProviderConfigDialog.tsx`. This component will receive the `config` object as a prop and display it in a dialog for editing.
    
    ```tsx
    src/web/webui/pages/setup/ProviderConfigDialog.tsx
    <<<<<<< ORIGINAL
    =======
    import React from 'react';
    import { Dialog, DialogTitle, DialogContent, TextField, DialogActions, Button } from '@mui/material';
    import { ProviderConfig } from '../../../../types';
    
    interface ProviderConfigDialogProps {
      open: boolean;
      config: ProviderConfig['config'];
      onClose: () => void;
      onSave: (config: ProviderConfig['config']) => void;
    }
    
    const ProviderConfigDialog: React.FC<ProviderConfigDialogProps> = ({ open, config, onClose, onSave }) => {
      const [localConfig, setLocalConfig] = React.useState(config);
    
      React.useEffect(() => {
        setLocalConfig(config);
      }, [config]);
    
      const handleSave = () => {
        onSave(localConfig);
      };
    
      return (
        <Dialog open={open} onClose={onClose}>
          <DialogTitle>Edit Provider Config</DialogTitle>
          <DialogContent>
            <TextField
              label="Temperature"
              value={localConfig.temperature}
              onChange={(e) => setLocalConfig({ ...localConfig, temperature: parseFloat(e.target.value) })}
            />
            <TextField
              label="Max Tokens"
              value={localConfig.max_tokens}
              onChange={(e) => setLocalConfig({ ...localConfig, max_tokens: parseInt(e.target.value, 10) })}
            />
          </DialogContent>
          <DialogActions>
            <Button onClick={onClose}>Cancel</Button>
            <Button onClick={handleSave}>Save</Button>
          </DialogActions>
        </Dialog>
      );
    };
    
    export default ProviderConfigDialog;
    >>>>>>> UPDATED
    ```
    
    Next, we need to modify the `ProviderSelector.tsx` component to show the `ProviderConfigDialog` when a chip is clicked. We'll add a new state variable to keep track of the currently selected provider and show the dialog when a chip is clicked. We'll also handle the save event from the dialog to update the provider's config.
    
    I'll provide the changes for `ProviderSelector.tsx` in the next message.

---
+++ src/web/webui/pages/setup/ProviderConfigDialog.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Dialog, DialogTitle, DialogContent, TextField, DialogActions, Button } from '@mui/material';
+import { ProviderConfig } from '../../../../types';
+
+interface ProviderConfigDialogProps {
+  open: boolean;
+  config: ProviderConfig['config'];
+  onClose: () => void;
+  onSave: (config: ProviderConfig['config']) => void;
+}
+
+const ProviderConfigDialog: React.FC<ProviderConfigDialogProps> = ({ open, config, onClose, onSave }) => {
+  const [localConfig, setLocalConfig] = React.useState(config);
+
+  React.useEffect(() => {
+    setLocalConfig(config);
+  }, [config]);
+
+  const handleSave = () => {
+    onSave(localConfig);
+  };
+
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>Edit Provider Config</DialogTitle>
+      <DialogContent>
+        <TextField
+          label="Temperature"
+          value={localConfig.temperature}
+          onChange={(e) => setLocalConfig({ ...localConfig, temperature: parseFloat(e.target.value) })}
+        />
+        <TextField
+          label="Max Tokens"
+          value={localConfig.max_tokens}
+          onChange={(e) => setLocalConfig({ ...localConfig, max_tokens: parseInt(e.target.value, 10) })}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>Cancel</Button>
+        <Button onClick={handleSave}>Save</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default ProviderConfigDialog;
