4028d1d06c362ddaec90a3ae90cc5da69e7c63b4
Ian Webster <ianw_github@ianww.com>
2023-08-13 12:10:28 -0700

    delete old web uis

--- src/web/webui/server/main.ts
+++
@@ -1,3 +0,0 @@
-import { startServer } from './index';
-
-startServer();
--- src/web/webui/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import react from '@vitejs/plugin-react';
-import ssr from 'vite-plugin-ssr/plugin';
-import { UserConfig } from 'vite';
-
-const config: UserConfig = {
-  plugins: [react(), ssr()],
-};
-
-export default config;
