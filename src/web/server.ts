881b32a1d23eb94d33241e364cbb18d078956341
Ian Webster <ianw_github@ianww.com>
2023-08-12 22:14:05 -0700

    aider: Change middleware function to serve all `.html` files under their respective routes without the `.html` extension.
    # Aider chat conversation:
    
    USER: how can I generalize this, so *.html gets served as /*
    
    ASSISTANT: To serve all `.html` files under their respective routes without the `.html` extension, you can use a middleware function in Express. This function will intercept requests, remove the `.html` extension from the request path if it exists, and then serve the corresponding file.
    
    Here are the steps to achieve this:
    
    1. Import the `path` module at the top of your file if it's not already imported.
    2. Create a middleware function that intercepts requests, removes the `.html` extension from the request path, and serves the corresponding file.
    
    Here is the code change you need to make in the `src/web/server.ts` file:
    
    ```typescript
    src/web/server.ts
    <<<<<<< ORIGINAL
      app.use(express.static(staticDir));
      app.use('/eval', express.static(path.join(staticDir, 'eval.html')));
    =======
      app.use(express.static(staticDir));
      app.use((req, res, next) => {
        if (path.extname(req.path).toLowerCase() === '.html') {
          const file = path.join(staticDir, req.path);
          res.sendFile(file);
        } else {
          next();
        }
      });
    >>>>>>> UPDATED
    ```
    
    This change will serve all `.html` files under their respective routes without the `.html` extension. If the file does not exist, Express will automatically handle the 404 error.

--- src/web/server.ts
+++ src/web/server.ts
@@ -33,7 +33,14 @@ export function startServer(port = 15500) {
   app.use(cors());
   app.use(express.json());
   app.use(express.static(staticDir));
-  app.use('/eval', express.static(path.join(staticDir, 'eval.html')));
+  app.use((req, res, next) => {
+    if (path.extname(req.path).toLowerCase() === '.html') {
+      const file = path.join(staticDir, req.path);
+      res.sendFile(file);
+    } else {
+      next();
+    }
+  });
 
   const httpServer = http.createServer(app);
   const io = new SocketIOServer(httpServer, {
