240002e9c6de29f21899ae76b1b982665304e634
Ian Webster <ianw_github@ianww.com>
2023-08-13 21:31:50 -0700

    Fix static build

--- src/web/nextui/src/app/eval/[id]/page.tsx
+++ src/web/nextui/src/app/eval/[id]/page.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 import Eval from '../Eval';
 
+export async function generateStaticParams() {
+  return [];
+}
+
 export default async function Page({ params }: { params: { id: string } }) {
   const response = await fetch(`https://api.promptfoo.dev/eval/${params.id}`);
   const data = await response.json();
