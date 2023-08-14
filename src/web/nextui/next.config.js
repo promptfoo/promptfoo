a97e382765b16d8bbc8ac9f0eb74edc3b3866053
Ian Webster <ianw_github@ianww.com>
2023-08-13 20:57:42 -0700

    web build fixes

--- src/web/nextui/next.config.js
+++ src/web/nextui/next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   //output: 'standalone',
   output: 'export',
   trailingSlash: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      'bufferutil': 'commonjs bufferutil',
+    })
+    return config
+  },
 };
 
 module.exports = nextConfig;
