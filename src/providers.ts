79b677e048cbe65f74a26a9a2cf5cb020c0e6556
Ian Webster <ianw_github@ianww.com>
2023-08-24 15:20:39 -0700

    wip

--- src/providers.ts
+++ src/providers.ts
@@ -6,6 +6,7 @@ import { ReplicateProvider } from './providers/replicate';
 import { LocalAiCompletionProvider, LocalAiChatProvider } from './providers/localai';
 import { LlamaProvider } from './providers/llama';
 import { OllamaProvider } from './providers/ollama';
+import { WebhookProvider } from './providers/webhook';
 import { ScriptCompletionProvider } from './providers/scriptCompletion';
 import {
   AzureOpenAiChatCompletionProvider,
@@ -157,7 +158,7 @@ export async function loadApiProvider(
   }
 
   if (providerPath.startsWith('webhook:')) {
-    const webhookUrl = providerPath.split(':')[1];
+    const webhookUrl = providerPath.substring('webhook:'.length);
     return new WebhookProvider(webhookUrl);
   } else if (providerPath === 'llama' || providerPath.startsWith('llama:')) {
     const modelName = providerPath.split(':')[1];
