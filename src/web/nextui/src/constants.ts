ec29384661239b68ea84e4dd25744ef36c89b1df
Ian Webster <ianw_github@ianww.com>
2023-08-27 10:37:20 -0700

    fix localhost check

--- src/web/nextui/src/constants.ts
+++ src/web/nextui/src/constants.ts
@@ -1,5 +1,5 @@
 // Behavior varies depending on whether the app is running locally or not (e.g. connecting to local socket).
-export const IS_RUNNING_LOCALLY = window.location.hostname === `localhost`;
+export const IS_RUNNING_LOCALLY = !process.env.BUILD_STANDALONE_SERVER;
 
 // The base URL of the separate API server, used locally.
 export const API_BASE_URL = `http://localhost:15500`;
