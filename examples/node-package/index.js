93d9b3b8627d95909f5f3a106446557ee0d804f4
Ian Webster <ianw_github@ianww.com>
2023-09-12 12:06:42 -0400

    formatting

--- examples/node-package/index.js
+++ examples/node-package/index.js
@@ -1,50 +1,53 @@
 import promptfoo from '../../dist/src/index.js';
 
 (async () => {
-  const results = await promptfoo.evaluate({
-    prompts: ['Rephrase this in French: {{body}}', 'Rephrase this like a pirate: {{body}}'],
-    providers: [
-      'openai:gpt-3.5-turbo',
-      (prompt, context) => {
-        // Call LLM here...
-        console.log(`Prompt: ${prompt}, vars: ${JSON.stringify(context.vars)}`);
-        return {
-          output: '<LLM output>',
-        };
-      },
-    ],
-    tests: [
-      {
-        vars: {
-          body: 'Hello world',
+  const results = await promptfoo.evaluate(
+    {
+      prompts: ['Rephrase this in French: {{body}}', 'Rephrase this like a pirate: {{body}}'],
+      providers: [
+        'openai:gpt-3.5-turbo',
+        (prompt, context) => {
+          // Call LLM here...
+          console.log(`Prompt: ${prompt}, vars: ${JSON.stringify(context.vars)}`);
+          return {
+            output: '<LLM output>',
+          };
         },
-      },
-      {
-        vars: {
-          body: "I'm hungry",
-        },
-        assert: [
-          {
-            type: 'javascript',
-            value: (output) => {
-              const pass = output.includes("J'ai faim");
-              return {
-                pass,
-                score: pass ? 1.0 : 0.0,
-                reason: pass ? 'Output contained substring' : 'Output did not contain substring',
-              };
-            },
+      ],
+      tests: [
+        {
+          vars: {
+            body: 'Hello world',
           },
-        ],
-      },
-    ],
-  }, {
-    // EvaluateOptions
-    maxConcurrency: 2,
-    progressCallback: (progress, total) => {
-      console.log(`PROGRESS: ${progress} / ${total}`);
+        },
+        {
+          vars: {
+            body: "I'm hungry",
+          },
+          assert: [
+            {
+              type: 'javascript',
+              value: (output) => {
+                const pass = output.includes("J'ai faim");
+                return {
+                  pass,
+                  score: pass ? 1.0 : 0.0,
+                  reason: pass ? 'Output contained substring' : 'Output did not contain substring',
+                };
+              },
+            },
+          ],
+        },
+      ],
     },
-  });
+    {
+      // EvaluateOptions
+      maxConcurrency: 2,
+      progressCallback: (progress, total) => {
+        console.log(`PROGRESS: ${progress} / ${total}`);
+      },
+    },
+  );
   console.log('RESULTS:');
   console.log(JSON.stringify(results, null, 2));
 })();
