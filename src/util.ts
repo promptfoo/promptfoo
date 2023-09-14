5300c58061292e13f4fa903994d7f24797ffbc00
Ian Webster <ianw_github@ianww.com>
2023-09-14 11:18:21 -0700

    Add support for loading vars directly from file (#139)

--- src/util.ts
+++ src/util.ts
@@ -304,8 +304,18 @@ export async function readTest(
     const ret: TestCase = { ...testCase, vars: undefined };
     if (typeof testCase.vars === 'string' || Array.isArray(testCase.vars)) {
       ret.vars = await readVarsFiles(testCase.vars, testBasePath);
-    } else {
-      ret.vars = testCase.vars;
+    } else if (typeof testCase.vars === 'object') {
+      const vars: Record<string, string | string[] | object> = {};
+      for (const [key, value] of Object.entries(testCase.vars)) {
+        if (typeof value === 'string' && value.startsWith('file://')) {
+          // Load file from disk.
+          vars[key] = yaml.load(fs.readFileSync(value.slice('file://'.length), 'utf-8')) as string;
+        } else {
+          // This is a normal key:value.
+          vars[key] = value;
+        }
+      }
+      ret.vars = vars;
     }
     return ret;
   };
