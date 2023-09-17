6ebaee05e87be248c599a6217e21a78461d1cf5c
Ian Webster <ianw_github@ianww.com>
2023-09-17 17:31:02 -0400

    move testcases out of util

--- src/util.ts
+++ src/util.ts
@@ -7,21 +7,15 @@ import $RefParser from '@apidevtools/json-schema-ref-parser';
 import yaml from 'js-yaml';
 import nunjucks from 'nunjucks';
 import { globSync } from 'glob';
-import { parse as parsePath } from 'path';
-import { parse as parseCsv } from 'csv-parse/sync';
 import { stringify } from 'csv-stringify/sync';
 
 import logger from './logger';
-import { fetchCsvFromGoogleSheet } from './fetch';
 import { getDirectory } from './esm';
 
 import type {
-  Assertion,
-  CsvRow,
   EvaluateSummary,
   EvaluateTableOutput,
   UnifiedConfig,
-  TestCase,
   Prompt,
   ProviderOptionsMap,
   TestSuite,
@@ -75,14 +69,6 @@ export function readProviderPromptMap(
 }
 
 const PROMPT_DELIMITER = '---';
-
-function parseJson(json: string): any | undefined {
-  try {
-    return JSON.parse(json);
-  } catch (err) {
-    return undefined;
-  }
-}
 
 let globalConfigCache: any = null;
 
@@ -277,153 +263,6 @@ export function readPrompts(
   return promptContents;
 }
 
-export async function readVarsFiles(
-  pathOrGlobs: string | string[],
-  basePath: string = '',
-): Promise<Record<string, string | string[] | object>> {
-  if (typeof pathOrGlobs === 'string') {
-    pathOrGlobs = [pathOrGlobs];
-  }
-
-  const ret: Record<string, string | string[] | object> = {};
-  for (const pathOrGlob of pathOrGlobs) {
-    const resolvedPath = path.resolve(basePath, pathOrGlob);
-    const paths = globSync(resolvedPath);
-
-    for (const p of paths) {
-      const yamlData = yaml.load(fs.readFileSync(p, 'utf-8'));
-      Object.assign(ret, yamlData);
-    }
-  }
-
-  return ret;
-}
-
-export async function readTestsFile(varsPath: string, basePath: string = ''): Promise<CsvRow[]> {
-  // This function is confusingly named - it reads a CSV, JSON, or YAML file of
-  // TESTS or test equivalents.
-  const resolvedVarsPath = path.resolve(basePath, varsPath);
-  const fileExtension = parsePath(varsPath).ext.slice(1);
-  let rows: CsvRow[] = [];
-
-  if (fileExtension === 'csv') {
-    if (varsPath.startsWith('https://docs.google.com/spreadsheets/')) {
-      const csvData = await fetchCsvFromGoogleSheet(varsPath);
-      rows = parseCsv(csvData, { columns: true });
-    } else {
-      rows = parseCsv(fs.readFileSync(resolvedVarsPath, 'utf-8'), { columns: true });
-    }
-  } else if (fileExtension === 'json') {
-    rows = parseJson(fs.readFileSync(resolvedVarsPath, 'utf-8'));
-  } else if (fileExtension === 'yaml' || fileExtension === 'yml') {
-    rows = yaml.load(fs.readFileSync(resolvedVarsPath, 'utf-8')) as unknown as any;
-  }
-
-  return rows;
-}
-
-type TestCaseWithVarsFile = TestCase<
-  Record<string, string | string[] | object> | string | string[]
->;
-export async function readTest(
-  test: string | TestCaseWithVarsFile,
-  basePath: string = '',
-): Promise<TestCase> {
-  const loadTestWithVars = async (
-    testCase: TestCaseWithVarsFile,
-    testBasePath: string,
-  ): Promise<TestCase> => {
-    const ret: TestCase = { ...testCase, vars: undefined };
-    if (typeof testCase.vars === 'string' || Array.isArray(testCase.vars)) {
-      ret.vars = await readVarsFiles(testCase.vars, testBasePath);
-    } else if (typeof testCase.vars === 'object') {
-      const vars: Record<string, string | string[] | object> = {};
-      for (const [key, value] of Object.entries(testCase.vars)) {
-        if (typeof value === 'string' && value.startsWith('file://')) {
-          // Load file from disk.
-          vars[key] = yaml.load(fs.readFileSync(value.slice('file://'.length), 'utf-8')) as string;
-        } else {
-          // This is a normal key:value.
-          vars[key] = value;
-        }
-      }
-      ret.vars = vars;
-    }
-    return ret;
-  };
-
-  let testCase: TestCase;
-
-  if (typeof test === 'string') {
-    const testFilePath = path.resolve(basePath, test);
-    const testBasePath = path.dirname(testFilePath);
-    const rawTestCase = yaml.load(fs.readFileSync(testFilePath, 'utf-8')) as TestCaseWithVarsFile;
-    testCase = await loadTestWithVars(rawTestCase, testBasePath);
-  } else {
-    testCase = await loadTestWithVars(test, basePath);
-  }
-
-  // Validation of the shape of test
-  if (!testCase.assert && !testCase.vars && !testCase.options) {
-    throw new Error(
-      `Test case must have either assert, vars, or options property. Instead got ${JSON.stringify(
-        testCase,
-        null,
-        2,
-      )}`,
-    );
-  }
-
-  return testCase;
-}
-
-export async function readTests(
-  tests: string | string[] | TestCase[] | undefined,
-  basePath: string = '',
-): Promise<TestCase[]> {
-  const ret: TestCase[] = [];
-
-  const loadTestsFromGlob = async (loadTestsGlob: string) => {
-    const resolvedPath = path.resolve(basePath, loadTestsGlob);
-    const testFiles = globSync(resolvedPath);
-    const ret = [];
-    for (const testFile of testFiles) {
-      const testFileContent = yaml.load(fs.readFileSync(testFile, 'utf-8')) as TestCase[];
-      for (const testCase of testFileContent) {
-        ret.push(await readTest(testCase, path.dirname(testFile)));
-      }
-    }
-    return ret;
-  };
-
-  if (typeof tests === 'string') {
-    if (tests.endsWith('yaml') || tests.endsWith('yml')) {
-      // Points to a tests file with multiple test cases
-      return loadTestsFromGlob(tests);
-    } else {
-      // Points to a legacy vars.csv
-      const vars = await readTestsFile(tests, basePath);
-      return vars.map((row, idx) => {
-        const test = testCaseFromCsvRow(row);
-        test.description = `Row #${idx + 1}`;
-        return test;
-      });
-    }
-  } else if (Array.isArray(tests)) {
-    for (const globOrTest of tests) {
-      if (typeof globOrTest === 'string') {
-        // Resolve globs
-        ret.push(...(await loadTestsFromGlob(globOrTest)));
-      } else {
-        // It's just a TestCase
-        ret.push(await readTest(globOrTest, basePath));
-      }
-    }
-  }
-
-  return ret;
-}
-
 export function writeOutput(
   outputPath: string,
   results: EvaluateSummary,
@@ -553,26 +392,6 @@ export function cosineSimilarity(vecA: number[], vecB: number[]) {
   const vecAMagnitude = Math.sqrt(vecA.reduce((acc, val) => acc + val * val, 0));
   const vecBMagnitude = Math.sqrt(vecB.reduce((acc, val) => acc + val * val, 0));
   return dotProduct / (vecAMagnitude * vecBMagnitude);
-}
-
-export function testCaseFromCsvRow(row: CsvRow): TestCase {
-  const vars: Record<string, string> = {};
-  const asserts: Assertion[] = [];
-  for (const [key, value] of Object.entries(row)) {
-    if (key === '__expected') {
-      if (value.trim() !== '') {
-        const { assertionFromString } = require('./assertions');
-        asserts.push(assertionFromString(value));
-      }
-    } else {
-      vars[key] = value;
-    }
-  }
-
-  return {
-    vars,
-    assert: asserts,
-  };
 }
 
 export function getNunjucksEngine() {
