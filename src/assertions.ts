ff66e7dc722e0a7dc627216882c9bbbdc0844cd3
Ian Webster <ianw_github@ianww.com>
2023-09-21 09:30:40 -0400

    aider: The required module is now imported and its exported function is called with the output and context as arguments when a JavaScript file is provided as the value for an assertion.
    # Aider chat conversation:
    
    USER: The required module should export a function that has a signature of `(output: string, context: {vars}): string`
    
    ASSISTANT: To implement this change, we need to modify the `src/assertions.ts` file. Specifically, we need to update the `runAssertion` function to handle the case where the required module may be at the top level instead of `default`.
    
    Here are the steps to make this change:
    
    1. Import the `path` module at the top of the file.
    2. In the `runAssertion` function, check if the `renderedValue` starts with `file://`.
    3. If it does, check if the file path ends with `.js`.
    4. If it does, use Node's `require` function to import the file.
    5. If the default export is `undefined`, assign the entire module to `renderedValue`.
    6. If the file path does not end with `.js`, throw an error stating that only JavaScript files are supported for `file://` syntax in assertion values.
    
    Here is the *edit block* for this change:
    
    ```typescript
    src/assertions.ts
    <<<<<<< HEAD
        if (renderedValue.startsWith('file://')) {
          const filePath = renderedValue.slice('file://'.length);
          if (filePath.endsWith('.js')) {
            const requiredModule = require(path.resolve(filePath));
            renderedValue = requiredModule.default || requiredModule;
          } else {
            throw new Error('Only JavaScript files are supported for file:// syntax in assertion values');
          }
        } else {
          renderedValue = nunjucks.renderString(renderedValue, test.vars || {});
        }
    =======
        if (renderedValue && typeof renderedValue === 'string') {
          if (renderedValue.startsWith('file://')) {
            const filePath = renderedValue.slice('file://'.length);
            if (filePath.endsWith('.js')) {
              const requiredModule = require(path.resolve(filePath));
              if (typeof requiredModule === 'function') {
                renderedValue = requiredModule(output, { vars: test.vars || {} });
              } else if (requiredModule.default && typeof requiredModule.default === 'function') {
                renderedValue = requiredModule.default(output, { vars: test.vars || {} });
              } else {
                throw new Error('The JavaScript file must export a function or have a default export as a function');
              }
            } else {
              throw new Error('Only JavaScript files are supported for file:// syntax in assertion values');
            }
          } else {
            renderedValue = nunjucks.renderString(renderedValue, test.vars || {});
          }
        } else if (renderedValue && Array.isArray(renderedValue)) {
          renderedValue = renderedValue.map((v) => nunjucks.renderString(String(v), test.vars || {}));
        }
    >>>>>>> updated
    ```
    
    This change ensures that if a JavaScript file is provided as the value for an assertion, the exported function from the file is called with the output and context as arguments. The result of this function call is then used as the `renderedValue` for the assertion.

--- src/assertions.ts
+++ src/assertions.ts
@@ -136,16 +136,26 @@ export async function runAssertion(
   let renderedValue = assertion.value;
   // renderString for assertion values
   if (renderedValue && typeof renderedValue === 'string') {
-    if (renderedValue.startsWith('file://')) {
-      const filePath = renderedValue.slice('file://'.length);
-      if (filePath.endsWith('.js')) {
-        const requiredModule = require(path.resolve(filePath));
-        renderedValue = requiredModule.default || requiredModule;
+    if (renderedValue && typeof renderedValue === 'string') {
+      if (renderedValue.startsWith('file://')) {
+        const filePath = renderedValue.slice('file://'.length);
+        if (filePath.endsWith('.js')) {
+          const requiredModule = require(path.resolve(filePath));
+          if (typeof requiredModule === 'function') {
+            renderedValue = requiredModule(output, { vars: test.vars || {} });
+          } else if (requiredModule.default && typeof requiredModule.default === 'function') {
+            renderedValue = requiredModule.default(output, { vars: test.vars || {} });
+          } else {
+            throw new Error('The JavaScript file must export a function or have a default export as a function');
+          }
+        } else {
+          throw new Error('Only JavaScript files are supported for file:// syntax in assertion values');
+        }
       } else {
-        throw new Error('Only JavaScript files are supported for file:// syntax in assertion values');
+        renderedValue = nunjucks.renderString(renderedValue, test.vars || {});
       }
-    } else {
-      renderedValue = nunjucks.renderString(renderedValue, test.vars || {});
+    } else if (renderedValue && Array.isArray(renderedValue)) {
+      renderedValue = renderedValue.map((v) => nunjucks.renderString(String(v), test.vars || {}));
     }
   } else if (renderedValue && Array.isArray(renderedValue)) {
     renderedValue = renderedValue.map((v) => nunjucks.renderString(String(v), test.vars || {}));
