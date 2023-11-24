2861d0d88ae38dc5da6ad1c4f6a58c05f00caf04
Ian Webster <ianw_github@ianww.com>
2023-11-24 10:41:23 -0800

    fix: multiple configs handle external test and prompt files correctly (#291)

--- src/util.ts
+++ src/util.ts
@@ -26,6 +26,7 @@ import type {
 } from './types';
 import invariant from 'tiny-invariant';
 import { readPrompts } from './prompts';
+import { readTests } from './testCases';
 
 let globalConfigCache: any = null;
 
@@ -126,9 +127,10 @@ export async function readConfigs(configPaths: string[]): Promise<UnifiedConfig>
   });
 
   const tests: UnifiedConfig['tests'] = [];
-  configs.forEach((config) => {
+  configs.forEach(async (config) => {
     if (typeof config.tests === 'string') {
-      tests.push(config.tests);
+      const newTests = await readTests(config.tests, path.dirname(configPaths[0]));
+      tests.push(...newTests);
     } else if (Array.isArray(config.tests)) {
       tests.push(...config.tests);
     }
@@ -137,10 +139,11 @@ export async function readConfigs(configPaths: string[]): Promise<UnifiedConfig>
   const prompts: UnifiedConfig['prompts'] = [];
   const seenPrompts = new Set<string>();
   configs.forEach((config, idx) => {
+    // Need to read prompts from the config file just so we can dedupe them. They may reference external files.
     const ps = readPrompts(config.prompts, path.dirname(configPaths[idx]));
-    ps.forEach((prompt) => {
+    ps.forEach((prompt, idx) => {
       if (!seenPrompts.has(prompt.raw)) {
-        prompts.push(prompt.raw);
+        prompts.push(typeof config.prompts === 'string' ? config.prompts : config.prompts[idx]);
         seenPrompts.add(prompt.raw);
       }
     });
