5960c9c66dc5f163116bb3b5867444159d4096aa
Ian Webster <ianw_github@ianww.com>
2023-12-04 08:42:25 -0800

    feat: allow web viewer to be run remotely

--- src/web/nextui/src/constants.ts
+++ src/web/nextui/src/constants.ts
@@ -2,7 +2,7 @@
 export const IS_RUNNING_LOCALLY = !process.env.NEXT_PUBLIC_PROMPTFOO_BUILD_STANDALONE_SERVER;
 
 // The base URL of the separate API server, used locally.
-export const API_BASE_URL = `http://localhost:15500`;
+export const API_BASE_URL = process.env.PROMPTFOO_API_BASE_URL || '';
 
 // The base URL of API routes built into the Next.js app.
 export const NEXTJS_BASE_URL = ``;
