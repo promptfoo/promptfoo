02d8a2e5d7c3707132f545369018c589ef429214
Ian Webster <ianw_github@ianww.com>
2023-12-16 12:56:30 -0800

    fix: use execFile/spawn for external processes (#343)

--- src/providers/scriptCompletion.ts
+++ src/providers/scriptCompletion.ts
@@ -1,7 +1,9 @@
-import logger from '../logger';
-import { exec } from 'child_process';
+import { execFile } from 'child_process';
 
-import { ApiProvider, ProviderOptions, ProviderResponse } from '../types';
+import invariant from 'tiny-invariant';
+
+import logger from '../logger';
+import type { ApiProvider, ProviderOptions, ProviderResponse } from '../types';
 
 const ANSI_ESCAPE = /\x1b(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])/g;
 
@@ -17,29 +19,53 @@ export class ScriptCompletionProvider implements ApiProvider {
   }
 
   async callApi(prompt: string, vars?: Record<string, string | object>) {
-    return new Promise((resolve, reject) => {
-      const escapedPrompt = prompt.replace(/"/g, '\\"').replace(/\n/g, '\\n');
-      const escapedOptions = JSON.stringify(this.options || {})
-        .replace(/"/g, '\\"')
-        .replace(/\n/g, '\\n');
-      const escapedTestContext = JSON.stringify(vars || {})
-        .replace(/"/g, '\\"')
-        .replace(/\n/g, '\\n');
-      const command = this.options?.config.basePath
-        ? `cd ${this.options.config.basePath} && ${this.scriptPath} "${escapedPrompt}" "${escapedOptions}" "${escapedTestContext}"`
-        : `${this.scriptPath} "${escapedPrompt}" "${escapedOptions}" "${escapedTestContext}"`;
+    return new Promise<ProviderResponse>((resolve, reject) => {
+      // This regex handles quoted arguments, which are passed to the execFile call as a single argument
+      const scriptPartsRegex = /[^\s"']+|"([^"]*)"|'([^']*)'/g;
+      let match;
+      const scriptParts = [];
+
+      // Collect all parts based on the regular expression
+      while ((match = scriptPartsRegex.exec(this.scriptPath)) !== null) {
+        // If it's a quoted match, push the first group (ignoring the quotes)
+        if (match[1]) {
+          scriptParts.push(match[1]);
+        } else if (match[2]) {
+          // If it's a single-quoted match, push the second group (ignoring the quotes)
+          scriptParts.push(match[2]);
+        } else {
+          // Otherwise, push the whole match
+          scriptParts.push(match[0]);
+        }
+      }
+      const command = scriptParts.shift();
+      invariant(command, 'No command found in script path');
+      const scriptArgs = scriptParts.concat([
+        prompt,
+        JSON.stringify(this.options || {}),
+        JSON.stringify(vars || {}),
+      ]);
+      const options = this.options?.config.basePath ? { cwd: this.options.config.basePath } : {};
 
       // TODO(ian): Caching
-      exec(command, (error, stdout, stderr) => {
+      execFile(command, scriptArgs, options, (error, stdout, stderr) => {
         if (error) {
           logger.debug(`Error running script ${this.scriptPath}: ${error.message}`);
           reject(error);
-        } else {
-          const output = stripText(stdout.trim());
-          logger.debug(`Output from script ${this.scriptPath}: ${output}`);
-          resolve({ output });
+          return;
         }
+        const standardOutput = stripText(String(stdout).trim());
+        const errorOutput = stripText(String(stderr).trim());
+        if (errorOutput) {
+          logger.debug(`Error output from script ${this.scriptPath}: ${errorOutput}`);
+          if (!standardOutput) {
+            reject(new Error(errorOutput));
+            return;
+          }
+        }
+        logger.debug(`Output from script ${this.scriptPath}: ${standardOutput}`);
+        resolve({ output: standardOutput });
       });
-    }) as Promise<ProviderResponse>;
+    });
   }
 }
--- test/providers.test.ts
+++ test/providers.test.ts
@@ -1,5 +1,7 @@
 import * as fs from 'fs';
 import fetch from 'node-fetch';
+import child_process from 'child_process';
+import Stream from 'stream';
 
 import {
   OpenAiAssistantProvider,
@@ -10,7 +12,6 @@ import { AnthropicCompletionProvider } from '../src/providers/anthropic';
 import { LlamaProvider } from '../src/providers/llama';
 
 import { clearCache, disableCache, enableCache } from '../src/cache';
-import { maybeEmitAzureOpenAiWarning } from '../src/providers/azureopenaiUtil';
 import { loadApiProvider, loadApiProviders } from '../src/providers';
 import {
   AzureOpenAiChatCompletionProvider,
@@ -23,6 +24,7 @@ import {
   HuggingfaceFeatureExtractionProvider,
   HuggingfaceTextClassificationProvider,
 } from '../src/providers/huggingface';
+import { ScriptCompletionProvider } from '../src/providers/scriptCompletion';
 
 import type { ProviderOptionsMap, ProviderFunction } from '../src/types';
 
@@ -59,7 +61,7 @@ jest.mock('glob', () => ({
   globSync: jest.fn(),
 }));
 
-describe('providers', () => {
+describe('call provider apis', () => {
   afterEach(async () => {
     jest.clearAllMocks();
     await clearCache();
@@ -419,6 +421,70 @@ describe('providers', () => {
     });
   });
 
+  describe.each([
+    ['python rag.py', 'python', ['rag.py']],
+    ['echo "hello world"', 'echo', ['hello world']],
+    ['./path/to/file.py run', './path/to/file.py', ['run']],
+    ['"/Path/To/My File.py"', '/Path/To/My File.py', []],
+  ])('ScriptCompletionProvider callApi with script %s', (script, inputFile, inputArgs) => {
+    test('returns expected output', async () => {
+      const mockResponse = 'Test script output';
+      const mockChildProcess = {
+        stdout: new Stream.Readable(),
+        stderr: new Stream.Readable(),
+      } as child_process.ChildProcess;
+      jest
+        .spyOn(child_process, 'execFile')
+        .mockImplementation(
+          (
+            file: string,
+            args: readonly string[] | null | undefined,
+            options: child_process.ExecFileOptions | null | undefined,
+            callback?:
+              | null
+              | ((
+                  error: child_process.ExecFileException | null,
+                  stdout: string | Buffer,
+                  stderr: string | Buffer,
+                ) => void),
+          ) => {
+            expect(callback).toBeTruthy();
+            if (callback) {
+              expect(file).toContain(inputFile);
+              expect(args).toEqual(
+                expect.arrayContaining(
+                  inputArgs.concat([
+                    'Test prompt',
+                    '{"config":{"some_config_val":42}}',
+                    '{"var1":"value 1","var2":"value 2 \\"with some double \\"quotes\\"\\""}',
+                  ]),
+                ),
+              );
+              process.nextTick(() => callback(null, Buffer.from(mockResponse), Buffer.from('')));
+            }
+            return mockChildProcess;
+          },
+        );
+
+      const provider = new ScriptCompletionProvider(script, {
+        config: {
+          some_config_val: 42,
+        },
+      });
+      const result = await provider.callApi('Test prompt', {
+        var1: 'value 1',
+        var2: 'value 2 "with some double "quotes""',
+      });
+
+      expect(result.output).toBe(mockResponse);
+      expect(child_process.execFile).toHaveBeenCalledTimes(1);
+
+      jest.restoreAllMocks();
+    });
+  });
+});
+
+describe('loadApiProvider', () => {
   test('loadApiProvider with filepath', async () => {
     const mockYamlContent = `id: 'openai:gpt-4'
 config:
