b71f2ad4cdfbf2902fa34db0aabfd22c2a24d226
Ian Webster <ianw_github@ianww.com>
2023-12-26 23:46:28 -0800

    Move misc table view options into settings dialog

---
+++ src/web/nextui/src/app/eval/ResultsViewSettingsModal.tsx
@@ -0,0 +1,76 @@
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Slider from '@mui/material/Slider';
+import Tooltip from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
+
+import { useStore as useResultsViewStore } from './store';
+
+interface SettingsModalProps {
+  open: boolean;
+  onClose: () => void;
+}
+
+const SettingsModal: React.FC<SettingsModalProps> = ({ open, onClose }) => {
+  const {
+    maxTextLength,
+    setMaxTextLength,
+    wordBreak,
+    setWordBreak,
+    showInferenceDetails,
+    setShowInferenceDetails,
+  } = useResultsViewStore();
+  return (
+    <Dialog open={open} onClose={onClose} fullWidth maxWidth="sm">
+      <DialogTitle>Table View Settings</DialogTitle>
+      <DialogContent>
+        <Box>
+          <Tooltip title="Forcing line breaks makes it easier to adjust column widths to your liking">
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={wordBreak === 'break-all'}
+                  onChange={(e) => setWordBreak(e.target.checked ? 'break-all' : 'break-word')}
+                />
+              }
+              label="Force line breaks"
+            />
+          </Tooltip>
+        </Box>
+        <Box>
+          <Tooltip title="Show detailed inference statistics such as latency, tokens used, cost, etc.">
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={showInferenceDetails}
+                  onChange={(e) => setShowInferenceDetails(e.target.checked)}
+                />
+              }
+              label="Show inference details"
+            />
+          </Tooltip>
+        </Box>
+        <Box maxWidth="sm">
+          <Typography mt={2}>Max text length: {maxTextLength}</Typography>
+          <Slider
+            min={25}
+            max={1000}
+            value={maxTextLength}
+            onChange={(_, val: number | number[]) => setMaxTextLength(val as number)}
+          />
+        </Box>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>Close</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default SettingsModal;
--- src/web/nextui/src/app/eval/store.ts
+++ src/web/nextui/src/app/eval/store.ts
@@ -8,6 +8,13 @@ interface TableState {
 
   config: Partial<UnifiedConfig> | null;
   setConfig: (config: Partial<UnifiedConfig> | null) => void;
+
+  maxTextLength: number;
+  setMaxTextLength: (maxTextLength: number) => void;
+  wordBreak: 'break-word' | 'break-all';
+  setWordBreak: (wordBreak: 'break-word' | 'break-all') => void;
+  showInferenceDetails: boolean;
+  setShowInferenceDetails: (showInferenceDetails: boolean) => void;
 }
 
 export const useStore = create<TableState>((set) => ({
@@ -15,4 +22,11 @@ export const useStore = create<TableState>((set) => ({
   setTable: (table: EvaluateTable | null) => set(() => ({ table })),
   config: null,
   setConfig: (config: Partial<UnifiedConfig> | null) => set(() => ({ config })),
+
+  maxTextLength: 250,
+  setMaxTextLength: (maxTextLength: number) => set(() => ({ maxTextLength })),
+  wordBreak: 'break-word',
+  setWordBreak: (wordBreak: 'break-word' | 'break-all') => set(() => ({ wordBreak })),
+  showInferenceDetails: true,
+  setShowInferenceDetails: (showInferenceDetails: boolean) => set(() => ({ showInferenceDetails })),
 }));
