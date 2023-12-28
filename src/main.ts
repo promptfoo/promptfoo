69f6078a58e6afae0c75ceb610ca27b77247ce09
Ian Webster <ianw_github@ianww.com>
2023-12-28 15:22:05 -0500

    fix: disable cache when using repeat

--- src/main.ts
+++ src/main.ts
@@ -299,7 +299,9 @@ async function main() {
       if (cmdObj.verbose) {
         setLogLevel('debug');
       }
-      if (!cmdObj.cache) {
+      const iterations = parseInt(cmdObj.repeat || '', 10);
+      const repeat = !isNaN(iterations) && iterations > 0 ? iterations : 1;
+      if (!cmdObj.cache || repeat > 1) {
         logger.info('Cache is disabled.');
         disableCache();
       }
@@ -343,7 +345,7 @@ async function main() {
       // Parse prompts, providers, and tests
 
       const parsedPrompts = readPrompts(config.prompts, cmdObj.prompts ? undefined : basePath);
-      const parsedProviders = await loadApiProviders(config.providers, { 
+      const parsedProviders = await loadApiProviders(config.providers, {
         env: config.env,
         basePath,
       });
@@ -396,7 +398,6 @@ async function main() {
       };
 
       let maxConcurrency = parseInt(cmdObj.maxConcurrency || '', 10);
-      const iterations = parseInt(cmdObj.repeat || '', 10);
       const delay = parseInt(cmdObj.delay || '', 0);
 
       if (delay > 0) {
@@ -412,7 +413,7 @@ async function main() {
             ? getLogLevel() !== 'debug'
             : cmdObj.progressBar,
         maxConcurrency: !isNaN(maxConcurrency) && maxConcurrency > 0 ? maxConcurrency : undefined,
-        repeat: !isNaN(iterations) && iterations > 0 ? iterations : 1,
+        repeat,
         delay: !isNaN(delay) && delay > 0 ? delay : undefined,
         ...evaluateOptions,
       };
