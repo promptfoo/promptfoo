b30d9863840dae3b9a10b62e3f0c69c022aff0d0
Ian Webster <ianw_github@ianww.com>
2024-01-31 10:48:23 -0800

    feat: add caching for exec and python providers (#435)

--- src/providers/scriptCompletion.ts
+++ src/providers/scriptCompletion.ts
@@ -3,6 +3,8 @@ import { execFile } from 'child_process';
 import invariant from 'tiny-invariant';
 
 import logger from '../logger';
+import { getCache, isCacheEnabled } from '../cache';
+
 import type {
   ApiProvider,
   CallApiContextParams,
@@ -23,54 +25,68 @@ export class ScriptCompletionProvider implements ApiProvider {
     return `exec:${this.scriptPath}`;
   }
 
-  async callApi(prompt: string, context?: CallApiContextParams) {
-    return new Promise<ProviderResponse>((resolve, reject) => {
-      // This regex handles quoted arguments, which are passed to the execFile call as a single argument
-      const scriptPartsRegex = /[^\s"']+|"([^"]*)"|'([^']*)'/g;
-      let match;
-      const scriptParts = [];
+  async callApi(prompt: string, context?: CallApiContextParams): Promise<ProviderResponse> {
+    const cacheKey = `exec:${this.scriptPath}:${prompt}:${JSON.stringify(this.options)}`;
+    const cache = await getCache();
+    let cachedResult;
 
-      // Collect all parts based on the regular expression
-      while ((match = scriptPartsRegex.exec(this.scriptPath)) !== null) {
-        // If it's a quoted match, push the first group (ignoring the quotes)
-        if (match[1]) {
-          scriptParts.push(match[1]);
-        } else if (match[2]) {
-          // If it's a single-quoted match, push the second group (ignoring the quotes)
-          scriptParts.push(match[2]);
-        } else {
-          // Otherwise, push the whole match
-          scriptParts.push(match[0]);
-        }
-      }
-      const command = scriptParts.shift();
-      invariant(command, 'No command found in script path');
-      const scriptArgs = scriptParts.concat([
-        prompt,
-        JSON.stringify(this.options || {}),
-        JSON.stringify(context || {}),
-      ]);
-      const options = this.options?.config.basePath ? { cwd: this.options.config.basePath } : {};
+    if (isCacheEnabled()) {
+      cachedResult = await cache.get(cacheKey);
+    }
 
-      // TODO(ian): Caching
-      execFile(command, scriptArgs, options, (error, stdout, stderr) => {
-        if (error) {
-          logger.debug(`Error running script ${this.scriptPath}: ${error.message}`);
-          reject(error);
-          return;
-        }
-        const standardOutput = stripText(String(stdout).trim());
-        const errorOutput = stripText(String(stderr).trim());
-        if (errorOutput) {
-          logger.debug(`Error output from script ${this.scriptPath}: ${errorOutput}`);
-          if (!standardOutput) {
-            reject(new Error(errorOutput));
-            return;
+    if (cachedResult) {
+      logger.debug(`Returning cached result for script ${this.scriptPath}`);
+      return JSON.parse(cachedResult as string);
+    } else {
+      return new Promise<ProviderResponse>((resolve, reject) => {
+        const scriptPartsRegex = /[^\s"']+|"([^"]*)"|'([^']*)'/g;
+        let match;
+        const scriptParts = [];
+
+        while ((match = scriptPartsRegex.exec(this.scriptPath)) !== null) {
+          // If it's a quoted match, push the first group (ignoring the quotes)
+          if (match[1]) {
+            scriptParts.push(match[1]);
+          } else if (match[2]) {
+            // If it's a single-quoted match, push the second group (ignoring the quotes)
+            scriptParts.push(match[2]);
+          } else {
+            // Otherwise, push the whole match
+            scriptParts.push(match[0]);
           }
         }
-        logger.debug(`Output from script ${this.scriptPath}: ${standardOutput}`);
-        resolve({ output: standardOutput });
+        const command = scriptParts.shift();
+        invariant(command, 'No command found in script path');
+        const scriptArgs = scriptParts.concat([
+          prompt,
+          JSON.stringify(this.options || {}),
+          JSON.stringify(context || {}),
+        ]);
+        const options = this.options?.config.basePath ? { cwd: this.options.config.basePath } : {};
+
+        execFile(command, scriptArgs, options, async (error, stdout, stderr) => {
+          if (error) {
+            logger.debug(`Error running script ${this.scriptPath}: ${error.message}`);
+            reject(error);
+            return;
+          }
+          const standardOutput = stripText(String(stdout).trim());
+          const errorOutput = stripText(String(stderr).trim());
+          if (errorOutput) {
+            logger.debug(`Error output from script ${this.scriptPath}: ${errorOutput}`);
+            if (!standardOutput) {
+              reject(new Error(errorOutput));
+              return;
+            }
+          }
+          logger.debug(`Output from script ${this.scriptPath}: ${standardOutput}`);
+          const result = { output: standardOutput };
+          if (isCacheEnabled()) {
+            await cache.set(cacheKey, JSON.stringify(result));
+          }
+          resolve(result);
+        });
       });
-    });
+    }
   }
 }
