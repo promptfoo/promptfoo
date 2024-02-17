1a45c5567af8b25c6720d9cbab1af05033f6f4f5
Ian Webster <ianw_github@ianww.com>
2024-02-17 08:43:47 -0800

    fix: webui build on mac

--- src/web/nextui/next.config.js
+++ src/web/nextui/next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
     config.externals.push({
       'utf-8-validate': 'commonjs utf-8-validate',
       bufferutil: 'commonjs bufferutil',
+      fsevents: 'require("fsevents")',
     });
 
     return config;
