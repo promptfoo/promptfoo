5682c5a6b33791bc05bad40518b00a30434a150a
Ian Webster <ianw_github@ianww.com>
2024-02-21 09:14:37 -0800

    chore: update logos

--- site/src/components/HomepageInfo/index.tsx
+++ site/src/components/HomepageInfo/index.tsx
@@ -16,7 +16,6 @@ export default function HomepageInfo(): JSX.Element {
           <h2 className={styles.blurb}>Used by developers at</h2>
           <div className={styles.logoContainer}>
             <img src="/img/brands/discord-logo-blue.svg" />
-            <img src="/img/brands/mathworks-logo.svg" />
             <img style={{ maxWidth: '85px' }} src="/img/brands/google-logo.svg" />
             <img style={{ maxWidth: '100px' }} src="/img/brands/microsoft-logo.svg" />
             <img style={{ maxHeight: '120px' }} src="/img/brands/carvana-logo.svg" />
