ea0d840f3fd9bc4ab7ae79e2379660f778f5eef6
Ian Webster <ianw_github@ianww.com>
2024-02-22 14:40:52 -0800

    fix: remove supabase dependency when not running locally

--- src/web/nextui/src/constants.ts
+++ src/web/nextui/src/constants.ts
@@ -1,5 +1,7 @@
-// Behavior varies depending on whether the app is running locally or not (e.g. connecting to local socket).
+// Behavior varies depending on whether the app is running as a static HTML app on the user's local machine.
 export const IS_RUNNING_LOCALLY = !process.env.NEXT_PUBLIC_PROMPTFOO_BUILD_STANDALONE_SERVER;
+
+export const USE_SUPABASE = !!process.env.NEXT_PUBLIC_PROMPTFOO_USE_SUPABASE;
 
 // The base URL of API routes built into the Next.js app.
 export const NEXTJS_BASE_URL = ``;
