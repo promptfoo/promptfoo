6abcb6f6c08b2d07f32b2593a0f1ef4b4b5ace9d
Ian Webster <ianw_github@ianww.com>
2024-03-02 15:44:46 -0800

    cleanup

--- examples/dynamic-var/topic_generator.py
+++
@@ -1,8 +0,0 @@
-# In this example, we're going to randomly select a topic from a list of topics.
-# In practice, you might do something like fetch context from a vector database
-# such as Pinecone, Chroma, etc.
-
-import random
-
-fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape']
-print(random.choice(fruits))
