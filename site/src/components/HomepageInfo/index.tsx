ce608b76948f5427d5c3c7d356453e1275e218cb
Ian Webster <ianw_github@ianww.com>
2024-03-23 18:25:19 -0700

    docs: landing page

--- site/src/components/HomepageInfo/index.tsx
+++ site/src/components/HomepageInfo/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import CodeBlock from '@theme/CodeBlock';
+
 import styles from './styles.module.css';
 
 type FeatureItem = {
@@ -15,10 +18,65 @@ export default function HomepageInfo(): JSX.Element {
         <div className="container">
           <h2 className={styles.blurb}>Used by developers at</h2>
           <div className={styles.logoContainer}>
-            <img src="/img/brands/discord-logo-blue.svg" />
-            <img style={{ maxWidth: '85px' }} src="/img/brands/google-logo.svg" />
-            <img style={{ maxWidth: '100px' }} src="/img/brands/microsoft-logo.svg" />
-            <img style={{ maxHeight: '120px' }} src="/img/brands/carvana-logo.svg" />
+            <img src="/img/brands/discord-logo-blue.svg" alt="Discord" />
+            <img style={{ maxWidth: '85px' }} src="/img/brands/google-logo.svg" alt="Google" />
+            <img
+              style={{ maxWidth: '100px' }}
+              src="/img/brands/microsoft-logo.svg"
+              alt="Microsoft"
+            />
+            <img
+              style={{ maxHeight: '50px' }}
+              src="/img/brands/salesforce-logo.svg"
+              alt="Salesforce"
+            />
+            <img style={{ maxHeight: '120px' }} src="/img/brands/carvana-logo.svg" alt="Carvana" />
+          </div>
+        </div>
+      </section>
+      <section className={styles.section}>
+        <div className="container">
+          <h2 className={styles.blurb}>... to rapidly improve prompts and evaluate models</h2>
+        </div>
+        <div className={styles.configToWebview}>
+          <div className={styles.codeExample}>
+            <p>Simple, declarative config</p>
+            <CodeBlock language="yaml" className={styles.codeBlock}>
+              {`
+# Compare prompts...
+prompts:
+  - "Summarize this in {{language}}: {{document}}"
+  - "Summarize this in {{language}}, concisely and professionally: {{document}}"
+
+# And models...
+providers:
+  - openai:gpt-4-0125-preview
+  - anthropic:claude-3-opus
+  - mistral:mistral-large-latest
+
+# ... using these tests
+tests:
+  - vars:
+      language: French
+      document: "To be or not to be, that is the question..."
+    assert:
+      - type: contains
+        value: "Être ou ne pas être"
+      - type: cost
+        threshold: 0.01
+      - type: latency
+        threshold: 1000
+      - type: llm-rubric
+        value: does not apologize
+  - # ...
+              `}
+            </CodeBlock>
+          </div>
+          <div className={styles.webviewExample}>
+            <p>Detailed, actionable results</p>
+            <Link to="https://user-images.githubusercontent.com/310310/261666627-ce5a7817-da82-4484-b26d-32474f1cabc5.png">
+              <img src="https://user-images.githubusercontent.com/310310/261666627-ce5a7817-da82-4484-b26d-32474f1cabc5.png" />
+            </Link>
           </div>
         </div>
       </section>
