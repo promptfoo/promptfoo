cb6cd14fc450200717178929cc17e407c19fb3af
Ian Webster <ianw_github@ianww.com>
2024-03-26 13:26:42 -0700

    feat: cli watch for vars and providers

--- src/main.ts
+++ src/main.ts
@@ -658,7 +658,34 @@ async function main() {
                   )
                   .filter(Boolean) as string[])
               : [];
-            const watchPaths = Array.from(new Set([...configPaths, ...promptPaths]));
+            const providerPaths = Array.isArray(config.providers)
+              ? (config.providers
+                  .map((p) =>
+                    typeof p === 'string' && p.startsWith('file://')
+                      ? path.resolve(basePath, p.slice('file://'.length))
+                      : null,
+                  )
+                  .filter(Boolean) as string[])
+              : [];
+            const varPaths = Array.isArray(config.tests)
+              ? (config.tests
+                  .map((t) => {
+                    if (typeof t === 'string' && t.startsWith('file://')) {
+                      return path.resolve(basePath, t.slice('file://'.length));
+                    } else if (typeof t !== 'string' && t.vars) {
+                      Object.values(t.vars).forEach((v) => {
+                        if (typeof v === 'string' && v.startsWith('file://')) {
+                          return path.resolve(basePath, v.slice('file://'.length));
+                        }
+                      });
+                    }
+                    return null;
+                  })
+                  .filter(Boolean) as string[])
+              : [];
+            const watchPaths = Array.from(
+              new Set([...configPaths, ...promptPaths, ...providerPaths, ...varPaths]),
+            );
             const watcher = chokidar.watch(watchPaths, { ignored: /^\./, persistent: true });
 
             watcher
