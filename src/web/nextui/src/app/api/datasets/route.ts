7a72705143eef26035563f7ae25d2b1434656869
Ian Webster <ianw_github@ianww.com>
2024-03-29 07:41:11 -0700

    fix(selfhost): add support for prompts and datasets api endpoints

---
+++ src/web/nextui/src/app/api/datasets/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server';
+
+import { IS_RUNNING_LOCALLY } from '@/constants';
+import { getTestCases } from '@/../../../util';
+
+export const dynamic = IS_RUNNING_LOCALLY ? 'auto' : 'force-dynamic';
+
+export async function POST(req: Request) {
+  try {
+    return NextResponse.json({ data: await getTestCases() });
+  } catch (error) {
+    console.error('Failed to get test cases', error);
+    return NextResponse.json(
+      { error: 'Failed to get test cases', details: (error as Error).toString() },
+      { status: 500 },
+    );
+  }
+}
---
+++ src/web/nextui/src/app/api/prompts/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server';
+
+import { IS_RUNNING_LOCALLY } from '@/constants';
+import { getPrompts } from '@/../../../util';
+
+export const dynamic = IS_RUNNING_LOCALLY ? 'auto' : 'force-dynamic';
+
+export async function POST(req: Request) {
+  try {
+    return NextResponse.json({ data: await getPrompts() });
+  } catch (error) {
+    console.error('Failed to get prompts', error);
+    return NextResponse.json(
+      { error: 'Failed to get prompts', details: (error as Error).toString() },
+      { status: 500 },
+    );
+  }
+}
