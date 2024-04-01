4fc1a20a5d5ca8728b233f72ac11e06a3c0ac39c
Ian Webster <ianw_github@ianww.com>
2024-04-01 07:31:07 -0700

    fix(selfhost): Consolidate to `NEXT_PUBLIC_PROMPTFOO_REMOTE_BASE_URL` (#609)

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,14 @@
 // This is used for fetching and sharing evals.
 export const REMOTE_API_BASE_URL =
+  // TODO(ian): Backwards compatibility, 2024-04-01
   process.env.NEXT_PUBLIC_PROMPTFOO_REMOTE_API_BASE_URL ||
+
+  process.env.NEXT_PUBLIC_PROMPTFOO_REMOTE_BASE_URL ||
   process.env.PROMPTFOO_REMOTE_API_BASE_URL ||
   `https://api.promptfoo.dev`;
 
 // This is used for viewing evals.
 export const REMOTE_APP_BASE_URL =
-  process.env.NEXT_PUBLIC_PROMPTFOO_REMOTE_APP_BASE_URL ||
+  process.env.NEXT_PUBLIC_PROMPTFOO_REMOTE_BASE_URL ||
   process.env.PROMPTFOO_REMOTE_APP_BASE_URL ||
   `https://app.promptfoo.dev`;
