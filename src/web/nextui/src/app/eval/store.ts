1365475dd5cd7f950d9e18cb1e7e8172e2ee94f5
Ian Webster <ianw_github@ianww.com>
2024-04-02 09:35:10 -0700

    feat(webui): store settings in localstorage

--- src/web/nextui/src/app/eval/store.ts
+++ src/web/nextui/src/app/eval/store.ts
@@ -1,9 +1,10 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
 import type { EvaluateTable, FilePath, UnifiedConfig } from './types';
 
 interface TableState {
-  evalId: string | null,
+  evalId: string | null;
   setEvalId: (evalId: string) => void;
 
   table: EvaluateTable | null;
@@ -26,25 +27,34 @@ interface TableState {
   setShowPrompts: (showPrompts: boolean) => void;
 }
 
-export const useStore = create<TableState>((set) => ({
-  evalId: null,
-  setEvalId: (evalId: string) => set(() => ({ evalId })),
+export const useStore = create<TableState>()(
+  persist(
+    (set, get) => ({
+      evalId: null,
+      setEvalId: (evalId: string) => set(() => ({ evalId })),
 
-  table: null,
-  setTable: (table: EvaluateTable | null) => set(() => ({ table })),
-  config: null,
-  setConfig: (config: Partial<UnifiedConfig> | null) => set(() => ({ config })),
+      table: null,
+      setTable: (table: EvaluateTable | null) => set(() => ({ table })),
+      config: null,
+      setConfig: (config: Partial<UnifiedConfig> | null) => set(() => ({ config })),
 
-  maxTextLength: 250,
-  setMaxTextLength: (maxTextLength: number) => set(() => ({ maxTextLength })),
-  wordBreak: 'break-word',
-  setWordBreak: (wordBreak: 'break-word' | 'break-all') => set(() => ({ wordBreak })),
-  showInferenceDetails: true,
-  setShowInferenceDetails: (showInferenceDetails: boolean) => set(() => ({ showInferenceDetails })),
-  renderMarkdown: false,
-  setRenderMarkdown: (renderMarkdown: boolean) => set(() => ({ renderMarkdown })),
-  prettifyJson: false,
-  setPrettifyJson: (prettifyJson: boolean) => set(() => ({ prettifyJson })),
-  showPrompts: false,
-  setShowPrompts: (showPrompts: boolean) => set(() => ({ showPrompts })),
-}));
+      maxTextLength: 250,
+      setMaxTextLength: (maxTextLength: number) => set(() => ({ maxTextLength })),
+      wordBreak: 'break-word',
+      setWordBreak: (wordBreak: 'break-word' | 'break-all') => set(() => ({ wordBreak })),
+      showInferenceDetails: true,
+      setShowInferenceDetails: (showInferenceDetails: boolean) =>
+        set(() => ({ showInferenceDetails })),
+      renderMarkdown: false,
+      setRenderMarkdown: (renderMarkdown: boolean) => set(() => ({ renderMarkdown })),
+      prettifyJson: false,
+      setPrettifyJson: (prettifyJson: boolean) => set(() => ({ prettifyJson })),
+      showPrompts: false,
+      setShowPrompts: (showPrompts: boolean) => set(() => ({ showPrompts })),
+    }),
+    {
+      name: 'ResultsViewStorage',
+      storage: createJSONStorage(() => localStorage),
+    },
+  ),
+);
