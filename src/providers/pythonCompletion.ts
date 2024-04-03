5fad3e5c7ba621acdf6b8f35cffb6e048fa63861
Ian Webster <ianw_github@ianww.com>
2024-04-03 05:39:18 -0700

    fix: do not require basepath in python provider. #610

--- src/providers/pythonCompletion.ts
+++ src/providers/pythonCompletion.ts
@@ -23,7 +23,7 @@ export class PythonProvider implements ApiProvider {
   }
 
   async callApi(prompt: string, context?: CallApiContextParams): Promise<ProviderResponse> {
-    const absPath = path.resolve(path.join(this.options?.config.basePath, this.scriptPath));
+    const absPath = path.resolve(path.join(this.options?.config.basePath || '', this.scriptPath));
     logger.debug(`Computing file hash for script ${absPath}`);
     const fileHash = sha256(fs.readFileSync(absPath, 'utf-8'));
     const cacheKey = `python:${this.scriptPath}:${fileHash}:${prompt}:${JSON.stringify(
