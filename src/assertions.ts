889812929e195c3008350c5e98c35ef4d913e1ff
Ian Webster <ianw_github@ianww.com>
2024-04-03 05:38:34 -0700

    more error msg

--- src/assertions.ts
+++ src/assertions.ts
@@ -743,7 +743,7 @@ ${
         }
         if (!parsed.hasOwnProperty('pass') || !parsed.hasOwnProperty('score')) {
           throw new Error(
-            'Python assertion must return a boolean, number, or {pass, score, reason} object',
+            `Python assertion must return a boolean, number, or {pass, score, reason} object. Got instead: ${result}`,
           );
         }
         return parsed;
@@ -752,7 +752,7 @@ ${
         pass = assertion.threshold ? score >= assertion.threshold : score > 0;
         if (isNaN(score)) {
           throw new Error(
-            'Python assertion must return a boolean, number, or {pass, score, reason} object',
+            `Python assertion must return a boolean, number, or {pass, score, reason} object. Instead got:\n${result}`,
           );
         }
         if (typeof assertion.threshold !== 'undefined' && score < assertion.threshold) {
