c73c8d90a04f42bab30203b00c7d1e1079568f55
Ian Webster <ianw_github@ianww.com>
2024-04-19 10:20:31 -0700

    fix: include detail from external python assertion (#691)

--- examples/python-assert-external/assert.py
+++ examples/python-assert-external/assert.py
@@ -2,4 +2,15 @@ def get_assert(output, context):
     print('Prompt:', context['prompt'])
     print('Vars', context['vars']['topic'])
 
-    return 'bananas' in output.lower()
+    # You can return a bool...
+    # return 'bananas' in output.lower()
+
+    # A score (where 0 = Fail)...
+    # return 0.5
+
+    # Or an entire grading result...
+    return {
+        'pass': 'bananas' in output.lower(),
+        'score': 0.5,
+        'reason': 'Contains banana',
+    }
