767caca28f8e8b7f00c76c7793cb7c04b4b6f03f
Mikko Haapoja <mikkoh@users.noreply.github.com>
2024-05-01 13:19:55 -0400

    feat!: promptfoo eval --filter-failing outputFile.json

--- test/commands/eval/filterTests.test.ts
+++ test/commands/eval/filterTests.test.ts
@@ -14,47 +14,50 @@ describe('filterTests', () => {
     description: 'Louie',
   };
   const tests = [testHuey, testNoDescription, testDewey, testLouie];
+  const testSuite = {
+    tests,
+  } as TestSuite;
 
-  it('should run all tests when no args', () => {
-    const result = filterTests(tests, {});
+  it('should run all tests when no args', async () => {
+    const result = await filterTests(testSuite, {});
 
     expect(result).toStrictEqual(tests);
   });
 
-  it('handles no tests', () => {
-    const result = filterTests(undefined, {});
+  it('handles no tests', async () => {
+    const result = await filterTests({} as TestSuite, {});
 
     expect(result).toBe(undefined);
   });
 
   describe('firstN', () => {
-    it('should only run Huey and Dewey', () => {
-      const result = filterTests(tests, { firstN: '2' });
+    it('should only run Huey and Dewey', async () => {
+      const result = await filterTests(testSuite, { firstN: '2' });
 
       expect(result).toStrictEqual(tests.slice(0, 2));
     });
 
-    it('throws an exception when firstN is not a number', () => {
-      expect(() => filterTests(tests, { firstN: 'NOPE' }))
-        .toThrow(new Error('firstN must be a number, got: NOPE'));
+    it('throws an exception when firstN is not a number', async () => {
+      await expect(() => filterTests(testSuite, { firstN: 'NOPE' }))
+        .rejects.toEqual(new Error('firstN must be a number, got: NOPE'));
     });
   });
 
   describe('pattern', () => {
-    it('should only return tests whose description ends in "ey"', () => {
-      const result = filterTests(tests, { pattern: 'ey$' });
+    it('should only return tests whose description ends in "ey"', async () => {
+      const result = await filterTests(testSuite, { pattern: 'ey$' });
 
       expect(result).toStrictEqual([testHuey, testDewey]);
     });
 
-    it('can combine firstN and pattern', () => {
-      const result = filterTests(tests, { firstN: '1', pattern: 'ey$' });
+    it('can combine firstN and pattern', async () => {
+      const result = await filterTests(testSuite, { firstN: '1', pattern: 'ey$' });
 
       expect(result).toStrictEqual([testHuey]);
     });
 
-    it('does not mutate when when has args', () => {
-      const result = filterTests(tests, { firstN: '1', pattern: 'ey$' });
+    it('does not mutate when when has args', async () => {
+      const result = await filterTests(testSuite, { firstN: '1', pattern: 'ey$' });
 
       expect(result).not.toBe(tests);
     });
