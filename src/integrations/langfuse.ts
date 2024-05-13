9baa152c1b6c43fe37c7006219801d103f2f3f96
Albert Putra Purnama <14824254+albertpurnama@users.noreply.github.com>
2024-05-13 00:22:33 -0700

    fix(langfuse): Check runtime type of `getPrompt`, stringify the result (#774)

--- src/integrations/langfuse.ts
+++ src/integrations/langfuse.ts
@@ -1,17 +1,18 @@
 const langfuseParams = {
-    publicKey: process.env.LANGFUSE_PUBLIC_KEY,
-    secretKey: process.env.LANGFUSE_SECRET_KEY,
-    baseUrl: process.env.LANGFUSE_HOST,
-}
+  publicKey: process.env.LANGFUSE_PUBLIC_KEY,
+  secretKey: process.env.LANGFUSE_SECRET_KEY,
+  baseUrl: process.env.LANGFUSE_HOST,
+};
 
 import { Langfuse } from 'langfuse';
 
 const langfuse = new Langfuse(langfuseParams);
 
-export async function getPrompt(
-    id: string,
-    version?: number,
-): Promise<string> {
-    const prompt = await langfuse.getPrompt(id, version);
-    return prompt.getLangchainPrompt();
+export async function getPrompt(id: string, version?: number): Promise<string> {
+  const prompt = await langfuse.getPrompt(id, version);
+  const outputPrompt = prompt.getLangchainPrompt();
+  if (typeof outputPrompt !== 'string') {
+    return JSON.stringify(outputPrompt);
+  }
+  return outputPrompt;
 }
