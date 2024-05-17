11623397f50bf54c482459b202b654f436996290
Ian Webster <ianw_github@ianww.com>
2024-05-16 17:57:10 -0700

    chore: include full error response in openai errors

--- src/util.ts
+++ src/util.ts
@@ -1292,14 +1292,15 @@ export function resultIsForTestCase(result: EvaluateResult, testCase: TestCase):
   return varsMatch(testCase.vars, result.vars) && providersMatch;
 }
 
-export function safeJsonStringify(value: any): string {
+export function safeJsonStringify(value: any, prettyPrint: boolean = false): string {
   // Prevent circular references
   const cache = new Set();
+  const space = prettyPrint ? 2 : undefined;
   return JSON.stringify(value, (key, val) => {
     if (typeof val === 'object' && val !== null) {
       if (cache.has(val)) return;
       cache.add(val);
     }
     return val;
-  });
+  }, space);
 };
