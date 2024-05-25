a144fbe7d3beccdf9ea26656a540889061efdfd4
Tom Quist <tomquist@users.noreply.github.com>
2024-05-25 07:58:55 +0200

    feat: Allow functions in renderVarsInObject (#813)
    * feat: Allow functions in renderVarsInObject
    
    This allows using functions to render vars in function calls where
    string interpolation doesn't work.
    
    Example:
    ```javascript
    providers: {
      id: "openai:chat:gpt-3.5-turbo-0613",
      config: {
        tools: [
          {
            type: "function",
            function: {
              name: "call_me",
              parameters: {
                type: "object",
                  properties: {
                    location: {
                      type: "string",
                      description: "The city and state, e.g. San Francisco, CA"
                    },
                    unit: {
                      type: "string",
                      enum: (vars) => vars.units,
                    }
                  },
                  required: ["location"]
              }
            }
          }
        ]
      }
    ```

--- src/util.ts
+++ src/util.ts
@@ -1325,6 +1325,9 @@ export function renderVarsInObject<T>(obj: T, vars?: Record<string, string | obj
       result[key] = renderVarsInObject((obj as Record<string, unknown>)[key], vars);
     }
     return result as T;
+  } else if (typeof obj === 'function') {
+    const fn = obj as Function;
+    return renderVarsInObject(fn({ vars }) as T);
   }
   return obj;
 }
