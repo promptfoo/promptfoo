b8b06152699d390d44a5dad504bda91c5260e123
James Braza <jamesbraza@gmail.com>
2024-06-01 20:13:06 -0700

    Added description of default --output

--- src/main.ts
+++ src/main.ts
@@ -614,7 +614,7 @@ async function main() {
     .option('-a, --assertions <path>', 'Path to assertions file')
     .option('--model-outputs <path>', 'Path to JSON containing list of LLM output strings')
     .option('-t, --tests <path>', 'Path to CSV with test cases')
-    .option('-o, --output <paths...>', 'Path to output file (csv, txt, json, yaml, yml, html)')
+    .option('-o, --output <paths...>', 'Path to output file (csv, txt, json, yaml, yml, html), default is no output file')
     .option(
       '-j, --max-concurrency <number>',
       'Maximum number of concurrent API calls',
