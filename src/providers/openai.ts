e97ce63221b0e06f7e03f46c466da36c5b713017
Ian Webster <ianw_github@ianww.com>
2024-06-06 16:12:08 -0700

    chore(openai): add tool_choice `required` type

--- src/providers/openai.ts
+++ src/providers/openai.ts
@@ -42,7 +42,7 @@ type OpenAiCompletionOptions = OpenAiSharedOptions & {
   functions?: OpenAiFunction[];
   function_call?: 'none' | 'auto' | { name: string };
   tools?: OpenAiTool[];
-  tool_choice?: 'none' | 'auto' | { type: 'function'; function?: { name: string } };
+  tool_choice?: 'none' | 'auto' | 'required' | { type: 'function'; function?: { name: string } };
   response_format?: { type: 'json_object' };
   stop?: string[];
   seed?: number;
