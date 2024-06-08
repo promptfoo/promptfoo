324a463ff9b298241c961ed32d939f95404a89e8
Michael D'Angelo <michael.l.dangelo@gmail.com>
2024-06-08 18:57:18 -0400

    add linterT

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  ...tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+  ),
+  {
+    ignores: [
+      "src/web/nextui/out/",
+      "src/web/nextui/.next/",
+    ]
+  }
+];
