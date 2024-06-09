696311753dd5c069a271ae5d27c63012a1d5422f
Ian Webster <ianw_github@ianww.com>
2024-06-09 16:03:04 -0700

    cleanup

--- src/web/nextui/src/app/eval/store.ts
+++ src/web/nextui/src/app/eval/store.ts
@@ -30,15 +30,12 @@ interface TableState {
 
 const storage: StateStorage = {
   getItem: async (name: string): Promise<string | null> => {
-    console.log(name, 'has been retrieved');
     return (await get(name)) || null;
   },
   setItem: async (name: string, value: string): Promise<void> => {
-    console.log(name, 'with value', value, 'has been saved');
     await set(name, value);
   },
   removeItem: async (name: string): Promise<void> => {
-    console.log(name, 'has been deleted');
     await del(name);
   },
 };
