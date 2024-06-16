c0e6acf8440ff4aee0c41d250e26ee8fe62e5cdd
Ian Webster <ianw_github@ianww.com>
2024-06-16 11:02:05 -0700

    feat(webui): carousel for multiple failure reasons (#939)

---
+++ src/web/nextui/src/app/eval/FailReasonCarousel.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import IconButton from '@mui/material/IconButton';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+import './FailReasonCarousel.css';
+
+interface FailReasonCarouselProps {
+  failReasons: string[];
+}
+
+const FailReasonCarousel: React.FC<FailReasonCarouselProps> = ({ failReasons }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : failReasons.length - 1));
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex < failReasons.length - 1 ? prevIndex + 1 : 0));
+  };
+
+  return (
+    <div className="fail-reason">
+      {failReasons.length > 1 && (
+        <span className="fail-reason-carousel-controls">
+          <IconButton onClick={handlePrev}>
+            <ArrowBackIosIcon sx={{ fontSize: 12 }} />
+          </IconButton>
+          <span>
+            {currentIndex + 1}/{failReasons.length}
+          </span>
+          <IconButton onClick={handleNext}>
+            <ArrowForwardIosIcon sx={{ fontSize: 12 }} />
+          </IconButton>
+        </span>
+      )}
+      {failReasons[currentIndex]
+        .trim()
+        .split('\n')
+        .map((line, index) => (
+          <React.Fragment key={index}>
+            {line}
+            <br />
+          </React.Fragment>
+        ))}
+    </div>
+  );
+};
+
+export default FailReasonCarousel;
