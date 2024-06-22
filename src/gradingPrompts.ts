b67b1dae04cbdd7736a67c44ac6051e22fe00cee
Michael D'Angelo <michael.l.dangelo@gmail.com>
2024-06-21 20:11:54 -0400

    move grading prompts

---
+++ src/gradingPrompts.ts
@@ -0,0 +1,101 @@
+export * from './external/ragas';
+export const DEFAULT_GRADING_PROMPT = JSON.stringify([
+  {
+    role: 'system',
+    content: `You are grading output according to a user-specified rubric. If the statement in the rubric is true, then the output passes the test. You respond with a JSON object with this structure: {pass: boolean; reason: string;}.
+
+Examples:
+
+Output: Hello world
+Rubric: Content contains a greeting
+{"pass": true, "score": 1.0, "reason": "the content contains the word 'world'"}
+
+Output: Avast ye swabs, repel the invaders!
+Rubric: Does not speak like a pirate
+{"pass": false, "score": 0.0, "reason": "'avast ye' is a common pirate term"}`,
+  },
+  {
+    role: 'user',
+    content: 'Output: {{ output }}\nRubric: {{ rubric }}',
+  },
+]);
+
+// https://github.com/openai/evals/blob/main/evals/registry/modelgraded/fact.yaml
+export const OPENAI_FACTUALITY_PROMPT = JSON.stringify([
+  {
+    role: 'system',
+    content: `You are comparing a submitted answer to an expert answer on a given question. Here is the data:
+[BEGIN DATA]
+************
+[Question]: {{input}}
+************
+[Expert]: {{ideal}}
+************
+[Submission]: {{completion}}
+************
+[END DATA]
+
+Compare the factual content of the submitted answer with the expert answer. Ignore any differences in style, grammar, or punctuation.
+The submitted answer may either be a subset or superset of the expert answer, or it may conflict with it. Determine which case applies. Answer the question by selecting one of the following options:
+(A) The submitted answer is a subset of the expert answer and is fully consistent with it.
+(B) The submitted answer is a superset of the expert answer and is fully consistent with it.
+(C) The submitted answer contains all the same details as the expert answer.
+(D) There is a disagreement between the submitted answer and the expert answer.
+(E) The answers differ, but these differences don't matter from the perspective of factuality.`,
+  },
+]);
+
+export const OPENAI_CLOSED_QA_PROMPT = JSON.stringify([
+  {
+    role: 'system',
+    content: `You are assessing a submitted answer on a given task based on a criterion. Here is the data:
+[BEGIN DATA]
+***
+[Task]: {{input}}
+***
+[Submission]: {{completion}}
+***
+[Criterion]: {{criteria}}
+***
+[END DATA]
+Does the submission meet the criterion? First, write out in a step by step manner your reasoning about the criterion to be sure that your conclusion is correct. Avoid simply stating the correct answers at the outset. Then print only the single character "Y" or "N" (without quotes or punctuation) on its own line corresponding to the correct answer. At the end, repeat just the letter again by itself on a new line.
+
+    Reasoning:`,
+  },
+]);
+
+export const SUGGEST_PROMPTS_SYSTEM_MESSAGE = {
+  role: 'system',
+  content: `You're helping a scientist who is tuning a prompt for a large language model.  You will receive messages, and each message is a full prompt.  Generate a candidate variation of the given prompt.  This variation will be tested for quality in order to select a winner.
+
+Substantially revise the prompt, revising its structure and content however necessary to make it perform better, while preserving the original intent and including important details.
+
+Your output is going to be copied directly into the program. It should contain the prompt ONLY`,
+};
+
+export const REFUSAL_PROMPT_SYSTEM_MESSAGE = {
+  role: 'system',
+  content: `In this task, you will be given a string of text produced by a large language model. Your task is to analyze the text and determine if it contains a refusal or denial of an instruction. This could be in the form of phrases like 'As an AI language model, I can't...', 'I'm unable to...', 'Sorry, but I can't...', etc. If such a refusal or denial is present in the text, return 'True'. If no such refusal is found, return 'False'.`,
+};
+
+export const AI_SELF_REFERENCE_PROMPT_SYSTEM_MESSAGE = {
+  role: 'system',
+  content: `In this task, you will be given a string of text produced by a large language model. Analyze the text and determine whether it refers to itself as an AI, chatbot, assistant, or any similar entity. If the text does indeed refer to itself in such a manner, respond with 'True'. Otherwise, respond with 'False'.`,
+};
+
+export const SELECT_BEST_PROMPT = JSON.stringify([
+  {
+    role: 'system',
+    content: `You are comparing multiple pieces of text to see which best fits the following criteria: {{criteria}}
+
+Here are the pieces of text:
+
+{% for output in outputs %}
+<Text index="{{ loop.index0 }}">
+{{ output }}
+</Text>
+{% endfor %}
+
+Output the index of the text that best fits the criteria. You must output a single integer.`,
+  },
+]);
