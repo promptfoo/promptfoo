b5838a9c5c486b3586a21028fb17886575c4316e
Michael D'Angelo <michael.l.dangelo@gmail.com>
2024-07-07 22:02:11 -0700

    add sentry

---
+++ src/instrument.ts
@@ -0,0 +1,11 @@
+import * as Sentry from "@sentry/node";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
+
+Sentry.init({
+  dsn: process.env.PROMPTFOO_SENTRY_DSN,
+  integrations: [
+    nodeProfilingIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0,
+});
