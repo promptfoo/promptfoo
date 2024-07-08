2a605780bd66b89edf902ce42c7cac9a1c20bb6b
Michael D'Angelo <michael.l.dangelo@gmail.com>
2024-07-07 22:04:42 -0700

    format

--- src/instrument.ts
+++ src/instrument.ts
@@ -1,11 +1,9 @@
-import * as Sentry from "@sentry/node";
-import { nodeProfilingIntegration } from "@sentry/profiling-node";
+import * as Sentry from '@sentry/node';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
 
 Sentry.init({
   dsn: process.env.PROMPTFOO_SENTRY_DSN,
-  integrations: [
-    nodeProfilingIntegration(),
-  ],
+  integrations: [nodeProfilingIntegration()],
   tracesSampleRate: 1.0,
   profilesSampleRate: 1.0,
 });
