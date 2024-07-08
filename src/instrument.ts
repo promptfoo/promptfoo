10c838419e3ea0119f845db72677f90b7d5a4d27
Michael D'Angelo <michael.l.dangelo@gmail.com>
2024-07-07 23:13:30 -0700

    re-add profiling

--- src/instrument.ts
+++ src/instrument.ts
@@ -1,14 +1,16 @@
 import * as Sentry from '@sentry/node';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
 import { getAuthor } from './accounts';
 import logger from './logger';
 
 if (process.env.PROMPTFOO_ENABLE_ERROR_REPORTING) {
   Sentry.init({
     dsn: process.env.PROMPTFOO_SENTRY_DSN,
+    integrations: [nodeProfilingIntegration()],
     tracesSampleRate: 1.0,
     profilesSampleRate: 1.0,
   });
-  logger.info('Error reporting enabled');
+  logger.info('Error reporting enabled.');
   const author = getAuthor();
   if (author) {
     Sentry.setUser({ email: author });
