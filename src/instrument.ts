1b3ec1647e11a9484fdf7a9accec221fe480462a
Michael D'Angelo <michael.l.dangelo@gmail.com>
2024-07-07 22:18:59 -0700

    wrap in an if statement

--- src/instrument.ts
+++ src/instrument.ts
@@ -1,9 +1,18 @@
 import * as Sentry from '@sentry/node';
 import { nodeProfilingIntegration } from '@sentry/profiling-node';
+import { getAuthor } from './accounts';
+import logger from './logger';
 
-Sentry.init({
-  dsn: process.env.PROMPTFOO_SENTRY_DSN,
-  integrations: [nodeProfilingIntegration()],
-  tracesSampleRate: 1.0,
-  profilesSampleRate: 1.0,
-});
+if (process.env.PROMPTFOO_ENABLE_ERROR_REPORTING) {
+  Sentry.init({
+    dsn: process.env.PROMPTFOO_SENTRY_DSN,
+    integrations: [nodeProfilingIntegration()],
+    tracesSampleRate: 1.0,
+    profilesSampleRate: 1.0,
+  });
+  logger.info('Error reporting enabled');
+  const author = getAuthor();
+  if (author) {
+    Sentry.setUser({ email: author });
+  }
+}
