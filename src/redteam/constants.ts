430aae8d7279e0e8b33f84c778548647b04d7d89
Ian Webster <ianw_github@ianww.com>
2024-07-09 13:36:56 -0700

    merge

--- src/redteam/constants.ts
+++ src/redteam/constants.ts
@@ -1,1 +1,21 @@
+import { HARM_CATEGORIES } from './plugins/legacy/harmful';
+
 export const REDTEAM_MODEL = 'openai:chat:gpt-4o';
+
+export const BASE_PLUGINS = [
+  'contracts',
+  'excessive-agency',
+  'hallucination',
+  'harmful',
+  'hijacking',
+  'jailbreak',
+  'overreliance',
+  'pii',
+  'politics',
+  'prompt-injection',
+];
+
+export const ADDITIONAL_PLUGINS = ['competitors', 'experimental-jailbreak'];
+
+export const DEFAULT_PLUGINS = new Set([...BASE_PLUGINS, ...Object.keys(HARM_CATEGORIES)]);
+export const ALL_PLUGINS = new Set([...DEFAULT_PLUGINS, ...ADDITIONAL_PLUGINS]);
--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,3 @@
-import $RefParser from '@apidevtools/json-schema-ref-parser';
 import { createHash } from 'crypto';
 import { stringify } from 'csv-stringify/sync';
 import dotenv from 'dotenv';
@@ -10,7 +9,6 @@ import yaml from 'js-yaml';
 import nunjucks from 'nunjucks';
 import * as os from 'os';
 import * as path from 'path';
-import invariant from 'tiny-invariant';
 import { getAuthor } from './accounts';
 import cliState from './cliState';
 import { TERMINAL_MAX_WIDTH } from './constants';
@@ -28,7 +26,6 @@ import { writeCsvToGoogleSheet } from './googleSheets';
 import logger from './logger';
 import { runDbMigrations } from './migrate';
 import { runPython } from './python/wrapper';
-import { readTests } from './testCases';
 import {
   type EvalWithMetadata,
   type EvaluateResult,
@@ -43,7 +40,6 @@ import {
   type TestCasesWithMetadataPrompt,
   type UnifiedConfig,
   type OutputFile,
-  type ProviderOptions,
   type Prompt,
   type CompletedPrompt,
   type CsvRow,
@@ -52,270 +48,6 @@ import {
 } from './types';
 
 const DEFAULT_QUERY_LIMIT = 100;
-
-export async function dereferenceConfig(rawConfig: UnifiedConfig): Promise<UnifiedConfig> {
-  if (process.env.PROMPTFOO_DISABLE_REF_PARSER) {
-    return rawConfig;
-  }
-
-  // Track and delete tools[i].function for each tool, preserving the rest of the properties
-  // https://github.com/promptfoo/promptfoo/issues/364
-
-  // Remove parameters from functions and tools to prevent dereferencing
-  const extractFunctionParameters = (functions: { parameters?: object }[]) => {
-    return functions.map((func) => {
-      const { parameters } = func;
-      delete func.parameters;
-      return { parameters };
-    });
-  };
-
-  const extractToolParameters = (tools: { function?: { parameters?: object } }[]) => {
-    return tools.map((tool) => {
-      const { parameters } = tool.function || {};
-      if (tool.function?.parameters) {
-        delete tool.function.parameters;
-      }
-      return { parameters };
-    });
-  };
-
-  // Restore parameters to functions and tools after dereferencing
-  const restoreFunctionParameters = (
-    functions: { parameters?: object }[],
-    parametersList: { parameters?: object }[],
-  ) => {
-    functions.forEach((func, index) => {
-      if (parametersList[index]?.parameters) {
-        func.parameters = parametersList[index].parameters;
-      }
-    });
-  };
-
-  const restoreToolParameters = (
-    tools: { function?: { parameters?: object } }[],
-    parametersList: { parameters?: object }[],
-  ) => {
-    tools.forEach((tool, index) => {
-      if (parametersList[index]?.parameters) {
-        tool.function = tool.function || {};
-        tool.function.parameters = parametersList[index].parameters;
-      }
-    });
-  };
-
-  const functionsParametersList: { parameters?: object }[][] = [];
-  const toolsParametersList: { parameters?: object }[][] = [];
-
-  if (Array.isArray(rawConfig.providers)) {
-    rawConfig.providers.forEach((provider, providerIndex) => {
-      if (typeof provider === 'string') return;
-      if (typeof provider === 'function') return;
-      if (!provider.config) {
-        // Handle when provider is a map
-        provider = Object.values(provider)[0] as ProviderOptions;
-      }
-
-      if (provider.config?.functions) {
-        functionsParametersList[providerIndex] = extractFunctionParameters(
-          provider.config.functions,
-        );
-      }
-
-      if (provider.config?.tools) {
-        toolsParametersList[providerIndex] = extractToolParameters(provider.config.tools);
-      }
-    });
-  }
-
-  // Dereference JSON
-  const config = (await $RefParser.dereference(rawConfig)) as unknown as UnifiedConfig;
-
-  // Restore functions and tools parameters
-  if (Array.isArray(config.providers)) {
-    config.providers.forEach((provider, index) => {
-      if (typeof provider === 'string') return;
-      if (typeof provider === 'function') return;
-      if (!provider.config) {
-        // Handle when provider is a map
-        provider = Object.values(provider)[0] as ProviderOptions;
-      }
-
-      if (functionsParametersList[index]) {
-        provider.config.functions = provider.config.functions || [];
-        restoreFunctionParameters(provider.config.functions, functionsParametersList[index]);
-      }
-
-      if (toolsParametersList[index]) {
-        provider.config.tools = provider.config.tools || [];
-        restoreToolParameters(provider.config.tools, toolsParametersList[index]);
-      }
-    });
-  }
-  return config;
-}
-
-export async function readConfig(configPath: string): Promise<UnifiedConfig> {
-  const ext = path.parse(configPath).ext;
-  switch (ext) {
-    case '.json':
-    case '.yaml':
-    case '.yml':
-      const rawConfig = yaml.load(fs.readFileSync(configPath, 'utf-8')) as UnifiedConfig;
-      return dereferenceConfig(rawConfig);
-    case '.js':
-    case '.cjs':
-    case '.mjs':
-      return (await importModule(configPath)) as UnifiedConfig;
-    default:
-      throw new Error(`Unsupported configuration file format: ${ext}`);
-  }
-}
-
-export async function maybeReadConfig(configPath: string): Promise<UnifiedConfig | undefined> {
-  if (!fs.existsSync(configPath)) {
-    return undefined;
-  }
-  return readConfig(configPath);
-}
-
-/**
- * Reads multiple configuration files and combines them into a single UnifiedConfig.
- *
- * @param {string[]} configPaths - An array of paths to configuration files. Supports glob patterns.
- * @returns {Promise<UnifiedConfig>} A promise that resolves to a unified configuration object.
- */
-export async function readConfigs(configPaths: string[]): Promise<UnifiedConfig> {
-  const configs: UnifiedConfig[] = [];
-  for (const configPath of configPaths) {
-    const globPaths = globSync(configPath, {
-      windowsPathsNoEscape: true,
-    });
-    if (globPaths.length === 0) {
-      throw new Error(`No configuration file found at ${configPath}`);
-    }
-    for (const globPath of globPaths) {
-      const config = await readConfig(globPath);
-      configs.push(config);
-    }
-  }
-
-  const providers: UnifiedConfig['providers'] = [];
-  const seenProviders = new Set<string>();
-  configs.forEach((config) => {
-    invariant(
-      typeof config.providers !== 'function',
-      'Providers cannot be a function for multiple configs',
-    );
-    if (typeof config.providers === 'string') {
-      if (!seenProviders.has(config.providers)) {
-        providers.push(config.providers);
-        seenProviders.add(config.providers);
-      }
-    } else if (Array.isArray(config.providers)) {
-      config.providers.forEach((provider) => {
-        if (!seenProviders.has(JSON.stringify(provider))) {
-          providers.push(provider);
-          seenProviders.add(JSON.stringify(provider));
-        }
-      });
-    }
-  });
-
-  const tests: UnifiedConfig['tests'] = [];
-  for (const config of configs) {
-    if (typeof config.tests === 'string') {
-      const newTests = await readTests(config.tests, path.dirname(configPaths[0]));
-      tests.push(...newTests);
-    } else if (Array.isArray(config.tests)) {
-      tests.push(...config.tests);
-    }
-  }
-
-  const configsAreStringOrArray = configs.every(
-    (config) => typeof config.prompts === 'string' || Array.isArray(config.prompts),
-  );
-  const configsAreObjects = configs.every((config) => typeof config.prompts === 'object');
-  let prompts: UnifiedConfig['prompts'] = configsAreStringOrArray ? [] : {};
-
-  const makeAbsolute = (configPath: string, relativePath: string | Prompt) => {
-    if (typeof relativePath === 'string') {
-      if (relativePath.startsWith('file://')) {
-        relativePath =
-          'file://' + path.resolve(path.dirname(configPath), relativePath.slice('file://'.length));
-      }
-      return relativePath;
-    } else if (typeof relativePath === 'object' && relativePath.id) {
-      if (relativePath.id.startsWith('file://')) {
-        relativePath.id =
-          'file://' +
-          path.resolve(path.dirname(configPath), relativePath.id.slice('file://'.length));
-      }
-      return relativePath;
-    } else {
-      throw new Error('Invalid prompt object');
-    }
-  };
-
-  const seenPrompts = new Set<string | Prompt>();
-  const addSeenPrompt = (prompt: string | Prompt) => {
-    if (typeof prompt === 'string') {
-      seenPrompts.add(prompt);
-    } else if (typeof prompt === 'object' && prompt.id) {
-      seenPrompts.add(prompt);
-    } else {
-      throw new Error('Invalid prompt object');
-    }
-  };
-  configs.forEach((config, idx) => {
-    if (typeof config.prompts === 'string') {
-      invariant(Array.isArray(prompts), 'Cannot mix string and map-type prompts');
-      const absolutePrompt = makeAbsolute(configPaths[idx], config.prompts);
-      addSeenPrompt(absolutePrompt);
-    } else if (Array.isArray(config.prompts)) {
-      invariant(Array.isArray(prompts), 'Cannot mix configs with map and array-type prompts');
-      config.prompts
-        .map((prompt) => makeAbsolute(configPaths[idx], prompt))
-        .forEach((prompt) => addSeenPrompt(prompt));
-    } else {
-      // Object format such as { 'prompts/prompt1.txt': 'foo', 'prompts/prompt2.txt': 'bar' }
-      invariant(typeof prompts === 'object', 'Cannot mix configs with map and array-type prompts');
-      prompts = { ...prompts, ...config.prompts };
-    }
-  });
-  if (Array.isArray(prompts)) {
-    prompts.push(...Array.from(seenPrompts));
-  }
-
-  // Combine all configs into a single UnifiedConfig
-  const combinedConfig: UnifiedConfig = {
-    description: configs.map((config) => config.description).join(', '),
-    providers,
-    prompts,
-    tests,
-    scenarios: configs.flatMap((config) => config.scenarios || []),
-    defaultTest: configs.reduce((prev: Partial<TestCase> | undefined, curr) => {
-      return {
-        ...prev,
-        ...curr.defaultTest,
-        vars: { ...prev?.vars, ...curr.defaultTest?.vars },
-        assert: [...(prev?.assert || []), ...(curr.defaultTest?.assert || [])],
-        options: { ...prev?.options, ...curr.defaultTest?.options },
-      };
-    }, {}),
-    nunjucksFilters: configs.reduce((prev, curr) => ({ ...prev, ...curr.nunjucksFilters }), {}),
-    env: configs.reduce((prev, curr) => ({ ...prev, ...curr.env }), {}),
-    evaluateOptions: configs.reduce((prev, curr) => ({ ...prev, ...curr.evaluateOptions }), {}),
-    commandLineOptions: configs.reduce(
-      (prev, curr) => ({ ...prev, ...curr.commandLineOptions }),
-      {},
-    ),
-    metadata: configs.reduce((prev, curr) => ({ ...prev, ...curr.metadata }), {}),
-    sharing: !configs.some((config) => config.sharing === false),
-  };
-
-  return combinedConfig;
-}
 
 export function getNunjucksEngine(filters?: NunjucksFilterMap) {
   if (process.env.PROMPTFOO_DISABLE_TEMPLATING) {
