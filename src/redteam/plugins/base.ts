c37c854ea0727240a18bdcef2ff129d9dcc112a2
Michael D'Angelo <michael.l.dangelo@gmail.com>
2024-07-09 00:04:14 -0700

    format

--- src/redteam/plugins/base.ts
+++ src/redteam/plugins/base.ts
@@ -6,7 +6,6 @@ import { getNunjucksEngine } from '../../util';
  * Abstract base class for creating plugins that generate test cases.
  */
 export default abstract class PluginBase {
-
   /**
    * Template string used to generate prompts.
    */
