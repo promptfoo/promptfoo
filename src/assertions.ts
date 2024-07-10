fe9652f3455c33f2e1609c72a4d0701d3c1be29d
Ian Webster <ianw_github@ianww.com>
2024-07-10 12:35:16 -0700

    Revert "fix: remove double period in sql validation failures"
    This reverts commit 448460a4efb7fd59b159a4c1012833bfdc27ce0b.

--- src/assertions.ts
+++ src/assertions.ts
@@ -167,7 +167,7 @@ export async function isSql(
     } catch (err) {
       pass = inverse;
       const error = err as Error;
-      failureReasons.push(`SQL validation failed: ${error.message}`);
+      failureReasons.push(`SQL validation failed: ${error.message}.`);
     }
   }
 
@@ -178,7 +178,7 @@ export async function isSql(
     } catch (err) {
       pass = inverse;
       const error = err as Error;
-      failureReasons.push(`SQL validation failed: ${error.message}`);
+      failureReasons.push(`SQL validation failed: ${error.message}.`);
     }
   }
 
