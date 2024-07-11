e1b4be8ccdd80fc0bd9c9fe566ef1606b1c70b81
Michael <michael.l.dangelo@gmail.com>
2024-07-11 12:23:11 -0700

    refactor(redteam): update plugin base class to support multiple assertions (#1139)

--- test/redteam/plugins/base.test.ts
+++ test/redteam/plugins/base.test.ts
@@ -1,12 +1,11 @@
 import PluginBase from '../../../src/redteam/plugins/base';
-import { ApiProvider } from '../../../src/types';
+import { ApiProvider, Assertion } from '../../../src/types';
 import { getNunjucksEngine } from '../../../src/util';
 
 class TestPlugin extends PluginBase {
   protected template = 'Test template with {{ purpose }}';
-
-  protected getAssertion(prompt: string): any {
-    return { type: 'assertion', value: prompt };
+  protected getAssertions(prompt: string): Assertion[] {
+    return [{ type: 'contains', value: prompt }];
   }
 }
 
@@ -33,11 +32,11 @@ describe('PluginBase', () => {
     await expect(plugin.generateTests()).resolves.toEqual([
       {
         vars: { testVar: 'test prompt' },
-        assert: [{ type: 'assertion', value: 'test prompt' }],
+        assert: [{ type: 'contains', value: 'test prompt' }],
       },
       {
         vars: { testVar: 'another prompt' },
-        assert: [{ type: 'assertion', value: 'another prompt' }],
+        assert: [{ type: 'contains', value: 'another prompt' }],
       },
     ]);
     expect(provider.callApi).toHaveBeenCalledWith(
@@ -59,9 +58,9 @@ describe('PluginBase', () => {
   it('should filter and process prompts correctly', async () => {
     expect.assertions(1);
     await expect(plugin.generateTests()).resolves.toEqual([
-      { assert: [{ type: 'assertion', value: 'test prompt' }], vars: { testVar: 'test prompt' } },
+      { assert: [{ type: 'contains', value: 'test prompt' }], vars: { testVar: 'test prompt' } },
       {
-        assert: [{ type: 'assertion', value: 'another prompt' }],
+        assert: [{ type: 'contains', value: 'another prompt' }],
         vars: { testVar: 'another prompt' },
       },
     ]);
