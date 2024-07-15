1d9306e0e4019652781e26620115d6696bc3e2dc
Michael D'Angelo <michael.l.dangelo@gmail.com>
2024-07-14 21:47:28 -0400

    respond to feedback

--- src/web/nextui/src/app/components/InfoModal.tsx
+++ src/web/nextui/src/app/components/InfoModal.tsx
@@ -3,21 +3,23 @@ import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import ForumIcon from '@mui/icons-material/Forum';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import MenuBookIcon from '@mui/icons-material/MenuBook';
-import { Modal } from '@mui/material';
+import { Box, Modal, Typography } from '@mui/material';
 import Link from 'next/link';
 import './InfoModal.css';
 
 export function InfoModal({ open, onClose }: { open: boolean; onClose: () => void }) {
   return (
     <Modal open={open} onClose={onClose} className="info-modal">
-      <div className="info-modal-content">
-        <h2 className="info-modal-title">About Promptfoo</h2>
-        <p className="info-modal-description">
+      <Box className="info-modal-content">
+        <Typography component="h2" variant="h5" className="info-modal-title">
+          About Promptfoo
+        </Typography>
+        <Typography component="p" variant="body2" className="info-modal-description">
           Promptfoo is a MIT licensed open-source tool for evaluating LLMs. We make it easy to track
           the performance of your models and prompts over time with automated support for dataset
           generation and grading.
-        </p>
-        <p className="info-modal-version">
+        </Typography>
+        <Typography component="h5" variant="h6" sx={{ marginTop: '.2em' }}>
           <Link
             href="https://github.com/promptfoo/promptfoo/releases"
             target="_blank"
@@ -25,37 +27,39 @@ export function InfoModal({ open, onClose }: { open: boolean; onClose: () => voi
           >
             Version {process.env.PROMPTFOO_VERSION}
           </Link>
-        </p>
-        <div className="info-modal-links">
-          <Link href="/docs" target="_blank" className="info-modal-link">
-            <MenuBookIcon fontSize="small" /> Documentation
-          </Link>
-          <Link
-            href="https://github.com/promptfoo/promptfoo"
-            target="_blank"
-            className="info-modal-link"
-          >
-            <GitHubIcon fontSize="small" /> GitHub Repository
-          </Link>
-          <Link
-            href="https://github.com/promptfoo/promptfoo/issues"
-            target="_blank"
-            className="info-modal-link"
-          >
-            <BugReportIcon fontSize="small" /> File an Issue
-          </Link>
-          <Link href="https://discord.gg/gHPS9jjfbs" target="_blank" className="info-modal-link">
-            <ForumIcon fontSize="small" /> Join Our Discord Community
-          </Link>
-          <Link
-            href="https://cal.com/team/promptfoo/intro"
-            target="_blank"
-            className="info-modal-link"
-          >
-            <CalendarTodayIcon fontSize="small" /> Book a Meeting
-          </Link>
-        </div>
-      </div>
+        </Typography>
+        <Link
+          href="https://www.promptfoo.dev/docs/intro"
+          target="_blank"
+          className="info-modal-link"
+        >
+          <MenuBookIcon fontSize="small" /> Documentation
+        </Link>
+        <Link
+          href="https://github.com/promptfoo/promptfoo"
+          target="_blank"
+          className="info-modal-link"
+        >
+          <GitHubIcon fontSize="small" /> GitHub Repository
+        </Link>
+        <Link
+          href="https://github.com/promptfoo/promptfoo/issues"
+          target="_blank"
+          className="info-modal-link"
+        >
+          <BugReportIcon fontSize="small" /> File an Issue
+        </Link>
+        <Link href="https://discord.gg/gHPS9jjfbs" target="_blank" className="info-modal-link">
+          <ForumIcon fontSize="small" /> Join Our Discord Community
+        </Link>
+        <Link
+          href="https://cal.com/team/promptfoo/intro"
+          target="_blank"
+          className="info-modal-link"
+        >
+          <CalendarTodayIcon fontSize="small" /> Book a Meeting
+        </Link>
+      </Box>
     </Modal>
   );
 }
