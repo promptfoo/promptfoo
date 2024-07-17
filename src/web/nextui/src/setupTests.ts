b0fd6afd4be96a8face4126fceae5be4042b59b3
Michael D'Angelo <michael.l.dangelo@gmail.com>
2024-07-16 22:27:11 -0400

    add vitest

---
+++ src/web/nextui/src/setupTests.ts
@@ -0,0 +1,4 @@
+import '@testing-library/jest-dom';
+
+// Mock the environment variables
+// process.env.PROMPTFOO_VERSION = '1.0.0';
---
+++ src/web/nextui/vitest.config.ts
@@ -0,0 +1,11 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    globals: true,
+  },
+});
