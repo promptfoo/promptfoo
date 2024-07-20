8eb1db0edaa740fafe7f6787c59613c6867d08ed
Ian Webster <ianw_github@ianww.com>
2024-07-20 16:28:30 -0700

    site: improve config validator

--- site/src/pages/validator.tsx
+++ site/src/pages/validator.tsx
@@ -1,13 +1,43 @@
 import { Editor } from '@monaco-editor/react';
 import React, { useState, useEffect } from 'react';
-import { Box, Container, Typography, Paper, Grid } from '@mui/material';
+import Link from '@docusaurus/Link';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 import Layout from '@theme/Layout';
 import Ajv from 'ajv';
 import yaml from 'js-yaml';
 
+const DEFAULT_INPUT = `prompts:
+  - "Write a tweet about {{topic}}"
+
+providers:
+  - openai:chat:gpt-4o
+  - anthropic:messages:claude-3-5-sonnet-20240620
+
+tests:
+  - vars:
+      topic: bananas
+    assert:
+      - type: contains
+        text: "banana"
+  - vars:
+      topic: pineapples
+    assert:
+      - type: llm-rubric
+        text: "mentions health benefits"
+        
+defaultTest:
+  assert:
+    - type: javascript
+      value: "output.length <= 280"
+`;
+
 const ConfigValidator = () => {
-  const [input, setInput] = useState('');
-  const [validationOutput, setValidationOutput] = useState('');
+  const [input, setInput] = useState(DEFAULT_INPUT);
+  const [validationOutput, setValidationOutput] = useState('Configuration is valid');
   const [schema, setSchema] = useState(null);
 
   useEffect(() => {
@@ -74,23 +104,27 @@ const ConfigValidator = () => {
             Promptfoo Config Validator
           </Typography>
           <Typography variant="body1" gutterBottom align="center">
-            Enter your YAML or JSON configuration below to validate it against the schema.
+            Enter your YAML or JSON configuration below to validate it against the{' '}
+            <Link to="https://github.com/promptfoo/promptfoo/blob/main/site/static/config-schema.json">
+              schema
+            </Link>
+            .
           </Typography>
         </Box>
         <Grid container spacing={3}>
           <Grid item xs={12} md={6}>
-            <Paper elevation={3} sx={{ height: '500px' }}>
+            <Paper elevation={3} sx={{ height: '80vh' }}>
               <Editor
                 height="100%"
                 defaultLanguage="yaml"
                 value={input}
                 onChange={handleEditorChange}
-                options={{ minimap: { enabled: false } }}
+                options={{ tabSize: 2, minimap: { enabled: false } }}
               />
             </Paper>
           </Grid>
           <Grid item xs={12} md={6}>
-            <Paper elevation={3} sx={{ height: '500px', p: 2, overflow: 'auto' }}>
+            <Paper elevation={3} sx={{ height: '80vh', p: 2, overflow: 'auto' }}>
               <pre>{validationOutput}</pre>
             </Paper>
           </Grid>
