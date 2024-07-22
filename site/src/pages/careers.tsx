bde40fbbe8a9d868e338c54f7bfc142075d2455f
Michael D'Angelo <michael.l.dangelo@gmail.com>
2024-07-22 00:59:42 -0400

    apply copy edits

--- site/src/pages/careers.tsx
+++ site/src/pages/careers.tsx
@@ -14,74 +14,64 @@ export default function Careers(): JSX.Element {
         <h2>Shaping the Future of LLMs</h2>
 
         <p>
-          At Promptfoo, we're dedicated to revolutionizing the way developers interact with and
-          optimize AI language models. Our open-source tool is at the forefront of prompt
-          engineering, helping companies and individuals harness the full potential of AI. Our
-          mission is to empower developers and businesses with robust tools for LLM testing and
-          prompt engineering, fostering a more reliable and transparent AI ecosystem.
+          PromptFoo is set to be the go-to platform for developers to iterate on LLM and Gen AI applications. 
+          Our open-source evaluation tool is widely adopted for prompt engineering and LLM evaluations, 
+          helping companies and individuals harness the full potential of AI. Our mission is to empower 
+          developers and businesses with robust tools for optimizing LLM development, testing and evaluating 
+          Gen AI applications, and fostering a more reliable and transparent AI ecosystem.
         </p>
 
         <h2>Why Work with Us?</h2>
+        <p>Of the developers, for the developers, by the developers–And with the developers everywhere.</p>
         <ul>
-          <li>
-            <strong>Open Source at Heart:</strong> Contribute to and help grow our open source
-            projects, making a lasting impact on the LLM community.
-          </li>
-          <li>
-            <strong>LLM Innovation:</strong> Be at the forefront of LLM development, working on
-            challenges that push the boundaries of what's possible.
-          </li>
-          <li>
-            <strong>Collaborative Spirit:</strong> Join a team that values diverse perspectives and
-            believes in the power of collective intelligence.
-          </li>
-          <li>
-            <strong>Continuous Learning:</strong> Grow your skills in a rapidly evolving field, with
-            opportunities to explore new ideas and technologies.
-          </li>
-          <li>
-            <strong>Meaningful Impact:</strong> Develop tools and solutions that help ensure LLMs
-            are used responsibly and effectively across industries.
-          </li>
-          <li>
-            <strong>Remote-First Culture:</strong> Work from anywhere, anytime.
-          </li>
+          <li>We are open-source at heart.</li>
+          <li>We put developers first.</li>
+          <li>We are pioneering the riskiest rocks in LLM development.</li>
         </ul>
+
+        <h3>Join our mission.</h3>
+        <p>
+          We are committed to bringing to the world a responsible, safe, and effective LLM development framework. 
+          You will make a meaningful impact at this crucial inflection point in technology and society.
+        </p>
+
+        <h3>...with your contribution!</h3>
+        <p>
+          We believe in diverse perspectives and horizontal team collaboration. It brings the most for the team, 
+          product, and the developer ecosystem. You will be a crucial member of the team from Day 1.
+        </p>
+        <p>You can work from anywhere, anytime.</p>
 
         <h2>Our Values</h2>
         <ul>
           <li>
-            <strong>Transparency:</strong> We believe in open communication and sharing knowledge.
+            <strong>Open-source:</strong> We believe in open communication and sharing knowledge–both in product and team cultures.
           </li>
           <li>
-            <strong>Collaboration:</strong> We thrive on teamwork and diverse perspectives.
+            <strong>Bottom-up:</strong> We believe in horizontal teams where everyone's voices make a meaningful impact.
           </li>
           <li>
-            <strong>User-Centric:</strong> Our users' needs drive our innovation.
+            <strong>Developer-first:</strong> We are committed to putting developer experience as the first and foremost top priority–everything else follows.
           </li>
           <li>
-            <strong>Excellence:</strong> We strive for the highest quality in everything we do.
+            <strong>Biggest-rock:</strong> We tackle the hardest, most challenging problems in safe, responsible, effective Gen AI development.
           </li>
         </ul>
 
         <h2>Our Focus Areas</h2>
-        <p>We're deeply involved in several key aspects of LLM technology:</p>
+        <p>We're deeply involved in several key aspects of LLM developer workflows and ecosystem:</p>
         <ul>
           <li>
-            <strong>Prompt Engineering:</strong> Develop innovative methods to optimize prompt
-            performance for diverse LLM applications.
+            <strong>Prompt Engineering:</strong> Develop innovative methods to optimize prompt performance for diverse LLM applications.
           </li>
           <li>
-            <strong>LLM Testing and Evaluation:</strong> Create sophisticated tools for rigorous
-            testing of LLM outputs and behaviors.
+            <strong>Gen AI Testing and Evaluation:</strong> Create sophisticated tools for rigorous testing of LLM outputs and behaviors.
           </li>
           <li>
-            <strong>Open Source LLM Tools:</strong> Build and maintain open source libraries that
-            empower the wider AI community.
+            <strong>Open Source LLM Tools:</strong> Build and maintain open source libraries that empower the wider AI community.
           </li>
           <li>
-            <strong>LLM Security and Ethics:</strong> Work on solutions to identify and mitigate
-            potential risks in LLM deployments.
+            <strong>LLM Security and AI Ethics:</strong> Work on solutions to identify and mitigate potential risks in LLM deployments.
           </li>
         </ul>
 
@@ -111,38 +101,34 @@ export default function Careers(): JSX.Element {
 
         <h2>Open Positions</h2>
         <p>
-          While we don't have specific job titles listed, we're always interested in talking to
-          talented individuals who align with our mission and values. Some areas where we often need
-          expertise include:
+          We want to hear from you! If our missions, values, and problems sound good to you, 
+          we want to discuss how we might work together in the following areas:
         </p>
         <ul>
           <li>Software Engineering (Backend and Frontend)</li>
           <li>AI Research</li>
           <li>Technical Writing</li>
           <li>Developer Advocacy</li>
+          <li>...and Other Areas we don't yet know</li>
         </ul>
 
         <h2>How to Join Us</h2>
         <p>
-          If you're excited about the work we're doing and want to contribute, we encourage you to
-          reach out. Send your resume and a brief introduction to{' '}
-          <a href="mailto:careers@promptfoo.dev">careers@promptfoo.dev</a>.
+          If you're excited about the work we're doing and want to contribute, please reach out to us at:{' '}
+          <a href="mailto:careers@promptfoo.dev">careers@promptfoo.dev</a>
         </p>
-        <p>In your message:</p>
+
+        <p>We want to hear about...</p>
         <ul>
-          <li>Tell us about your experience with LLMs or related technologies</li>
-          <li>
-            Share any open source projects you've contributed to or personal projects you're proud
-            of
-          </li>
-          <li>
-            Explain why you're interested in Promptfoo and how you think you can contribute to our
-            mission
-          </li>
+          <li>Problems you solved using LLMs and Gen AI.</li>
+          <li>Challenges you encountered therein.</li>
+          <li>Any open source projects you've contributed to or personal projects you're proud of.</li>
+          <li>Your thoughts on PromptFoo and how you would contribute to our mission.</li>
         </ul>
+
         <p>
-          We look forward to hearing from you and exploring how we can work together to advance the
-          field of LLM technology and create tools that benefit the entire AI community.
+          We look forward to hearing from you and exploring how we can work together to advance the field of 
+          LLM technology and create tools that benefit the entire AI community.
         </p>
 
         <p>
