c6155effa08ab8e2bcc357e62b7e5514c4633696
Robusta Runner <aantny@gmail.com>
2024-07-24 14:41:00 +0300

    Update json.ts

--- src/util/json.ts
+++ src/util/json.ts
@@ -15,7 +15,7 @@ export function safeJsonStringify(value: any, prettyPrint: boolean = false): str
     value,
     (key, val) => {
       if (typeof val === 'object' && val !== null) {
-        if (cache.has(val)) return;
+        if (cache.has(val)) return val;
         cache.add(val);
       }
       return val;
