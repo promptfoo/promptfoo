f4ebf8d15d7cceb1f36a2456a526f77463a5efe5
Michael D'Angelo <michael.l.dangelo@gmail.com>
2024-07-24 19:03:38 -0400

    update copy

--- site/src/pages/careers.tsx
+++ site/src/pages/careers.tsx
@@ -30,7 +30,9 @@ export default function Careers(): JSX.Element {
         </p>
         <p>
           If you're interested in exploring opportunities, please reach out to{' '}
-          <a href="mailto:careers@promptfoo.dev">careers@promptfoo.dev</a>.
+          <a href="mailto:careers@promptfoo.dev">careers@promptfoo.dev</a>. Include a link to your
+          LinkedIn or GitHub profile in your email. Strong applicants will have already made a
+          meaningful contribution to our open source project.
         </p>
       </main>
     </Layout>
