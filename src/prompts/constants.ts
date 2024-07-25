25acbffa6e9cd0e1f4742dcad0d66d97c7f4dfec
Ben Asher <benasher44@gmail.com>
2024-07-25 10:29:20 -0700

    Support TS files in the rest of promptfoo

--- src/prompts/constants.ts
+++ src/prompts/constants.ts
@@ -1,11 +1,14 @@
 export const PROMPT_DELIMITER = process.env.PROMPTFOO_PROMPT_SEPARATOR || '---';
 export const VALID_FILE_EXTENSIONS = [
   '.cjs',
+  '.cts',
   '.js',
   '.json',
   '.jsonl',
   '.mjs',
+  '.mts',
   '.py',
+  '.ts',
   '.txt',
   '.yml',
   '.yaml',
