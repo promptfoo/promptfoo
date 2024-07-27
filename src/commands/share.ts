761eb9c2367665c6bef5b87355d0d3ad941e4da0
Michael <michael.l.dangelo@gmail.com>
2024-07-27 16:20:17 -0400

    chore(cli): add option to strip auth info from shared URLs (#1304)

--- src/commands/share.ts
+++ src/commands/share.ts
@@ -1,10 +1,17 @@
 import chalk from 'chalk';
 import { Command } from 'commander';
 import readline from 'readline';
+import { URL } from 'url';
 import logger from '../logger';
 import { createShareableUrl } from '../share';
 import telemetry from '../telemetry';
+import { ResultsFile } from '../types';
 import { readLatestResults, readResult, setupEnv } from '../util';
+
+async function createPublicUrl(results: ResultsFile, showAuth: boolean) {
+  const url = await createShareableUrl(results.results, results.config, showAuth);
+  logger.info(`View results: ${chalk.greenBright.bold(url)}`);
+}
 
 export function shareCommand(program: Command) {
   program
@@ -12,8 +19,16 @@ export function shareCommand(program: Command) {
     .description('Create a shareable URL of an eval (defaults to most recent)')
     .option('-y, --yes', 'Skip confirmation')
     .option('--env-file <path>', 'Path to .env file')
+    .option(
+      '--show-auth',
+      'Show username/password authentication information in the URL if exists',
+      false,
+    )
     .action(
-      async (evalId: string | undefined, cmdObj: { yes: boolean; envFile?: string } & Command) => {
+      async (
+        evalId: string | undefined,
+        cmdObj: { yes: boolean; envFile?: string; showAuth: boolean } & Command,
+      ) => {
         setupEnv(cmdObj.envFile);
         telemetry.maybeShowNotice();
         telemetry.record('command_used', {
@@ -36,13 +51,8 @@ export function shareCommand(program: Command) {
           }
         }
 
-        const createPublicUrl = async () => {
-          const url = await createShareableUrl(results.results, results.config);
-          logger.info(`View results: ${chalk.greenBright.bold(url)}`);
-        };
-
         if (cmdObj.yes || process.env.PROMPTFOO_DISABLE_SHARE_WARNING) {
-          await createPublicUrl();
+          await createPublicUrl(results, cmdObj.showAuth);
         } else {
           const reader = readline.createInterface({
             input: process.stdin,
@@ -62,7 +72,7 @@ export function shareCommand(program: Command) {
               }
               reader.close();
 
-              await createPublicUrl();
+              await createPublicUrl(results, cmdObj.showAuth);
             },
           );
         }
