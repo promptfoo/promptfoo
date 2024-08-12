32608e0891f62320303f230eb400a2751b8ba834
Ian Webster <ianw_github@ianww.com>
2024-08-11 23:29:38 -0700

    feat: `basic` strategy to represent raw payloads only

--- src/validators/redteam.ts
+++ src/validators/redteam.ts
@@ -157,6 +157,9 @@ export const RedteamConfigSchema = z
     const strategies = data.strategies
       ?.map((strategy) => {
         if (typeof strategy === 'string') {
+          if (strategy === 'basic') {
+            return [];
+          }
           return strategy === 'default'
             ? DEFAULT_STRATEGIES.map((id) => ({ id }))
             : { id: strategy };
