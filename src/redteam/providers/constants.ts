99e47e45045009bfd6b1956da4d6d2bd0d4b2e03
Michael D'Angelo <michael.l.dangelo@gmail.com>
2024-08-16 10:30:45 -0400

    move prompts

--- src/redteam/providers/constants.ts
+++ src/redteam/providers/constants.ts
@@ -1,9 +1,5 @@
 export const ATTACKER_MODEL = 'gpt-4o';
 
-export const NUM_ITERATIONS = process.env.PROMPTFOO_NUM_JAILBREAK_ITERATIONS
-  ? parseInt(process.env.PROMPTFOO_NUM_JAILBREAK_ITERATIONS, 10)
-  : 4;
-
 export const TEMPERATURE = process.env.PROMPTFOO_JAILBREAK_TEMPERATURE
   ? parseFloat(process.env.PROMPTFOO_JAILBREAK_TEMPERATURE)
   : 0.5;
