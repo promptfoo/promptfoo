15bb6d7a0c37768a28ec6ccf1ee8ef2d05f8e04e
Ian Webster <ianw_github@ianww.com>
2024-08-27 16:28:35 -0700

    chore: remove console log

--- src/config.ts
+++ src/config.ts
@@ -451,7 +451,6 @@ export async function resolveConfigs(
         scenario.tests = parsedScenarioTests;
       }
       invariant(typeof scenario === 'object', 'scenario must be an object');
-      console.error(`scenario: ${JSON.stringify(scenario)}`);
       const filteredTests = await filterTests(
         {
           ...(scenario ?? {}),
