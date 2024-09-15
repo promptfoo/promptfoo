7c7b26055a23edc446e056787d1ba501ddb1cdbb
Albert Lie <alberttri23@gmail.com>
2024-09-14 20:01:19 -0400

    fix linting issue

--- src/util/json.ts
+++ src/util/json.ts
@@ -35,14 +35,14 @@ export function extractJsonObjects(str: string): object[] {
 
   while (startIndex < str.length) {
     const openBracket = str.indexOf('{', startIndex);
-    if (openBracket === -1) break;
+    if (openBracket === -1) { break; }
 
     let bracketCount = 1;
     let closeBracket = openBracket + 1;
 
     while (bracketCount > 0 && closeBracket < str.length) {
-        if (str[closeBracket] === '{') bracketCount++;
-        if (str[closeBracket] === '}') bracketCount--;
+        if (str[closeBracket] === '{') { bracketCount++; }
+        if (str[closeBracket] === '}') { bracketCount--; }
         closeBracket++;
     }
 
@@ -56,7 +56,7 @@ export function extractJsonObjects(str: string): object[] {
             } catch (err) {
                 // Try including the next closing bracket
                 closeBracket = str.indexOf('}', closeBracket) + 1;
-                if (closeBracket === 0) break; // No more closing brackets
+                if (closeBracket === 0) { break; } // No more closing brackets
             }
             attempts++;
         }
