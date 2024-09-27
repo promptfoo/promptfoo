0ab49d423981ca439be32631853da276ec5934d3
Steven Klein <steve@promptfoo.dev>
2024-09-27 11:39:56 -0600

    remove log

--- src/util/index.ts
+++ src/util/index.ts
@@ -336,8 +336,7 @@ export function listPreviousResults(
 ): ResultLightweight[] {
   const db = getDb();
   const startTime = performance.now();
-  const test = db.select().from(evals).limit(1).all();
-  console.log({ test });
+
   const query = db
     .select({
       evalId: evals.id,
