1306548b717c9f24e456f5ce31587a1d1f95f5f3
Michael <michael.l.dangelo@gmail.com>
2024-10-01 18:05:26 -0400

    fix(redteam): read redteam config during redteam eval command (#1803)

--- .jest/setEnvVars.js
+++ .jest/setEnvVars.js
@@ -1,5 +1,6 @@
-process.env.OPENAI_API_KEY = 'foo';
-process.env.AZURE_OPENAI_API_KEY = 'foo';
-process.env.AZURE_OPENAI_API_HOST = 'azure.openai.host';
 process.env.ANTHROPIC_API_KEY = 'foo';
+process.env.AZURE_OPENAI_API_HOST = 'azure.openai.host';
+process.env.AZURE_OPENAI_API_KEY = 'foo';
 process.env.HF_API_TOKEN = 'foo';
+process.env.OPENAI_API_KEY = 'foo';
+delete process.env.PROMPTFOO_REMOTE_GENERATION_URL;
