159e6251f4147eabf7ca0e05da15487bfab7a881
Ian Webster <ianw_github@ianww.com>
2024-10-01 08:43:55 -0700

    fix: limits around share email prompt

--- src/share.ts
+++ src/share.ts
@@ -3,6 +3,7 @@ import chalk from 'chalk';
 import type { Response } from 'node-fetch';
 import { URL } from 'url';
 import { SHARE_API_BASE_URL, SHARE_VIEW_BASE_URL, DEFAULT_SHARE_VIEW_BASE_URL } from './constants';
+import { getEnvBool, isCI } from './envars';
 import { fetchWithProxy } from './fetch';
 import { getAuthor } from './globalConfig/accounts';
 import { getUserEmail, setUserEmail } from './globalConfig/accounts';
@@ -38,15 +39,17 @@ export async function createShareableUrl(
   config: Partial<UnifiedConfig>,
   showAuth: boolean = false,
 ): Promise<string | null> {
-  let email = getUserEmail();
-  if (!email) {
-    email = await input({
-      message: `${chalk.bold('Please enter your work email address')} (for managing shared URLs):`,
-      validate: (value) => {
-        return value.includes('@') || 'Please enter a valid email address';
-      },
-    });
-    setUserEmail(email);
+  if (process.stdout.isTTY && !isCI() && !getEnvBool('PROMPTFOO_DISABLE_SHARE_EMAIL_REQUEST')) {
+    let email = getUserEmail();
+    if (!email) {
+      email = await input({
+        message: `${chalk.bold('Please enter your work email address')} (for managing shared URLs):`,
+        validate: (value) => {
+          return value.includes('@') || 'Please enter a valid email address';
+        },
+      });
+      setUserEmail(email);
+    }
   }
 
   const sharedResults: SharedResults = {
