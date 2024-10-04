4608bd6f0fd2780acdee5fb063bba8fa80d269e3
Steven Klein <steve@promptfoo.dev>
2024-10-04 09:34:48 -0400

    Some nerds still want to run coverage :shrug:

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,8 @@ import type { Config } from 'jest';
 
 const config: Config = {
   collectCoverage: false,
+  coverageDirectory: '.coverage',
+  coverageProvider: 'v8',
   extensionsToTreatAsEsm: ['.ts'],
   modulePathIgnorePatterns: ['<rootDir>/dist', '<rootDir>/examples', '<rootDir>/node_modules'],
   setupFiles: ['<rootDir>/.jest/setEnvVars.js'],
