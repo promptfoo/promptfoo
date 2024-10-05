0a0b2f078cb3827c5b9293966803cda4b3b4e47c
Ian Webster <ianw_github@ianww.com>
2024-10-05 00:42:20 -0700

    site: fix contact dark theme.....

--- site/src/pages/contact.tsx
+++ site/src/pages/contact.tsx
@@ -1,5 +1,6 @@
 import Cal, { getCalApi } from '@calcom/embed-react';
 import React, { useEffect, useState } from 'react';
+import { useColorMode } from '@docusaurus/theme-common';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
@@ -62,144 +63,145 @@ function TabPanel(props: TabPanelProps) {
   );
 }
 
-export default function Contact(): JSX.Element {
+function Contact(): JSX.Element {
+  const isDarkTheme = useColorMode().colorMode === 'dark';
   const [tabValue, setTabValue] = useState(0);
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-  const [mode, setMode] = useState<'light' | 'dark'>(prefersDarkMode ? 'dark' : 'light');
 
   const theme = React.useMemo(
     () =>
       createTheme({
         palette: {
-          mode,
+          mode: isDarkTheme ? 'dark' : 'light',
         },
       }),
-    [mode],
+    [isDarkTheme],
   );
 
   const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
     setTabValue(newValue);
   };
 
-  const toggleColorMode = () => {
-    setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-  };
-
   return (
     <ThemeProvider theme={theme}>
-      <Layout title="Contact Us" description="Schedule a meeting with the promptfoo team">
-        <Container maxWidth="md">
-          <Box
-            sx={{
-              my: 4,
-              textAlign: 'center',
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-            }}
-          >
-            <Typography variant="h3" gutterBottom>
-              Chat with us
-            </Typography>
-          </Box>
+      <Container maxWidth="md">
+        <Box
+          sx={{
+            my: 4,
+            textAlign: 'center',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <Typography variant="h3" gutterBottom>
+            Chat with us
+          </Typography>
+        </Box>
 
-          <Box my={4}>
-            <Typography variant="body1" gutterBottom>
-              <strong>Ways to get in touch:</strong>
-            </Typography>
-            <Typography variant="body1" gutterBottom>
-              💬 Join our{' '}
-              <Link href="https://discord.gg/gHPS9jjfbs" target="_blank">
-                Discord
-              </Link>
-            </Typography>
-            <Typography variant="body1" gutterBottom>
-              🐙 Visit our{' '}
-              <Link href="https://github.com/promptfoo/promptfoo" target="_blank">
-                GitHub
-              </Link>
-            </Typography>
-            <Typography variant="body1" gutterBottom>
-              ✉️ Email us at{' '}
-              <Link href="mailto:inquiries@promptfoo.dev">inquiries@promptfoo.dev</Link>
-            </Typography>
-            <Typography variant="body1" gutterBottom>
-              📅 Or book a time below
-            </Typography>
-          </Box>
+        <Box my={4}>
+          <Typography variant="body1" gutterBottom>
+            <strong>Ways to get in touch:</strong>
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            💬 Join our{' '}
+            <Link href="https://discord.gg/gHPS9jjfbs" target="_blank">
+              Discord
+            </Link>
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            🐙 Visit our{' '}
+            <Link href="https://github.com/promptfoo/promptfoo" target="_blank">
+              GitHub
+            </Link>
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            ✉️ Email us at{' '}
+            <Link href="mailto:inquiries@promptfoo.dev">inquiries@promptfoo.dev</Link>
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            📅 Or book a time below
+          </Typography>
+        </Box>
 
-          <Box sx={{ borderBottom: 1, borderColor: 'divider', mb: 2 }}>
-            <Tabs value={tabValue} onChange={handleTabChange} aria-label="contact tabs">
-              <Tab label={<strong>Schedule a Meeting</strong>} />
-              <Tab label={<strong>Contact Form</strong>} />
-            </Tabs>
-          </Box>
-          <Box mb={8}>
-            <TabPanel value={tabValue} index={0}>
-              <Calendar />
-            </TabPanel>
-            <TabPanel value={tabValue} index={1}>
-              <form action="https://submit-form.com/ghriv7voL" className={styles.contactForm}>
-                <TextField
-                  fullWidth
-                  id="name"
-                  name="name"
-                  label="Your Name"
-                  variant="outlined"
-                  required
-                  margin="normal"
-                />
-                <TextField
-                  fullWidth
-                  id="email"
-                  name="email"
-                  label="Work Email"
-                  type="email"
-                  variant="outlined"
-                  required
-                  margin="normal"
-                />
-                <TextField
-                  fullWidth
-                  id="company"
-                  name="company"
-                  label="Company Name"
-                  variant="outlined"
-                  required
-                  margin="normal"
-                />
-                <FormControl fullWidth margin="normal" variant="outlined" required>
-                  <InputLabel id="interested-in-label">Interested In</InputLabel>
-                  <Select
-                    labelId="interested-in-label"
-                    id="interested-in"
-                    name="interested-in"
-                    label="Interested In"
-                  >
-                    <MenuItem value="Security/red teaming">Security/red teaming</MenuItem>
-                    <MenuItem value="Evaluations">Evaluations</MenuItem>
-                    <MenuItem value="Other">Other</MenuItem>
-                  </Select>
-                </FormControl>
-                <TextField
-                  fullWidth
-                  id="meeting-about"
-                  name="meeting-about"
-                  label="I want to discuss..."
-                  multiline
-                  rows={4}
-                  variant="outlined"
-                  required
-                  margin="normal"
-                />
-                <Button type="submit" variant="contained" color="primary" sx={{ mt: 6 }}>
-                  Send
-                </Button>
-              </form>
-            </TabPanel>
-          </Box>
-        </Container>
-      </Layout>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider', mb: 2 }}>
+          <Tabs value={tabValue} onChange={handleTabChange} aria-label="contact tabs">
+            <Tab label={<strong>Schedule a Meeting</strong>} />
+            <Tab label={<strong>Contact Form</strong>} />
+          </Tabs>
+        </Box>
+        <Box mb={8}>
+          <TabPanel value={tabValue} index={0}>
+            <Calendar />
+          </TabPanel>
+          <TabPanel value={tabValue} index={1}>
+            <form action="https://submit-form.com/ghriv7voL" className={styles.contactForm}>
+              <TextField
+                fullWidth
+                id="name"
+                name="name"
+                label="Your Name"
+                variant="outlined"
+                required
+                margin="normal"
+              />
+              <TextField
+                fullWidth
+                id="email"
+                name="email"
+                label="Work Email"
+                type="email"
+                variant="outlined"
+                required
+                margin="normal"
+              />
+              <TextField
+                fullWidth
+                id="company"
+                name="company"
+                label="Company Name"
+                variant="outlined"
+                required
+                margin="normal"
+              />
+              <FormControl fullWidth margin="normal" variant="outlined" required>
+                <InputLabel id="interested-in-label">Interested In</InputLabel>
+                <Select
+                  labelId="interested-in-label"
+                  id="interested-in"
+                  name="interested-in"
+                  label="Interested In"
+                >
+                  <MenuItem value="Security/red teaming">Security/red teaming</MenuItem>
+                  <MenuItem value="Evaluations">Evaluations</MenuItem>
+                  <MenuItem value="Other">Other</MenuItem>
+                </Select>
+              </FormControl>
+              <TextField
+                fullWidth
+                id="meeting-about"
+                name="meeting-about"
+                label="I want to discuss..."
+                multiline
+                rows={4}
+                variant="outlined"
+                required
+                margin="normal"
+              />
+              <Button type="submit" variant="contained" color="primary" sx={{ mt: 6 }}>
+                Send
+              </Button>
+            </form>
+          </TabPanel>
+        </Box>
+      </Container>
     </ThemeProvider>
+  );
+}
+
+export default function Page(): JSX.Element {
+  return (
+    <Layout title="Contact Us" description="Schedule a meeting with the promptfoo team">
+      <Contact />
+    </Layout>
   );
 }
