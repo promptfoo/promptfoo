0d03d8039393e2534ee7f9f3e9c20a66f95da965
Michael D'Angelo <michael.l.dangelo@gmail.com>
2024-10-15 10:06:20 -0700

    protect redteam setup behind auth

--- src/app/src/App.tsx
+++ src/app/src/App.tsx
@@ -2,6 +2,8 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   Navigate,
+  useLocation,
+  Outlet,
   Route,
   RouterProvider,
 } from 'react-router-dom';
@@ -18,6 +20,40 @@ import ReportPage from './pages/report/page';
 
 const DEFAULT_ROUTE = import.meta.env.VITE_PUBLIC_HOSTED ? '/setup' : '/eval';
 const basename = import.meta.env.VITE_PUBLIC_BASENAME || '';
+
+const useAuth = () => {
+  return {
+    user: true,
+    isLoading: false,
+    organization: true,
+  };
+};
+
+export const RequireAuth: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const { user, isLoading, organization } = useAuth();
+  const location = useLocation();
+
+  if (isLoading) {
+    return <div></div>;
+  }
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  if (!organization) {
+    return <Navigate to="/organization/new" state={{ from: location }} replace />;
+  }
+
+  return <>{children}</>;
+};
+
+const ProtectedRoutes = () => (
+  <RequireAuth>
+    <Outlet />
+  </RequireAuth>
+);
+
 const router = createBrowserRouter(
   createRoutesFromElements([
     <Route path="/" element={<PageShell />}>
@@ -29,12 +65,12 @@ const router = createBrowserRouter(
       <Route path="/progress" element={<ProgressPage />} />
       <Route path="/datasets" element={<DatasetsPage />} />
       <Route path="/report" element={<ReportPage />} />
-      {import.meta.env.VITE_PROMPTFOO_EXPERIMENTAL && (
-        <>
-          <Route path="/redteam/setup" element={<RedteamSetupPage />} />
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/redteam/setup" element={<RedteamSetupPage />} />
+        {import.meta.env.VITE_PROMPTFOO_EXPERIMENTAL && (
           <Route path="/dashboard" element={<DashboardPage />} />
-        </>
-      )}
+        )}
+      </Route>
     </Route>,
   ]),
   { basename },
