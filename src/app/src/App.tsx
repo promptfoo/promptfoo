e1d9e13439f576ac94fd5a9f4f33390b4758358e
Michael D'Angelo <michael.l.dangelo@gmail.com>
2024-10-15 11:01:07 -0700

    revert

--- src/app/src/App.tsx
+++ src/app/src/App.tsx
@@ -2,57 +2,21 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   Navigate,
-  useLocation,
-  Outlet,
   Route,
   RouterProvider,
 } from 'react-router-dom';
 import PageShell from './components/PageShell';
+import RedteamDashboardPage from './pages/redteam/dashboard/page';
 import DatasetsPage from './pages/datasets/page';
 import EvalCreatorPage from './pages/eval-creator/page';
 import EvalPage from './pages/eval/page';
 import ProgressPage from './pages/progress/page';
 import PromptsPage from './pages/prompts/page';
-import RedteamDashboardPage from './pages/redteam/dashboard/page';
 import RedteamSetupPage from './pages/redteam/setup/page';
 import ReportPage from './pages/report/page';
 
 const DEFAULT_ROUTE = import.meta.env.VITE_PUBLIC_HOSTED ? '/setup' : '/eval';
 const basename = import.meta.env.VITE_PUBLIC_BASENAME || '';
-
-const useAuth = () => {
-  return {
-    user: true,
-    isLoading: false,
-    organization: true,
-  };
-};
-
-export const RequireAuth: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const { user, isLoading, organization } = useAuth();
-  const location = useLocation();
-
-  if (isLoading) {
-    return <div></div>;
-  }
-
-  if (!user) {
-    return <Navigate to="/login" state={{ from: location }} replace />;
-  }
-
-  if (!organization) {
-    return <Navigate to="/organization/new" state={{ from: location }} replace />;
-  }
-
-  return <>{children}</>;
-};
-
-const ProtectedRoutes = () => (
-  <RequireAuth>
-    <Outlet />
-  </RequireAuth>
-);
-
 const router = createBrowserRouter(
   createRoutesFromElements([
     <Route path="/" element={<PageShell />}>
@@ -64,10 +28,12 @@ const router = createBrowserRouter(
       <Route path="/progress" element={<ProgressPage />} />
       <Route path="/datasets" element={<DatasetsPage />} />
       <Route path="/report" element={<ReportPage />} />
-      <Route element={<ProtectedRoutes />}>
-        <Route path="/redteam/dashboard" element={<RedteamDashboardPage />} />
-        <Route path="/redteam/setup" element={<RedteamSetupPage />} />
-      </Route>
+      {import.meta.env.VITE_PROMPTFOO_EXPERIMENTAL && (
+        <>
+          <Route path="/dashboard" element={<RedteamDashboardPage />} />
+          <Route path="/redteam/setup" element={<RedteamSetupPage />} />
+        </>
+      )}
     </Route>,
   ]),
   { basename },
