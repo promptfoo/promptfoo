9ea54c79f8078cd8ae392f6945e572285c4b41bd
Michael D'Angelo <michael.l.dangelo@gmail.com>
2024-10-19 19:41:22 -0700

    disable

--- src/app/src/hooks/useTelemetry.tsx
+++ src/app/src/hooks/useTelemetry.tsx
@@ -1,23 +1,35 @@
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 import { callApi } from '@app/utils/api';
+import type { EventProperties, TelemetryEventTypes } from '@promptfoo/telemetry';
 
 export function useTelemetry() {
-  const recordEvent = useCallback(async (eventName: string, properties: Record<string, any>) => {
-    try {
-      await callApi('/telemetry', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          eventName,
-          properties,
-        }),
-      });
-    } catch (error) {
-      console.error('Failed to record telemetry event:', error);
-    }
+  const isTelemetryDisabled = useMemo(() => {
+    return;
   }, []);
+
+  const recordEvent = useCallback(
+    async (eventName: TelemetryEventTypes, properties: EventProperties) => {
+      if (!['0', 'false'].includes(import.meta.env.VITE_PROMPTFOO_DISABLE_TELEMETRY)) {
+        return;
+      }
+
+      try {
+        await callApi('/telemetry', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            event: eventName,
+            properties,
+          }),
+        });
+      } catch (error) {
+        console.error('Failed to record telemetry event:', error);
+      }
+    },
+    [isTelemetryDisabled],
+  );
 
   return { recordEvent };
 }
