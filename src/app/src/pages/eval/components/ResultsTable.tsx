135ccbeaee308abbcc0af447b6be850c41588d04
Steven Klein <steve@promptfoo.dev>
2024-10-24 09:43:55 -0600

    chore: move snackbar out of component so it can be re-used

--- src/app/src/pages/eval/components/ResultsTable.tsx
+++ src/app/src/pages/eval/components/ResultsTable.tsx
@@ -8,7 +8,7 @@ import {
 import * as React from 'react';
 import ReactMarkdown from 'react-markdown';
 import { Link } from 'react-router-dom';
-import { useToast } from '@app/hooks/useToast';
+import { useToast } from '@app/contexts/ToastContext';
 import type {
   EvaluateTableRow,
   EvaluateTableOutput,
