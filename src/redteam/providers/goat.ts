694fb2e36cb36f24d84664b2c94e9a5b685768bd
Michael D'Angelo <michael.l.dangelo@gmail.com>
2024-10-28 17:56:47 -0400

    use inject var

--- src/redteam/providers/goat.ts
+++ src/redteam/providers/goat.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk';
 import invariant from 'tiny-invariant';
 import logger from '../../logger';
 import { HttpProvider } from '../../providers/http';
@@ -15,13 +16,16 @@ export default class GoatProvider implements ApiProvider {
   private redteamProvider: HttpProvider;
   private targetProvider: ApiProvider | undefined;
   private maxTurns: number;
-
-  constructor(options: ProviderOptions & { maxTurns?: number } = {}) {
+  private readonly injectVar: string;
+  constructor(options: ProviderOptions & { maxTurns?: number; injectVar?: string } = {}) {
     if (neverGenerateRemote()) {
       throw new Error(`GOAT strategy requires remote grading to be enabled`);
     }
     logger.warn(`REMOTE_GENERATION_URL: ${REMOTE_GENERATION_URL}`);
-    this.maxTurns = options.maxTurns || 10;
+    invariant(typeof options.injectVar === 'string', 'Expected injectVar to be set');
+    this.injectVar = options.injectVar;
+    logger.warn(`injectVar: ${this.injectVar}`);
+    this.maxTurns = options.maxTurns || 2;
     this.redteamProvider = new HttpProvider(REMOTE_GENERATION_URL, {
       ...options,
       config: {
@@ -53,6 +57,11 @@ export default class GoatProvider implements ApiProvider {
     let response: ProviderResponse | undefined = undefined;
     this.targetProvider = context?.originalProvider;
     invariant(this.targetProvider, 'Expected originalProvider to be set');
+
+    logger.warn(`Starting GOAT attack with goal:\n${prompt}`);
+    logger.warn(chalk.blue(`raw prompt:\n${context?.prompt?.raw}`));
+    logger.warn(`vars:\n${JSON.stringify(context?.vars, null, 2)}`);
+
     for (let turn = 0; turn < this.maxTurns; turn++) {
       response = await this.redteamProvider.callApi(prompt, {
         ...context,
