10f611350f9cab06437e3b7ca0b7ca56659aebdc
sklein12 <steve@promptfoo.dev>
2024-11-12 15:03:34 -0700

    fix: Config for all strategies (#2126)

--- src/redteam/strategies/crescendo.ts
+++ src/redteam/strategies/crescendo.ts
@@ -1,12 +1,17 @@
 import type { TestCase } from '../../types';
 
-export function addCrescendo(testCases: TestCase[], injectVar: string): TestCase[] {
+export function addCrescendo(
+  testCases: TestCase[],
+  injectVar: string,
+  config: Record<string, any>,
+): TestCase[] {
   return testCases.map((testCase) => ({
     ...testCase,
     provider: {
       id: 'promptfoo:redteam:crescendo',
       config: {
         injectVar,
+        ...config,
       },
     },
     assert: testCase.assert?.map((assertion) => ({
--- src/redteam/strategies/iterative.ts
+++ src/redteam/strategies/iterative.ts
@@ -4,6 +4,7 @@ export function addIterativeJailbreaks(
   testCases: TestCase[],
   injectVar: string,
   strategy: 'iterative' | 'iterative:tree' = 'iterative',
+  config: Record<string, any>,
 ): TestCase[] {
   const providerName =
     strategy === 'iterative' ? 'promptfoo:redteam:iterative' : 'promptfoo:redteam:iterative:tree';
@@ -13,6 +14,7 @@ export function addIterativeJailbreaks(
       id: providerName,
       config: {
         injectVar,
+        ...config,
       },
     },
     assert: testCase.assert?.map((assertion) => ({
