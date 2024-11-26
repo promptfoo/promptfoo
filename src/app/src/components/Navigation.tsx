2d22adf113dfdafff1cd1bea0fc3815aae69186d
Ian Webster <ianw_github@ianww.com>
2024-11-25 23:04:14 -0800

    chore: progress -> history

--- src/app/src/components/Navigation.tsx
+++ src/app/src/components/Navigation.tsx
@@ -132,7 +132,7 @@ export default function Navigation({
             <NavLink href="/eval" label="Evals" />
             <NavLink href="/prompts" label="Prompts" />
             <NavLink href="/datasets" label="Datasets" />
-            <NavLink href="/progress" label="Progress" />
+            <NavLink href="/progress" label="History" />
             <NavLink href="https://promptfoo.dev/careers" label="Careers" />
           </NavSection>
           <NavSection>
