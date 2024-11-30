203c5a2fa4e3eb5ead492a2278811684e8983292
Evan Harris <echarris@smcm.edu>
2024-11-30 11:30:04 -0500

    fix: only show action buttons for the column being hovered over
    Signed-off-by: Evan Harris <echarris@smcm.edu>

--- src/app/src/pages/eval/components/ResultsTable.tsx
+++ src/app/src/pages/eval/components/ResultsTable.tsx
@@ -589,6 +589,7 @@ function ResultsTable({
             cell: (info: CellContext<EvaluateTableRow, EvaluateTableOutput>) => {
               const output = getOutput(info.row.index, idx);
               return output ? (
+                // hmm
                 <EvalOutputCell
                   output={output}
                   maxTextLength={maxTextLength}
@@ -781,7 +782,7 @@ function ResultsTable({
                       }}
                       className={`${isMetadataCol ? 'variable' : ''} ${
                         shouldDrawRowBorder ? 'first-prompt-row' : ''
-                      } ${shouldDrawColBorder ? 'first-prompt-col' : ''}`}
+                      } ${shouldDrawColBorder ? 'first-prompt-col' : 'second-prompt-column'}`}
                     >
                       {cellContent}
                     </td>
