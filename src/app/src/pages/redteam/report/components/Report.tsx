a6cfc8fa4d663caf797382e0e288da738fd873c4
Ian Webster <ianw_github@ianww.com>
2024-11-30 09:56:22 -0800

    fix: handle missing `metrics` property

--- src/app/src/pages/redteam/report/components/Report.tsx
+++ src/app/src/pages/redteam/report/components/Report.tsx
@@ -333,7 +333,7 @@ const App: React.FC = () => {
                   <>
                     <strong>Depth:</strong>{' '}
                     {(
-                      selectedPrompt.metrics?.tokenUsage.numRequests || tableData.length
+                      selectedPrompt?.metrics?.tokenUsage?.numRequests || tableData.length
                     ).toLocaleString()}{' '}
                     probes
                   </>
