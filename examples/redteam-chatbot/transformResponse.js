dcd70cf9f610e8d0536bb6a95f7953c6a352a4d5
Michael D'Angelo <michael.l.dangelo@gmail.com>
2024-12-04 18:32:38 -0800

    remove comment

--- examples/redteam-chatbot/transformResponse.js
+++ examples/redteam-chatbot/transformResponse.js
@@ -1,5 +1,4 @@
 module.exports = (json, text) => {
-  // console.log(`response transform: ${JSON.stringify(json, null, 2)}`);
   if (!json.chat_history || !Array.isArray(json.chat_history)) {
     throw new Error(`No chat history found in response: ${text}`);
   }
