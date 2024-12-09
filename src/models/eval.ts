e78993f2e81d134680ba80fe48fa2eb979c43c2a
GICodeWarrior <GICodeWarrior@gmail.com>
2024-12-08 17:18:48 -0800

    Attempt to fix metrics after share to self-hosted

--- src/models/eval.ts
+++ src/models/eval.ts
@@ -170,6 +170,7 @@ export default class Eval {
           description: config.description,
           config,
           results: {},
+          prompts: renderedPrompts,
         })
         .run();
 
