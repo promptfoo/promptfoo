d213664f07728b9568739794f5fc66d414444461
Michael D'Angelo <michael.l.dangelo@gmail.com>
2024-12-10 17:16:10 -0500

    update examples

--- src/providers/bedrock.ts
+++ src/providers/bedrock.ts
@@ -676,7 +676,9 @@ export const BEDROCK_MODEL = {
   },
   MISTRAL: {
     params: (config: BedrockMistralGenerationOptions, prompt: string, stop: string[]) => {
-      const params: any = { prompt, stop };
+      const params: any = {
+        prompt: `<s>[INST] ${prompt} [/INST]`,
+      };
       addConfigParam(
         params,
         'max_tokens',
@@ -689,10 +691,13 @@ export const BEDROCK_MODEL = {
         'temperature',
         config?.temperature,
         getEnvFloat('MISTRAL_TEMPERATURE'),
-        0,
+        0.5,
       );
-      addConfigParam(params, 'top_p', config?.top_p, getEnvFloat('MISTRAL_TOP_P'), 1);
-      addConfigParam(params, 'top_k', config?.top_k, getEnvFloat('MISTRAL_TOP_K'), 0);
+      addConfigParam(params, 'top_p', config?.top_p, getEnvFloat('MISTRAL_TOP_P'), 0.9);
+      addConfigParam(params, 'top_k', config?.top_k, getEnvFloat('MISTRAL_TOP_K'), 50);
+      if (stop && stop.length > 0) {
+        params.stop_sequences = stop;
+      }
       return params;
     },
     output: (responseJson: any) => responseJson?.outputs[0]?.text,
