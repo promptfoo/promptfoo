5f34947e89edfbe01beb4e8babd2338217e90c26
Ian Webster <ianw_github@ianww.com>
2024-12-12 09:41:57 -0700

    fix(webui): use injectVar in redteam report view (#2366)

--- src/app/src/pages/redteam/report/components/Report.tsx
+++ src/app/src/pages/redteam/report/components/Report.tsx
@@ -104,10 +104,8 @@ const App: React.FC = () => {
         if (!failures[pluginId]) {
           failures[pluginId] = [];
         }
-        // Backwards compatibility for old evals that used 'query' instead of 'prompt'. 2024-12-12
         const injectVar = evalData.config.redteam?.injectVar ?? 'prompt';
-        const injectVarValue =
-          result.vars[injectVar]?.toString() || result.vars['query']?.toString();
+        const injectVarValue = result.vars[injectVar]?.toString();
         failures[pluginId].push({
           prompt: injectVarValue || result.prompt.raw,
           output: result.response?.output,
