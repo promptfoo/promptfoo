1d185470547c60e68ef42b4e8bd903deb79360ac
Ian Webster <ianw_github@ianww.com>
2024-12-18 17:37:22 -0800

    fix: safeJsonStringify always returns a string

--- src/util/json.ts
+++ src/util/json.ts
@@ -10,21 +10,23 @@ export function isValidJson(str: string): boolean {
 }
 
 export function safeJsonStringify(value: any, prettyPrint: boolean = false): string {
-  // Prevent circular references
+  // Prevent circular references and always return a string
   const cache = new Set();
   const space = prettyPrint ? 2 : undefined;
-  return JSON.stringify(
-    value,
-    (key, val) => {
-      if (typeof val === 'object' && val !== null) {
-        if (cache.has(val)) {
-          return;
+  return (
+    (JSON.stringify(
+      value,
+      (key, val) => {
+        if (typeof val === 'object' && val !== null) {
+          if (cache.has(val)) {
+            return;
+          }
+          cache.add(val);
         }
-        cache.add(val);
-      }
-      return val;
-    },
-    space,
+        return val;
+      },
+      space,
+    ) as string | undefined) ?? ''
   );
 }
 
