f1ded80e5edf8abc3a01daa7f14a65a9d92310c9
Michael D'Angelo <michael.l.dangelo@gmail.com>
2024-12-27 07:16:36 -1000

    add missing import

--- src/app/src/pages/redteam/setup/components/StrategyConfigDialog.tsx
+++ src/app/src/pages/redteam/setup/components/StrategyConfigDialog.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Chip from '@mui/material/Chip';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
