1de25ad3804217547085841be7ae95a073b149ab
vedantr <vedantr@users.noreply.github.com>
2025-01-07 14:26:20 -0800

    chore: minor change to python assert example and revert provider to gpt4 mini (#2564)

--- examples/python-assert-external/assert_with_config.py
+++ examples/python-assert-external/assert_with_config.py
@@ -4,7 +4,7 @@ def get_assert(output, context):
     print("Context", context)
 
     test_configuration = context.get("config", {})
-    canonical_fruit_list = test_configuration.get("southAmericanFruits", [])
+    canonical_fruit_list = test_configuration.get("fruitList", [])
     assert_passed = False
 
     for fruit in canonical_fruit_list:
