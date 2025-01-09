2899f55c6b614d56cd4ed6bee8c546f64329f7b9
Michael <michael.l.dangelo@gmail.com>
2025-01-09 00:11:15 -0800

    chore(redteam): store attack prompt instead of rendered prompt in metadata (#2602)

--- src/redteam/providers/iterativeTree.ts
+++ src/redteam/providers/iterativeTree.ts
@@ -553,6 +553,7 @@ export async function runRedteamConversation({
         if (score > maxScore) {
           maxScore = score;
           bestResponse = targetResponse.output;
+          bestNode.prompt = newInjectVar;
           logger.debug(`[Depth ${depth}, Attempt ${attempts}] New max score: ${maxScore}`);
         } else if (score > bestScore) {
           bestScore = score;
@@ -616,7 +617,7 @@ export async function runRedteamConversation({
             output: targetResponse.output,
             metadata: {
               highestScore: maxScore,
-              redteamFinalPrompt: targetPrompt,
+              redteamFinalPrompt: bestNode.prompt,
               stoppingReason,
               attempts,
               treeOutputs: JSON.stringify(treeOutputs, null, 2),
@@ -644,7 +645,7 @@ export async function runRedteamConversation({
             output: bestResponse,
             metadata: {
               highestScore: maxScore,
-              redteamFinalPrompt: targetPrompt,
+              redteamFinalPrompt: bestNode.prompt,
               stoppingReason,
               attempts,
               treeOutputs: JSON.stringify(treeOutputs, null, 2),
@@ -673,7 +674,7 @@ export async function runRedteamConversation({
             output: bestResponse,
             metadata: {
               highestScore: maxScore,
-              redteamFinalPrompt: targetPrompt,
+              redteamFinalPrompt: bestNode.prompt,
               stoppingReason,
               attempts,
               treeOutputs: JSON.stringify(treeOutputs, null, 2),
@@ -762,7 +763,7 @@ export async function runRedteamConversation({
     output: bestResponse,
     metadata: {
       highestScore: maxScore,
-      redteamFinalPrompt: finalTargetPrompt,
+      redteamFinalPrompt: bestNode.prompt,
       stoppingReason,
       attempts,
       treeOutputs: JSON.stringify(treeOutputs, null, 2),
