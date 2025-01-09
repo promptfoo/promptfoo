c0f3d93ddc87c2a0a7e7019311bae793737a4158
MrFlounder <k.zanggs@gmail.com>
2025-01-08 21:46:20 -0800

    make the hackbetter

--- src/util/convertEvalResultsToTable.ts
+++ src/util/convertEvalResultsToTable.ts
@@ -76,7 +76,19 @@ export function convertResultsToTable(eval_: ResultsFile): EvaluateTable {
       test: result.testCase,
     };
 
-    result.vars['prompt'] = result.metadata?.redteamFinalPrompt || result.vars['prompt'];
+    if (result.vars && result.metadata?.redteamFinalPrompt) {
+      const varKeys = Object.keys(result.vars);
+      if (varKeys.length === 1 && varKeys[0] !== 'harmCategory') {
+        result.vars[varKeys[0]] = result.metadata.redteamFinalPrompt;
+      } else if (varKeys.length > 1) {
+        // NOTE: This is a hack. We should use config.redteam.injectVar to determine which key to update but we don't have access to the config here
+        const targetKeys = ['prompt', 'query', 'question'];
+        const keyToUpdate = targetKeys.find((key) => result.vars[key]);
+        if (keyToUpdate) {
+          result.vars[keyToUpdate] = result.metadata.redteamFinalPrompt;
+        }
+      }
+    }
     varValuesForRow.set(result.testIdx, result.vars as Record<string, string>);
     rowMap[result.testIdx] = row;
 
