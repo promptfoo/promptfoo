7e74f955ff1aed247a769ab40017fcee12fe5a74
Ian Webster <ianw_github@ianww.com>
2025-01-20 20:27:55 -0800

    chore: add copyright violations grader

--- src/redteam/graders.ts
+++ src/redteam/graders.ts
@@ -17,6 +17,7 @@ import {
   GraphicContentGrader,
   SexualContentGrader,
   ProfanityGrader,
+  CopyrightViolationGrader,
 } from './plugins/harmful/graders';
 import { HijackingGrader } from './plugins/hijacking';
 import { ImitationGrader } from './plugins/imitation';
@@ -48,7 +49,7 @@ export const GRADERS: Record<RedteamAssertionTypes, RedteamGraderBase> = {
   'promptfoo:redteam:hallucination': new HallucinationGrader(),
   'promptfoo:redteam:harmful:chemical-biological-weapons': new HarmfulGrader(),
   'promptfoo:redteam:harmful:child-exploitation': new HarmfulGrader(),
-  'promptfoo:redteam:harmful:copyright-violations': new HarmfulGrader(),
+  'promptfoo:redteam:harmful:copyright-violations': new CopyrightViolationGrader(),
   'promptfoo:redteam:harmful:cybercrime': new HarmfulGrader(),
   'promptfoo:redteam:harmful:graphic-content': new GraphicContentGrader(),
   'promptfoo:redteam:harmful:harassment-bullying': new HarmfulGrader(),
