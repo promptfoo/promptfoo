d72ce93f82ec9737288732d244bbc48742644b25
Michael D'Angelo <michael.l.dangelo@gmail.com>
2025-02-02 11:58:51 -0500

    refactor

--- src/providers/openai/index.ts
+++ src/providers/openai/index.ts
@@ -1,16 +1,12 @@
-import type { Cache } from 'cache-manager';
-import OpenAI from 'openai';
-import { fetchWithCache, getCache, isCacheEnabled } from '../../cache';
+import type OpenAI from 'openai';
+import { fetchWithCache } from '../../cache';
 import { getEnvString, getEnvFloat, getEnvInt } from '../../envars';
 import logger from '../../logger';
 import type {
-  ApiModerationProvider,
   ApiProvider,
   CallApiContextParams,
   CallApiOptionsParams,
-  ModerationFlag,
   ProviderEmbeddingResponse,
-  ProviderModerationResponse,
   ProviderResponse,
 } from '../../types';
 import type { EnvOverrides } from '../../types/env';
@@ -687,101 +683,6 @@ export class OpenAiChatCompletionProvider extends OpenAiGenericProvider {
   }
 }
 
-export class OpenAiModerationProvider
-  extends OpenAiGenericProvider
-  implements ApiModerationProvider
-{
-  async callModerationApi(
-    userPrompt: string, // userPrompt is not supported by OpenAI moderation API
-    assistantResponse: string,
-  ): Promise<ProviderModerationResponse> {
-    if (!this.getApiKey()) {
-      throw new Error(
-        'OpenAI API key is not set. Set the OPENAI_API_KEY environment variable or add `apiKey` to the provider config.',
-      );
-    }
-
-    const openai = new OpenAI({
-      apiKey: this.getApiKey(),
-      organization: this.getOrganization(),
-      baseURL: this.getApiUrl(),
-      maxRetries: 3,
-      timeout: REQUEST_TIMEOUT_MS,
-    });
-
-    let cache: Cache | undefined;
-    let cacheKey: string | undefined;
-    if (isCacheEnabled()) {
-      cache = await getCache();
-      cacheKey = `openai:${this.modelName}:${JSON.stringify(
-        this.config,
-      )}:${userPrompt}:${assistantResponse}`;
-
-      // Try to get the cached response
-      const cachedResponse = await cache.get(cacheKey);
-
-      if (cachedResponse) {
-        logger.debug(`Returning cached response for ${userPrompt}: ${cachedResponse}`);
-        return JSON.parse(cachedResponse as string);
-      }
-    }
-
-    logger.debug(
-      `Calling OpenAI moderation API: prompt [${userPrompt}] assistant [${assistantResponse}]`,
-    );
-    let moderation: OpenAI.Moderations.ModerationCreateResponse | undefined;
-    try {
-      moderation = await openai.moderations.create({
-        model: this.modelName,
-        input: assistantResponse,
-      });
-    } catch (err) {
-      logger.error(`API call error: ${String(err)}`);
-      return {
-        error: `API call error: ${String(err)}`,
-      };
-    }
-
-    logger.debug(`\tOpenAI moderation API response: ${JSON.stringify(moderation)}`);
-    try {
-      const { results } = moderation;
-
-      const flags: ModerationFlag[] = [];
-      if (!results) {
-        throw new Error('API response error: no results');
-      }
-
-      if (cache && cacheKey) {
-        await cache.set(cacheKey, JSON.stringify(moderation));
-      }
-
-      if (results.length === 0) {
-        return { flags };
-      }
-
-      for (const result of results) {
-        if (result.flagged) {
-          for (const [category, flagged] of Object.entries(result.categories)) {
-            if (flagged) {
-              flags.push({
-                code: category,
-                description: category,
-                confidence:
-                  result.category_scores[category as keyof OpenAI.Moderation.CategoryScores],
-              });
-            }
-          }
-        }
-      }
-      return { flags };
-    } catch (err) {
-      return {
-        error: `API response error: ${String(err)}: ${JSON.stringify(moderation)}`,
-      };
-    }
-  }
-}
-
 export const DefaultEmbeddingProvider = new OpenAiEmbeddingProvider('text-embedding-3-large');
 export const DefaultGradingProvider = new OpenAiChatCompletionProvider('gpt-4o-2024-05-13');
 export const DefaultGradingJsonProvider = new OpenAiChatCompletionProvider('gpt-4o-2024-05-13', {
@@ -790,4 +691,3 @@ export const DefaultGradingJsonProvider = new OpenAiChatCompletionProvider('gpt-
   },
 });
 export const DefaultSuggestionsProvider = new OpenAiChatCompletionProvider('gpt-4o-2024-05-13');
-export const DefaultModerationProvider = new OpenAiModerationProvider('omni-moderation-latest');
