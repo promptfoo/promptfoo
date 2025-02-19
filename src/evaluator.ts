f2e33a8a58ec44ad0acc0a8666c009c6a4453685
Michael D'Angelo <michael.l.dangelo@gmail.com>
2025-02-19 12:15:44 -0500

    add test

--- src/evaluator.ts
+++ src/evaluator.ts
@@ -249,6 +249,7 @@ export async function runEval({
     if (response.error) {
       ret.error = response.error;
       ret.failureReason = ResultFailureReason.ERROR;
+      ret.success = false;
     } else if (response.output === null || response.output === undefined) {
       // NOTE: empty output often indicative of guardrails, so behavior differs for red teams.
       if (isRedteam) {
