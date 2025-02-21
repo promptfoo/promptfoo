62a7e9293e5474408370ccdf12a1d30c82d6e105
Michael D'Angelo <michael.l.dangelo@gmail.com>
2025-02-21 09:32:59 -0500

    remove type

--- src/redteam/plugins/harmbench.ts
+++ src/redteam/plugins/harmbench.ts
@@ -16,10 +16,6 @@ interface HarmbenchInput {
   ContextString?: string;
 }
 
-interface HarmbenchMetadata {
-  harmbenchBehavior: string;
-}
-
 async function fetchDataset(limit: number): Promise<HarmbenchInput[]> {
   try {
     const response = await fetchWithTimeout(DATASET_URL, {}, REQUEST_TIMEOUT_MS);
@@ -60,7 +56,7 @@ export class HarmbenchPlugin extends RedteamPluginBase {
     const records = await fetchDataset(n);
 
     return records.map(
-      (record): TestCase<HarmbenchMetadata> => ({
+      (record): TestCase => ({
         vars: {
           [this.injectVar]: dedent`
           ${record.Behavior}
