9c20d9d539accad4f93983c84b4ab96793e1fb35
Michael D'Angelo <michael.l.dangelo@gmail.com>
2025-03-19 01:39:29 -0400

    refactor(azure): add backwards compatibility layer

---
+++ src/providers/azure.ts
@@ -0,0 +1,7 @@
+/**
+ * @deprecated This file is maintained for backwards compatibility.
+ * Please import directly from 'src/providers/azure/index.ts' instead.
+ */
+
+// Re-export everything from the azure folder
+export * from './azure/index'; 
