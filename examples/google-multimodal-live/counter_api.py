75967f9e90e1300c2b4d0142df83c82d8d0391f5
Angus Brayne <angus_brayne@hotmail.co.uk>
2025-03-27 14:25:46 +0000

    tidy up

--- examples/google-multimodal-live/counter_api.py
+++ examples/google-multimodal-live/counter_api.py
@@ -7,31 +7,31 @@ counter = 3
 
 @app.route('/add_one', methods=['POST'])
 def add_one():
-  global counter
-  counter += 1
-  return {}
+    global counter
+    counter += 1
+    return {}
 
 
 @app.route('/get_count', methods=['GET'])
 def get_count():
-  return flask.jsonify({'counter': counter})
+    return flask.jsonify({'counter': counter})
 
 
 @app.route('/get_state', methods=['GET'])
 def get_state():
-  return flask.jsonify({'counter': counter})
+    return flask.jsonify({'counter': counter})
 
 
 @app.route('/shutdown', methods=['POST'])
 def shutdown():
-  func = flask.request.environ.get('werkzeug.server.shutdown')
-  if func is None:
-    return flask.jsonify(
-        {'message': 'Error: not running with the Werkzeug Server'}
-    )
-  func()
-  return flask.jsonify({'message': 'Server shutting down...'})
+    func = flask.request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        return flask.jsonify(
+            {'message': 'Error: not running with the Werkzeug Server'}
+        )
+    func()
+    return flask.jsonify({'message': 'Server shutting down...'})
 
 
 if __name__ == '__main__':
-  app.run(port=5000)
+    app.run(port=5000)
