a9f3f8b88fb53a5de5c93d4023258f4c503bd808
Angus Brayne <angus_brayne@hotmail.co.uk>
2025-03-27 14:30:23 +0000

    tidy up

--- examples/google-multimodal-live/counter_api.py
+++ examples/google-multimodal-live/counter_api.py
@@ -5,33 +5,33 @@ app = flask.Flask(__name__)
 counter = 3
 
 
-@app.route('/add_one', methods=['POST'])
+@app.route("/add_one", methods=["POST"])
 def add_one():
     global counter
     counter += 1
     return {}
 
 
-@app.route('/get_count', methods=['GET'])
+@app.route("/get_count", methods=["GET"])
 def get_count():
-    return flask.jsonify({'counter': counter})
+    return flask.jsonify({"counter": counter})
 
 
-@app.route('/get_state', methods=['GET'])
+@app.route("/get_state", methods=["GET"])
 def get_state():
-    return flask.jsonify({'counter': counter})
+    return flask.jsonify({"counter": counter})
 
 
-@app.route('/shutdown', methods=['POST'])
+@app.route("/shutdown", methods=["POST"])
 def shutdown():
-    func = flask.request.environ.get('werkzeug.server.shutdown')
+    func = flask.request.environ.get("werkzeug.server.shutdown")
     if func is None:
         return flask.jsonify(
-            {'message': 'Error: not running with the Werkzeug Server'}
+            {"message": "Error: not running with the Werkzeug Server"}
         )
     func()
-    return flask.jsonify({'message': 'Server shutting down...'})
+    return flask.jsonify({"message": "Server shutting down..."})
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=5000)
