9bd2d8efa310a32c4234321823dab89feb207908
Faizan Minhas <53799808+faizanminhas@users.noreply.github.com>
2025-03-28 08:12:24 -0700

    chore: fix type of Prompt to use omit (#3526)

--- src/app/src/pages/prompts/PromptDialog.tsx
+++ src/app/src/pages/prompts/PromptDialog.tsx
@@ -20,12 +20,12 @@ import TextField from '@mui/material/TextField';
 import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
 import { alpha } from '@mui/material/styles';
-import type { PromptWithMetadata } from '@promptfoo/types';
+import type { ServerPromptWithMetadata } from '@promptfoo/types';
 
 interface PromptDialogProps {
   openDialog: boolean;
   handleClose: () => void;
-  selectedPrompt: PromptWithMetadata & { recentEvalDate: string };
+  selectedPrompt: ServerPromptWithMetadata;
   showDatasetColumn?: boolean;
 }
 
