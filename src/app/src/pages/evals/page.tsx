da26b34b43c0a97a2388b38fc3314501bd23b07b
Will Holley <will@promptfoo.dev>
2025-03-31 14:36:05 -0400

    chore: set document title

--- src/app/src/pages/evals/page.tsx
+++ src/app/src/pages/evals/page.tsx
@@ -1,9 +1,14 @@
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Container, Paper, Typography, Box } from '@mui/material';
 import EvalsDataGrid from './components/EvalsDataGrid';
 
 export default function EvalsIndexPage() {
   const navigate = useNavigate();
+
+  useEffect(() => {
+    document.title = 'Evals | promptfoo';
+  }, []);
 
   return (
     <Container maxWidth="xl" sx={{ py: 4, height: '100%' }}>
