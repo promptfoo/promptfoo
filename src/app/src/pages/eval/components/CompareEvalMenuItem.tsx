1a907aaf3f61745b3fea463715b42c25fbc1f310
Will Holley <will@promptfoo.dev>
2025-03-31 15:35:57 -0400

    chore: lint

--- src/app/src/pages/eval/components/CompareEvalMenuItem.tsx
+++ src/app/src/pages/eval/components/CompareEvalMenuItem.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { callApi } from '@app/utils/api';
+import { useState } from 'react';
 import CompareIcon from '@mui/icons-material/Compare';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
@@ -14,40 +13,11 @@ interface CompareEvalMenuItemProps {
   onComparisonEvalSelected: (evalId: string) => void;
 }
 
-function CompareEvalMenuItem({ initialEvals, onComparisonEvalSelected }: CompareEvalMenuItemProps) {
+function CompareEvalMenuItem({ onComparisonEvalSelected }: CompareEvalMenuItemProps) {
   const { evalId: currentEvalId } = useStore();
   const [dialogOpen, setDialogOpen] = useState(false);
-  const [recentEvals, setRecentEvals] = useState<ResultLightweightWithLabel[]>(initialEvals);
-
-  const fetchRecentEvals = async () => {
-    try {
-      // First, get the dataset for the currentEvalId
-      // TODO(ian): In theory we already have this datasetId because we've fetched the current eval...dataset should probably be stored in the store
-      const fetchEvalId = currentEvalId || initialEvals[0].evalId;
-      const currentEvalResponse = await callApi(`/results/${fetchEvalId}`, {
-        cache: 'no-store',
-      });
-      const currentEvalData = await currentEvalResponse.json();
-      const datasetId = currentEvalData.data.datasetId;
-
-      if (!datasetId) {
-        console.error('No datasetId found for current eval ' + fetchEvalId);
-        return;
-      }
-
-      // Then, fetch the results with the obtained datasetId
-      const response = await callApi(`/results?datasetId=${datasetId}`, {
-        cache: 'no-store',
-      });
-      const body = (await response.json()) as { data: ResultLightweightWithLabel[] };
-      setRecentEvals(body.data.filter((_eval) => _eval.evalId !== fetchEvalId));
-    } catch (error) {
-      console.error('Error fetching recent evals:', error);
-    }
-  };
 
   const handleOpenDialog = () => {
-    fetchRecentEvals();
     setDialogOpen(true);
   };
 
