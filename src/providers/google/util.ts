79e0cfe72d6d723a67778bcf3772f9e95e5261be
abrayne <47567152+abrayne@users.noreply.github.com>
2025-04-03 16:03:26 +0100

    fix: AIStudio & Live to handle system prompts as thoroughly as vertex (#3588)

--- src/providers/google/util.ts
+++ src/providers/google/util.ts
@@ -1,8 +1,14 @@
 import type { GoogleAuth } from 'google-auth-library';
+import Clone from 'rfdc';
 import { z } from 'zod';
 import logger from '../../logger';
 import { maybeLoadFromExternalFile, renderVarsInObject } from '../../util';
+import { getNunjucksEngine } from '../../util/templates';
+import { parseChatPrompt } from '../shared';
 import { type Tool } from './types';
+import type { Content, Part } from './types';
+
+const clone = Clone();
 
 type Probability = 'NEGLIGIBLE' | 'LOW' | 'MEDIUM' | 'HIGH';
 
@@ -14,24 +20,6 @@ interface SafetyRating {
     | 'HARM_CATEGORY_DANGEROUS_CONTENT';
   probability: Probability;
   blocked: boolean;
-}
-
-interface PartText {
-  text: string;
-}
-
-interface PartFunctionCall {
-  functionCall: {
-    name: string;
-    args: Record<string, string>;
-  };
-}
-
-type Part = PartText | PartFunctionCall;
-
-interface Content {
-  parts: Part[];
-  role?: 'model';
 }
 
 interface Candidate {
@@ -132,7 +120,7 @@ export type GeminiPart = z.infer<typeof PartSchema>;
 export function maybeCoerceToGeminiFormat(contents: any): {
   contents: GeminiFormat;
   coerced: boolean;
-  systemInstruction: { parts: [GeminiPart, ...GeminiPart[]] } | undefined;
+  systemInstruction: { parts: [Part, ...Part[]] } | undefined;
 } {
   let coerced = false;
   const parseResult = GeminiFormatSchema.safeParse(contents);
@@ -224,9 +212,7 @@ export function maybeCoerceToGeminiFormat(contents: any): {
     contents: coercedContents,
     coerced,
     systemInstruction:
-      systemPromptParts.length > 0
-        ? { parts: systemPromptParts as [GeminiPart, ...GeminiPart[]] }
-        : undefined,
+      systemPromptParts.length > 0 ? { parts: systemPromptParts as [Part, ...Part[]] } : undefined,
   };
 }
 
@@ -282,7 +268,7 @@ export function loadFile(
 ) {
   // Ensures that files are loaded correctly. Files may be defined in multiple ways:
   // 1. Directly in the provider:
-  //    config_tools will be the file path, which will be loaded here in maybeLoadFromExternalFile.
+  //    config_var will be the file path, which will be loaded here in maybeLoadFromExternalFile.
   // 2. In a test variable that is used in the provider via a nunjucks:
   //    context_vars will contain a string of the contents of the file with whitespace.
   //    This will be inserted into the nunjucks in contfig_tools and the output needs to be parsed.
@@ -291,9 +277,71 @@ export function loadFile(
     try {
       return JSON.parse(fileContents);
     } catch (err) {
-      logger.debug(`ERROR: failed to file contents to JSON:\n${JSON.stringify(err)}`);
+      logger.debug(`ERROR: failed to convert file contents to JSON:\n${JSON.stringify(err)}`);
       return fileContents;
     }
   }
   return fileContents;
+}
+
+export function geminiFormatAndSystemInstructions(
+  prompt: string,
+  contextVars?: Record<string, string | object>,
+  configSystemInstruction?: Content | string,
+): {
+  contents: GeminiFormat;
+  systemInstruction: Content | { parts: [Part, ...Part[]] } | undefined;
+} {
+  let contents: GeminiFormat = parseChatPrompt(prompt, [
+    {
+      parts: [
+        {
+          text: prompt,
+        },
+      ],
+      role: 'user',
+    },
+  ]);
+  const {
+    contents: updatedContents,
+    coerced,
+    systemInstruction: parsedSystemInstruction,
+  } = maybeCoerceToGeminiFormat(contents);
+  if (coerced) {
+    logger.debug(`Coerced JSON prompt to Gemini format: ${JSON.stringify(contents)}`);
+    contents = updatedContents;
+  }
+
+  let systemInstruction: Content | string | undefined = parsedSystemInstruction;
+  if (configSystemInstruction && !systemInstruction) {
+    // Make a copy
+    systemInstruction = clone(configSystemInstruction);
+
+    // Load SI from file
+    if (typeof configSystemInstruction === 'string') {
+      systemInstruction = loadFile(configSystemInstruction, contextVars);
+    }
+
+    // Format SI if string was not a filepath above
+    if (typeof systemInstruction === 'string') {
+      systemInstruction = { parts: [{ text: systemInstruction }] };
+    }
+
+    if (contextVars && systemInstruction) {
+      const nunjucks = getNunjucksEngine();
+      for (const part of systemInstruction.parts) {
+        if (part.text) {
+          try {
+            part.text = nunjucks.renderString(part.text, contextVars);
+          } catch (err) {
+            throw new Error(`Unable to render nunjunks in systemInstruction: ${err}`);
+          }
+        }
+      }
+    }
+  } else if (configSystemInstruction && systemInstruction) {
+    throw new Error(`Template error: system instruction defined in prompt and config.`);
+  }
+
+  return { contents, systemInstruction };
 }
