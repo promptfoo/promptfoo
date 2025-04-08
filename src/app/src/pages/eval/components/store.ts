30c678a494ac58fb0e446b408ca105b84303f17f
Will Holley <will@promptfoo.dev>
2025-04-08 15:11:50 -0400

    chore: Removes IndexedDB backing from eval results store
    - State is currently re-initialized from the server on mount
    - Can contain stale state (i.e. constructed by deprecated logic)
    - This combination results in difficult-to-trace state reconciliation edge cases

--- src/app/src/pages/eval/components/store.ts
+++ src/app/src/pages/eval/components/store.ts
@@ -1,22 +1,7 @@
 import { convertResultsToTable } from '@promptfoo/util/convertEvalResultsToTable';
 import type { VisibilityState } from '@tanstack/table-core';
-import { get, set, del } from 'idb-keyval';
 import { create } from 'zustand';
-import type { StateStorage } from 'zustand/middleware';
-import { persist, createJSONStorage } from 'zustand/middleware';
 import type { EvaluateSummaryV2, EvaluateTable, ResultsFile, UnifiedConfig } from './types';
-
-const storage: StateStorage = {
-  getItem: async (name: string): Promise<string | null> => {
-    return (await get(name)) || null;
-  },
-  setItem: async (name: string, value: string): Promise<void> => {
-    await set(name, value);
-  },
-  removeItem: async (name: string): Promise<void> => {
-    await del(name);
-  },
-};
 
 interface ColumnState {
   selectedColumns: string[];
@@ -69,69 +54,62 @@ interface TableState {
   setMaxImageHeight: (maxImageHeight: number) => void;
 }
 
-export const useStore = create<TableState>()(
-  persist(
-    (set, get) => ({
-      evalId: null,
-      setEvalId: (evalId: string) => set(() => ({ evalId })),
+export const useStore = create<TableState>()((set, get) => ({
+  evalId: null,
+  setEvalId: (evalId: string) => set(() => ({ evalId })),
 
-      author: null,
-      setAuthor: (author: string | null) => set(() => ({ author })),
+  author: null,
+  setAuthor: (author: string | null) => set(() => ({ author })),
 
-      version: null,
-      setVersion: (version: number) => set(() => ({ version })),
+  version: null,
+  setVersion: (version: number) => set(() => ({ version })),
 
-      table: null,
-      setTable: (table: EvaluateTable | null) => set(() => ({ table })),
-      setTableFromResultsFile: (resultsFile: ResultsFile) => {
-        if (resultsFile.version && resultsFile.version >= 4) {
-          set(() => ({ table: convertResultsToTable(resultsFile), version: resultsFile.version }));
-        } else {
-          const results = resultsFile.results as EvaluateSummaryV2;
-          set(() => ({ table: results.table, version: resultsFile.version }));
-        }
+  table: null,
+  setTable: (table: EvaluateTable | null) => set(() => ({ table })),
+  setTableFromResultsFile: (resultsFile: ResultsFile) => {
+    if (resultsFile.version && resultsFile.version >= 4) {
+      set(() => ({ table: convertResultsToTable(resultsFile), version: resultsFile.version }));
+    } else {
+      const results = resultsFile.results as EvaluateSummaryV2;
+      set(() => ({ table: results.table, version: resultsFile.version }));
+    }
+  },
+  config: null,
+  setConfig: (config: Partial<UnifiedConfig> | null) => set(() => ({ config })),
+
+  maxTextLength: 250,
+  setMaxTextLength: (maxTextLength: number) => set(() => ({ maxTextLength })),
+  wordBreak: 'break-word',
+  setWordBreak: (wordBreak: 'break-word' | 'break-all') => set(() => ({ wordBreak })),
+  showInferenceDetails: true,
+  setShowInferenceDetails: (showInferenceDetails: boolean) => set(() => ({ showInferenceDetails })),
+  renderMarkdown: false,
+  setRenderMarkdown: (renderMarkdown: boolean) => set(() => ({ renderMarkdown })),
+  prettifyJson: false,
+  setPrettifyJson: (prettifyJson: boolean) => set(() => ({ prettifyJson })),
+  showPrompts: false,
+  setShowPrompts: (showPrompts: boolean) => set(() => ({ showPrompts })),
+  showPassFail: true,
+  setShowPassFail: (showPassFail: boolean) => set(() => ({ showPassFail })),
+
+  inComparisonMode: false,
+  setInComparisonMode: (inComparisonMode: boolean) => set(() => ({ inComparisonMode })),
+  stickyHeader: true,
+  setStickyHeader: (stickyHeader: boolean) => set(() => ({ stickyHeader })),
+
+  columnStates: {},
+  setColumnState: (evalId: string, state: ColumnState) => {
+    console.log('setColumnState', evalId, state);
+    return set((prevState) => ({
+      columnStates: {
+        ...prevState.columnStates,
+        [evalId]: state,
       },
-      config: null,
-      setConfig: (config: Partial<UnifiedConfig> | null) => set(() => ({ config })),
+    }));
+  },
 
-      maxTextLength: 250,
-      setMaxTextLength: (maxTextLength: number) => set(() => ({ maxTextLength })),
-      wordBreak: 'break-word',
-      setWordBreak: (wordBreak: 'break-word' | 'break-all') => set(() => ({ wordBreak })),
-      showInferenceDetails: true,
-      setShowInferenceDetails: (showInferenceDetails: boolean) =>
-        set(() => ({ showInferenceDetails })),
-      renderMarkdown: false,
-      setRenderMarkdown: (renderMarkdown: boolean) => set(() => ({ renderMarkdown })),
-      prettifyJson: false,
-      setPrettifyJson: (prettifyJson: boolean) => set(() => ({ prettifyJson })),
-      showPrompts: false,
-      setShowPrompts: (showPrompts: boolean) => set(() => ({ showPrompts })),
-      showPassFail: true,
-      setShowPassFail: (showPassFail: boolean) => set(() => ({ showPassFail })),
-
-      inComparisonMode: false,
-      setInComparisonMode: (inComparisonMode: boolean) => set(() => ({ inComparisonMode })),
-      stickyHeader: true,
-      setStickyHeader: (stickyHeader: boolean) => set(() => ({ stickyHeader })),
-
-      columnStates: {},
-      setColumnState: (evalId: string, state: ColumnState) =>
-        set((prevState) => ({
-          columnStates: {
-            ...prevState.columnStates,
-            [evalId]: state,
-          },
-        })),
-
-      maxImageWidth: 256,
-      setMaxImageWidth: (maxImageWidth: number) => set(() => ({ maxImageWidth })),
-      maxImageHeight: 256,
-      setMaxImageHeight: (maxImageHeight: number) => set(() => ({ maxImageHeight })),
-    }),
-    {
-      name: 'ResultsViewStorage',
-      storage: createJSONStorage(() => storage),
-    },
-  ),
-);
+  maxImageWidth: 256,
+  setMaxImageWidth: (maxImageWidth: number) => set(() => ({ maxImageWidth })),
+  maxImageHeight: 256,
+  setMaxImageHeight: (maxImageHeight: number) => set(() => ({ maxImageHeight })),
+}));
