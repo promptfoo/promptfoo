2bebd80f38ea9cdef71d9f7e57e2e9e083c06fa9
Michael D'Angelo <michael.l.dangelo@gmail.com>
2025-05-20 00:05:53 -0700

    patch

--- src/app/vite.config.ts
+++ src/app/vite.config.ts
@@ -47,6 +47,11 @@ export default defineConfig({
     alias: {
       '@app': path.resolve(__dirname, './src'),
       '@promptfoo': path.resolve(__dirname, '../'),
+      react: path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+      'react-dom/client': path.resolve(__dirname, './node_modules/react-dom/client.js'),
+      'react/jsx-runtime': path.resolve(__dirname, './node_modules/react/jsx-runtime.js'),
+      'react/jsx-dev-runtime': path.resolve(__dirname, './node_modules/react/jsx-dev-runtime.js'),
     },
   },
   build: {
