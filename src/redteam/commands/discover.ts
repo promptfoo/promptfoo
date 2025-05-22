29b6572d8079585ef67b734645656f5eaf01b913
Will Holley <will@promptfoo.dev>
2025-05-22 16:15:26 -0400

    fix: linebreaks

--- src/redteam/commands/discover.ts
+++ src/redteam/commands/discover.ts
@@ -295,7 +295,7 @@ export function mergeTargetPurposeDiscoveryResults(
     `,
   ]
     .filter(Boolean)
-    .join('');
+    .join('\n');
 }
 
 // ========================================================
