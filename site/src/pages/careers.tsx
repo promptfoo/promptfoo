d050a4bb3daac48cc6a83ad6e0b4890707d4efa2
Ian Webster <ianw_github@ianww.com>
2025-05-31 06:33:59 -0700

    simpler

--- site/src/pages/careers.tsx
+++ site/src/pages/careers.tsx
@@ -13,20 +13,7 @@ export default function Careers(): JSX.Element {
 
   return (
     <Layout title="Careers at Promptfoo" description="Shape the future of LLM Security">
-      <main className={styles.careersContainer}>
-        <h1>Careers at Promptfoo</h1>
-        <p>Our mission is to help developers ship secure and reliable AI apps.</p>
-        <p>
-          Our core product is an open-source pentesting and evaluation framework used by 80,000+
-          developers. Promptfoo is among the most popular evaluation frameworks and is the first
-          product to adapt AI-specific pentesting techniques to your application.
-        </p>
-        <p>
-          We're betting that the future of AI is open-source and are deeply committed to our
-          developer community and our{' '}
-          <a href="https://github.com/promptfoo/promptfoo">open-source</a> offering.
-        </p>
-      </main>
+      <h1 className={styles.careersTitle}>Careers at Promptfoo</h1>
       <div id="ashby_embed" />
     </Layout>
   );
