e85737e6b9c05aa77f294972d753127f6b26a449
Michael <michael.l.dangelo@gmail.com>
2025-06-04 07:22:05 -0700

    Update src/validators/redteam.ts

--- src/validators/redteam.ts
+++ src/validators/redteam.ts
@@ -387,5 +387,3 @@ assert<TypeEqualityGuard<RedteamFileConfig, z.infer<typeof RedteamConfigSchema>>
 
 // TODO: Why is this never?
 // assert<TypeEqualityGuard<RedteamPluginObject, z.infer<typeof RedteamPluginObjectSchema>>>();
-
-export { assert };
