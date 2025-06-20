5b5e1a45f30c4e47731ea97bcf622ed14e7b592c
Will Holley <will@promptfoo.dev>
2025-06-20 16:33:27 -0400

    chore: Extend `SimulatedUser` provider

--- src/redteam/providers/simulatedUser.ts
+++ src/redteam/providers/simulatedUser.ts
@@ -1,43 +1,23 @@
 import { SimulatedUser } from '../../providers/simulatedUser';
-import type {
-  ApiProvider,
-  CallApiContextParams,
-  CallApiOptionsParams,
-  ProviderOptions,
-  ProviderResponse,
-} from '../../types';
 import invariant from '../../util/invariant';
 import { REDTEAM_SIMULATED_USER_STRATEGY_ID } from '../constants/strategies';
 
-export default class RedteamSimulatedUserProvider implements ApiProvider {
-  private readonly injectVar: string;
-  private readonly maxTurns?: number;
-  private readonly userProvider?: ProviderOptions;
+const PROVIDER_ID = `promptfoo:redteam:${REDTEAM_SIMULATED_USER_STRATEGY_ID}`;
 
-  constructor(config: ProviderOptions['config'] & { injectVar: string }) {
+export default class RedteamSimulatedUserProvider extends SimulatedUser {
+  constructor(config: { injectVar: string; maxTurns?: number }) {
     invariant(config.injectVar, 'Expected injectVar to be set');
-    this.injectVar = config.injectVar;
-    this.maxTurns = config.maxTurns as number | undefined;
-    this.userProvider = config.userProvider as ProviderOptions | undefined;
+
+    super({
+      id: PROVIDER_ID,
+      config: {
+        instructions: `{{${config.injectVar}}}`,
+        maxTurns: config.maxTurns,
+      },
+    });
   }
 
   id() {
-    return `promptfoo:redteam:${REDTEAM_SIMULATED_USER_STRATEGY_ID}`;
-  }
-
-  async callApi(
-    prompt: string,
-    context?: CallApiContextParams,
-    options?: CallApiOptionsParams,
-  ): Promise<ProviderResponse> {
-    const provider = new SimulatedUser({
-      id: this.id(),
-      config: {
-        instructions: `{{${this.injectVar}}}`,
-        maxTurns: this.maxTurns,
-        userProvider: this.userProvider,
-      },
-    });
-    return provider.callApi(prompt, context, options);
+    return PROVIDER_ID;
   }
 }
