3c300f3b9ef435088689565f7f732fc419274cbc
Michael <michael.l.dangelo@gmail.com>
2025-06-21 12:56:09 -0800

    feat: add feedback page and update CLI link

---
+++ site/src/pages/feedback.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useColorMode } from '@docusaurus/theme-common';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Layout from '@theme/Layout';
+
+export default function Feedback(): JSX.Element {
+  const { colorMode } = useColorMode();
+  const iframeStyle: React.CSSProperties = {
+    width: '100%',
+    height: '800px',
+    border: 0,
+    backgroundColor: colorMode === 'dark' ? '#fff' : undefined,
+  };
+
+  return (
+    <Layout title="Feedback">
+      <Container maxWidth="md" sx={{ py: 6 }}>
+        <Typography variant="h3" component="h1" align="center" gutterBottom>
+          We'd love your feedback
+        </Typography>
+        <Box mt={4}>
+          <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLScAnqlqX-ep-aOn6umjXXDVafc1sLTOEd5W6rMAPKllLk0CIA/viewform?embedded=1"
+            style={iframeStyle}
+            loading="lazy"
+          >
+            Loading…
+          </iframe>
+        </Box>
+      </Container>
+    </Layout>
+  );
+}
