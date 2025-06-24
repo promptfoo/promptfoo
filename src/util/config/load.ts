a1a405147bb85d911b31e45b11b9cd41c7bacb0b
sidd2pir <sidd@2pir.ai>
2025-06-23 20:51:12 -0700

    feat: assertion generation (#4559)

--- src/util/config/load.ts
+++ src/util/config/load.ts
@@ -462,7 +462,7 @@ export async function combineConfigs(configPaths: string[]): Promise<UnifiedConf
 export async function resolveConfigs(
   cmdObj: Partial<CommandLineOptions>,
   _defaultConfig: Partial<UnifiedConfig>,
-  type?: 'DatasetGeneration',
+  type?: 'DatasetGeneration' | 'AssertionGeneration',
 ): Promise<{ testSuite: TestSuite; config: Partial<UnifiedConfig>; basePath: string }> {
   let fileConfig: Partial<UnifiedConfig> = {};
   let defaultConfig = _defaultConfig;
@@ -552,7 +552,6 @@ export async function resolveConfigs(
     `);
     process.exit(1);
   }
-
   if (!hasPrompts) {
     logger.error('You must provide at least 1 prompt');
     process.exit(1);
@@ -561,6 +560,7 @@ export async function resolveConfigs(
   if (
     // Dataset configs don't require providers
     type !== 'DatasetGeneration' &&
+    type !== 'AssertionGeneration' &&
     !hasProviders
   ) {
     logger.error('You must specify at least 1 provider (for example, openai:gpt-4.1)');
