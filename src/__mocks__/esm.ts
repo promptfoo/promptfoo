8379b701b51fddfc5cad6a2baf71f518192e0fa1
Michael D'Angelo <michael.l.dangelo@gmail.com>
2025-06-29 19:57:49 -0400

    simplify

--- src/__mocks__/esm.ts
+++ src/__mocks__/esm.ts
@@ -1,9 +1,5 @@
 import * as path from 'path';
 
-export function getDirectory() {
-  return '/test/dir';
-}
-
 export function importModule(filePath: string, functionName?: string) {
   // eslint-disable-next-line @typescript-eslint/no-require-imports
   const mod = require(path.resolve(filePath));
