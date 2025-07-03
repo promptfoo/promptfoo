d5781bd314185f1c3386135f36442344f9e5ddfb
Michael D'Angelo <michael.l.dangelo@gmail.com>
2025-07-03 01:48:19 -0400

    revert

--- test/assertions/runAssertion.test.ts
+++ test/assertions/runAssertion.test.ts
@@ -17,6 +17,7 @@ jest.mock('../../src/redteam/remoteGeneration', () => ({
   shouldGenerateRemote: jest.fn().mockReturnValue(false),
 }));
 
+// Causes a SIGSEGV in github actions.
 jest.mock('better-sqlite3');
 
 jest.mock('proxy-agent', () => ({
