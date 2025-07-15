779dee8a10077cb2fabdc7dcbb1579484f5aa7e1
Will Holley <will@promptfoo.dev>
2025-07-15 15:48:23 -0400

    Revert "Removes `<MetricFilterSelector/>`"
    This reverts commit a0f136cbe7cfc93bfcc465b2a7e7d74d4f0488ce.

--- src/app/src/pages/eval/components/ResultsView.tsx
+++ src/app/src/pages/eval/components/ResultsView.tsx
@@ -41,6 +41,7 @@ import { EvalIdChip } from './EvalIdChip';
 import EvalSelectorDialog from './EvalSelectorDialog';
 import EvalSelectorKeyboardShortcut from './EvalSelectorKeyboardShortcut';
 import { FilterModeSelector } from './FilterModeSelector';
+import { MetricFilterSelector } from './MetricFilterSelector';
 import ResultsCharts from './ResultsCharts';
 import FiltersButton from './ResultsFilters/FiltersButton';
 import FiltersForm from './ResultsFilters/FiltersForm';
