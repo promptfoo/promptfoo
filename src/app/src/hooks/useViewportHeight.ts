885803d11842b63c9454704492725e24010c711c
Will <8657791+will-holley@users.noreply.github.com>
2025-07-15 15:13:03 -0400

    fix(webui): Eval results table horizontal scrolling (#4826)

---
+++ src/app/src/hooks/useViewportHeight.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+
+/**
+ * Returns the height of the viewport in pixels.
+ * Responds to window resize events.
+ *
+ * @returns The height of the viewport in pixels.
+ *
+ * @example
+ * const viewportHeight = useViewportHeight();
+ * console.log(viewportHeight); // 1000
+ */
+export default function useViewportHeight() {
+  const [viewportHeight, setViewportHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = () => setViewportHeight(window.innerHeight);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return viewportHeight;
+}
