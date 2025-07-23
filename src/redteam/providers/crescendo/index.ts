969277b7edff95b15c41d16caff239d7eafdd936
Faizan Minhas <faizan1030@gmail.com>
2025-07-23 10:41:51 -0700

    feat: Apply plugin modifiers for crescendo

--- src/redteam/providers/crescendo/index.ts
+++ src/redteam/providers/crescendo/index.ts
@@ -8,10 +8,10 @@ import { extractFirstJsonObject, safeJsonStringify } from '../../../util/json';
 import { getNunjucksEngine } from '../../../util/templates';
 import { sleep } from '../../../util/time';
 import {
+  accumulateGraderTokenUsage,
+  accumulateResponseTokenUsage,
   accumulateTokenUsage,
   createEmptyTokenUsage,
-  accumulateResponseTokenUsage,
-  accumulateGraderTokenUsage,
 } from '../../../util/tokenUsageUtils';
 import { shouldGenerateRemote } from '../../remoteGeneration';
 import { isBasicRefusal } from '../../util';
@@ -261,11 +261,17 @@ export class CrescendoProvider implements ApiProvider {
 
     const totalTokenUsage: TokenUsage = createEmptyTokenUsage();
 
+    logger.debug(`[Crescendo] Test metadata: ${JSON.stringify(context?.test, null, 2)}`);
+    logger.debug(`[Crescendo] Config: ${JSON.stringify(this.config, null, 2)}`);
     const systemPrompt = this.nunjucks.renderString(CRESCENDO_SYSTEM_PROMPT, {
       conversationObjective: this.userGoal,
       currentRound: roundNum + 1,
       maxTurns: this.maxTurns,
       purpose: context?.test?.metadata?.purpose,
+      modifierSection:
+        Object.entries(context?.test?.metadata?.modifiers || {})
+          .map(([key, value]) => `${key}: ${value}`)
+          .join('\n') || undefined,
     });
 
     this.memory.addMessage(this.redTeamingChatConversationId, {
