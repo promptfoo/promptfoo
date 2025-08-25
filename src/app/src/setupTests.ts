cab6dd167494ce4e31943b227fdc4e5f706ffbf1
MrFlounder <k.zanggs@gmail.com>
2025-08-25 12:05:01 -0700

    attempt another fix

--- src/app/src/setupTests.ts
+++ src/app/src/setupTests.ts
@@ -1,10 +1,4 @@
 import '@testing-library/jest-dom';
-import 'prismjs';
-import 'prismjs/components/prism-clike';
-import 'prismjs/components/prism-javascript';
-import 'prismjs/components/prism-json';
-import 'prismjs/components/prism-yaml';
-import 'prismjs/components/prism-http';
 
 // We can mock the environment variables. For example:
 // process.env.PROMPTFOO_VERSION = '1.0.0';
