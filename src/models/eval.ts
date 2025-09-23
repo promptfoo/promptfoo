a5639ceedefa243317b473c4542bbeffffc4aaeb
Will Holley <will@promptfoo.dev>
2025-09-23 15:44:47 -0400

    chore: Sanitize SQL value

--- src/models/eval.ts
+++ src/models/eval.ts
@@ -607,7 +607,8 @@ export default class Eval {
             condition = `(${explicit})`;
           }
         } else if (type === 'policy' && operator === 'equals') {
-          condition = `(named_scores LIKE '%PolicyViolation:%' AND named_scores LIKE '%${value}%')`;
+          const sanitizedValue = sanitizeValue(value);
+          condition = `(named_scores LIKE '%PolicyViolation:%' AND named_scores LIKE '%${sanitizedValue}%')`;
         }
 
         if (condition) {
