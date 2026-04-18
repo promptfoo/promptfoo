eb4177919dbd4577906eb7f52bf54b1bcef43d3e
Michael D'Angelo <mdangelo@openai.com>
2026-04-17 18:04:49 -0700

    test: isolate env in cache tests

--- test/test-hygiene.test.ts
+++ test/test-hygiene.test.ts
@@ -121,7 +121,6 @@ const allowedSkippedTests: AllowedSkip[] = [
 const legacyHoistedPersistentMockFiles = new Set<string>();
 
 const legacyDirectProcessEnvMutationFiles = new Set([
-  'cache.test.ts',
   'code-scan-action/main.test.ts',
   'commands/modelScan.test.ts',
   'constants.test.ts',
