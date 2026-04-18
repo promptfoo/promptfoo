1659a3c655195acf5ffb02b3a27a040aa3c85440
Michael D'Angelo <mdangelo@openai.com>
2026-04-17 18:05:54 -0700

    test: isolate env in evaluatorHelpers tests

--- test/test-hygiene.test.ts
+++ test/test-hygiene.test.ts
@@ -121,7 +121,6 @@ const allowedSkippedTests: AllowedSkip[] = [
 const legacyHoistedPersistentMockFiles = new Set<string>();
 
 const legacyDirectProcessEnvMutationFiles = new Set([
-  'evaluatorHelpers.test.ts',
   'fetch.test.ts',
   'globalConfig/cloud.test.ts',
   'logger.test.ts',
