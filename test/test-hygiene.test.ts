c3bcabb117987e8dc43327b82d5f9ae0aecd65b2
Michael D'Angelo <mdangelo@openai.com>
2026-04-17 18:05:00 -0700

    test: isolate env in constants tests

--- test/test-hygiene.test.ts
+++ test/test-hygiene.test.ts
@@ -121,7 +121,6 @@ const allowedSkippedTests: AllowedSkip[] = [
 const legacyHoistedPersistentMockFiles = new Set<string>();
 
 const legacyDirectProcessEnvMutationFiles = new Set([
-  'constants.test.ts',
   'database.test.ts',
   'envars.test.ts',
   'evaluatorHelpers.test.ts',
