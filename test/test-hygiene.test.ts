0b2c4da690d66c7308ed4aa14b78e96dfa841f69
Michael D'Angelo <mdangelo@openai.com>
2026-04-17 18:07:12 -0700

    test: isolate env in logger tests

--- test/test-hygiene.test.ts
+++ test/test-hygiene.test.ts
@@ -121,7 +121,6 @@ const allowedSkippedTests: AllowedSkip[] = [
 const legacyHoistedPersistentMockFiles = new Set<string>();
 
 const legacyDirectProcessEnvMutationFiles = new Set([
-  'logger.test.ts',
   'matchers/utils.test.ts',
   'onboarding.test.ts',
   'prompts/index.test.ts',
