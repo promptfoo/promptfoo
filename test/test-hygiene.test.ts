9e94f8eaf02ed0484875a3fd14d1dafeae4d332e
Michael D'Angelo <mdangelo@openai.com>
2026-04-17 18:12:29 -0700

    test: isolate env in util render tests

--- test/test-hygiene.test.ts
+++ test/test-hygiene.test.ts
@@ -121,7 +121,6 @@ const allowedSkippedTests: AllowedSkip[] = [
 const legacyHoistedPersistentMockFiles = new Set<string>();
 
 const legacyDirectProcessEnvMutationFiles = new Set([
-  'util/render.test.ts',
   'util/templates.test.ts',
   'util/transform.test.ts',
   'util/utils.test.ts',
